198fa6b5c733811ebbfd6407a254a09cd61b2936
Jeff Hollan <jeff.hollan@snowflake.com>
2022-08-13 16:41:54 -0700

    add login

--- snowcli.py
+++ snowcli.py
@@ -12,6 +12,13 @@ def deploy():
 def build():
     click.echo('Building...')
 
+@click.command()
+@click.option('--account', prompt=True, help='Snowflake account')
+@click.option('--username', prompt=True, help='Snowflake username')
+@click.option('--password', prompt=True, hide_input=True, help='Snowflake password')
+def login(account, username, password):
+    click.echo(f'Logging in... with {account} {username} {password}')
+
 @click.group()
 def cli():
     pass
@@ -19,6 +26,7 @@ def cli():
 cli.add_command(create)
 cli.add_command(build)
 cli.add_command(deploy)
+cli.add_command(login)
 
 def main():
     cli()
