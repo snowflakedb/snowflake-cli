d42b15f45077ca9b7fb861a1597e7a5fd7b0a777
Jeff Hollan <jeffhollan@outlook.com>
2022-08-13 16:35:06 -0700

    basic scaffolding

--- snowcli.py
+++ snowcli.py
@@ -1,5 +1,24 @@
 import click
 
 @click.command()
+def create():
+    click.echo('Creating a function...')
+
+@click.command()
+def deploy():
+    click.echo('Deploying...')
+
+@click.command()
+def build():
+    click.echo('Building...')
+
+@click.group()
 def cli():
-    click.echo('Hello world')
+    pass
+
+cli.add_command(create)
+cli.add_command(build)
+cli.add_command(deploy)
+
+def main():
+    cli()
