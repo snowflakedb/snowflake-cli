6457b3f5cf954efa9952835b960b5d686138d4d8
Jeff Hollan <jeff.hollan@snowflake.com>
2022-08-14 08:52:10 -0700

    moving config into config.py

--- snowcli.py
+++ snowcli.py
@@ -1,10 +1,5 @@
 import click
-import configparser
-import snow_connector
-
-CONFIG = configparser.ConfigParser()
-CONFIG.read('credentials')
-SNOWFLAKE_CONN: snow_connector.SnowflakeConnector
+import config
 
 @click.group()
 def function():
@@ -15,47 +10,36 @@ def function():
 @click.option('--database', '-d', help='Database name')
 @click.option('--schema', '-s', help='Schema name')
 def function_create(name, database, schema):
-    if isAuth():
-        connectToSnowflake()
-        click.echo(SNOWFLAKE_CONN.createFunction(name='testFunction', inputParameters='', returnType='string', handler='app.run', imports='@~/my-deployment-package.zip', database='JEFFHOLLAN_DEMO', schema='PUBLIC', role='ACCOUNTADMIN', warehouse='SRI'))
+    if config.isAuth():
+        config.connectToSnowflake()
+        click.echo(config.snowflake_connection.createFunction(name='testFunction', inputParameters='', returnType='string', handler='app.run', imports='@~/my-deployment-package.zip', database='JEFFHOLLAN_DEMO', schema='PUBLIC', role='ACCOUNTADMIN', warehouse='SRI'))
 
 @click.command()
 def function_deploy():
-    if isAuth():
+    if config.isAuth():
         click.echo('Deploying...')
 
 @click.command()
 def function_build():
-    if isAuth():
-        click.echo(f'Building... with {CONFIG.get("default", "account")}')
+    if config.isAuth():
+        click.echo(f'Building... with {config.auth_config.get("default", "account")}')
 
 @click.command()
 @click.option('--account', prompt=True, help='Snowflake account')
 @click.option('--username', prompt=True, help='Snowflake username')
 @click.option('--password', prompt=True, hide_input=True, help='Snowflake password')
 def login(account, username, password):
-    global CONFIG
-    CONFIG['default'] = {
+    config.auth_config['default'] = {
         'account': account,
         'username': username,
         'password': password
     }
     with open('credentials', 'w') as configfile:
-        CONFIG.write(configfile)
+        config.auth_config.write(configfile)
 
 @click.group()
 def cli():
     pass
-
-def isAuth():
-    if not CONFIG.has_option('default', 'account'):
-        click.echo('You must login first with `snowcli login`')
-        return False
-    return True
-
-def connectToSnowflake():
-    global SNOWFLAKE_CONN
-    SNOWFLAKE_CONN = snow_connector.SnowflakeConnector(CONFIG.get('default', 'account'), CONFIG.get('default', 'username'), CONFIG.get('default', 'password'))
 
 def main():
     cli()
