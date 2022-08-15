a5d6755534546787f667fbc8e06565a7ae571fb3
Jeff Hollan <jeff.hollan@snowflake.com>
2022-08-14 17:13:29 -0700

    create and upload working

---
+++ utils.py
@@ -0,0 +1,18 @@
+import shutil
+
+def getDeployNames(database, schema, name) -> dict:
+    stage = f'{database}.{schema}.deployments'
+    path = f'/{name}/app.zip'
+    directory = f'/{name}'
+    return { 
+        'stage': stage, 
+        'path': path, 
+        'full_path': f'@{stage}{path}',
+        'directory': directory 
+        }
+
+# create a temporary directory, copy the file_path to it and rename to app.zip
+def prepareAppZip(file_path, temp_dir) -> str:
+    temp_path = temp_dir + '/app.zip'
+    shutil.copy(file_path, temp_path)
+    return temp_path
