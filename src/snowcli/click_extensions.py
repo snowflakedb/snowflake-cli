d8a2d938210c31d26350bb90d0df1187a565cb5c
Jon Roes <jon.roes@snowflake.com>
2022-08-25 16:59:45 -0400

    no longer using click extensions

--- src/snowcli/click_extensions.py
+++
@@ -1,9 +0,0 @@
-import click
-from snowcli.config import AppConfig
-
-def require_environment(function):
-    app_config = AppConfig()
-
-    function = click.option(
-        '--environment', '-e', help='Environment name', default=app_config.config.get('environment'))(function)
-    return function
