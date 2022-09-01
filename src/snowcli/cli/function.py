a37fef8d41ce5331cd6b5ad7d37836bd7a021cec
Jon Roes <jon.roes@snowflake.com>
2022-09-01 15:08:15 -0400

    switch to Rich print

--- src/snowcli/cli/function.py
+++ src/snowcli/cli/function.py
@@ -9,7 +9,6 @@ import pkg_resources
 import tempfile
 import re
 from rich import print
-from rich.console import Console
 from rich.table import Table
 import typer
 
@@ -18,7 +17,6 @@ from snowcli.config import AppConfig
 from snowcli.snowsql_config import SnowsqlConfig
 from snowcli.utils import print_db_cursor, print_list_tuples
 
-console = Console()
 app = typer.Typer()
 EnvironmentOption = typer.Option("dev", help='Environment name')
 
@@ -32,7 +30,8 @@ def function_init():
 
 @app.command("create")
 def function_create(environment: str = EnvironmentOption,
-                    name: str = typer.Option(..., '--name', '-n', help="Name of the function"),
+                    name: str = typer.Option(..., '--name', '-n',
+                                             help="Name of the function"),
                     file: Path = typer.Option('app.py',
                                               '--file',
                                               '-f', 
@@ -65,15 +64,15 @@ def function_create(environment: str = EnvironmentOption,
     if config.isAuth():
         config.connectToSnowflake()
         deploy_dict = utils.getDeployNames(env_conf['database'], env_conf['schema'], name)
-        click.echo('Uploading deployment file to stage...')
+        print('Uploading deployment file to stage...')
 
         with tempfile.TemporaryDirectory() as temp_dir:
             temp_app_zip_path = utils.prepareAppZip(file, temp_dir)
             config.snowflake_connection.uploadFileToStage(
                 file_path=temp_app_zip_path, destination_stage=deploy_dict['stage'], path=deploy_dict['directory'], overwrite=overwrite, role=env_conf['role'])
         packages = utils.getSnowflakePackages()
-        click.echo('Creating function...')
-        click.echo(
+        print('Creating function...')
+        print(
             config.snowflake_connection.createFunction(name=name, inputParameters=input_parameters,
                                                        returnType=return_type,
                                                        handler=handler,
@@ -105,11 +104,11 @@ def function_update(environment: str = EnvironmentOption,
     if config.isAuth():
         config.connectToSnowflake()
         deploy_dict = utils.getDeployNames(env_conf['database'], env_conf['schema'], name)
-        click.echo(f'Finding function {name}...')
+        print(f'Finding function {name}...')
         functions = config.snowflake_connection.listFunctions(
             database=env_conf['database'], schema=env_conf['schema'], role=env_conf['role'], warehouse=env_conf['warehouse'], like=name)
         if len(functions) == 0:
-            click.echo(f'No functions named: {name} found')
+            print(f'No functions named: {name} found')
             return
         else:
             # if functions == 1, get function arguments property and regular expression till the fist )
@@ -123,34 +122,34 @@ def function_update(environment: str = EnvironmentOption,
                 # get the first group from regex
                 function_signature = regex.search(
                     function[8]).group(1)
-                click.echo(f'Found function {function_signature}')
+                print(f'Found function {function_signature}')
             else:
-                click.echo(f'Found {len(functions)} like: {name}')
+                print(f'Found {len(functions)} like: {name}')
                 function_signatures = []
                 for function in functions:
                     function_signatures.append(regex.search(
                         function[8]).group(1))
                 function_signature = click.prompt(
                     'Please select the function you want to deploy', type=click.Choice(function_signatures))
-        click.echo(f'Deploying new file for {name}...')
+        print(f'Deploying new file for {name}...')
         with tempfile.TemporaryDirectory() as temp_dir:
             temp_app_zip_path = utils.prepareAppZip(file, temp_dir)
             deploy_response = config.snowflake_connection.uploadFileToStage(
                 file_path=temp_app_zip_path, destination_stage=deploy_dict['stage'], path=deploy_dict['directory'], overwrite=True, role=env_conf['role'])
-        click.echo(
+        print(
             f'{deploy_response} uploaded to stage {deploy_dict["full_path"]}')
-        click.echo(f'Checking if any new packages to update...')
+        print(f'Checking if any new packages to update...')
         function_details = config.snowflake_connection.describeFunction(
             function=function_signature, database=env_conf['database'], schema=env_conf['schema'], role=env_conf['role'], warehouse=env_conf['warehouse'])
         function_json = utils.convertFunctionDetailsToDict(function_details)
         anaconda_packages = function_json['packages']
-        click.echo(
+        print(
             f'Found {len(anaconda_packages)} defined Anaconda packages in deployed function...')
-        click.echo(
+        print(
             f'Checking if any packages defined or missing from requirements.snowflake.txt...')
         updatedPackageList = utils.getSnowflakePackagesDelta(anaconda_packages)
         if updatedPackageList:
-            click.echo(f'Replacing function with updated packages...')
+            print(f'Replacing function with updated packages...')
             config.snowflake_connection.createFunction(
                 name=name,
                 inputParameters=function_json['signature'].strip('()'),
@@ -163,35 +162,35 @@ def function_update(environment: str = EnvironmentOption,
                 warehouse=env_conf['warehouse'],
                 overwrite=True,
                 packages=updatedPackageList)
-            click.echo(
+            print(
                 f'Function {name} updated with new packages. Deployment complete!')
         else:
-            click.echo(f'No packages to update. Deployment complete!')
+            print(f'No packages to update. Deployment complete!')
 
 @app.command("package")
 def function_package():
-    click.echo('Resolving any requirements from requirements.txt...')
+    print('Resolving any requirements from requirements.txt...')
     requirements = utils.parseRequirements()
     pack_dir: str = None
     if requirements:
-        click.echo('Comparing provided packages from Snowflake Anaconda...')
+        print('Comparing provided packages from Snowflake Anaconda...')
         parsedRequirements = utils.parseAnacondaPackages(requirements)
         if not parsedRequirements['other']:
-            click.echo('No packages to manually resolve')
+            print('No packages to manually resolve')
         if parsedRequirements['other']:
-            click.echo('Writing requirements.other.txt...')
+            print('Writing requirements.other.txt...')
             with open('requirements.other.txt', 'w') as f:
                 for package in parsedRequirements['other']:
                     f.write(package + '\n')
         # if requirements.other.txt exists
         if os.path.isfile('requirements.other.txt'):
             if click.confirm('Do you want to try to download non-Anaconda packages?', default=True):
-                click.echo('Installing non-Anaconda packages...')
+                print('Installing non-Anaconda packages...')
                 if utils.installPackages('requirements.other.txt'):
                     pack_dir = 'packages'
         # write requirements.snowflake.txt file
         if parsedRequirements['snowflake']:
-            click.echo('Writing requirements.snowflake.txt file...')
+            print('Writing requirements.snowflake.txt file...')
             with open('requirements.snowflake.txt', 'w') as f:
                 for package in parsedRequirements['snowflake']:
                     f.write(package + '\n')
@@ -201,21 +200,7 @@ def function_package():
             utils.standardZipDir('app.zip')
     else:
         utils.standardZipDir('app.zip')
-    click.echo('\n\nDeployment package now ready: app.zip')
-
-@click.command("list")
-def function_list():
-    click.echo('Not yet implemented...')
-
-
-@click.command("delete")
-def function_delete():
-    click.echo('Not yet implemented...')
-
-
-@click.command("logs")
-def function_logs():
-    click.echo('Not yet implemented...')
+    print('\n\nDeployment package now ready: app.zip')
 
 @app.command("execute")
 def function_execute(environment: str = EnvironmentOption,
@@ -228,7 +213,7 @@ def function_execute(environment: str = EnvironmentOption,
         config.connectToSnowflake()
         results = config.snowflake_connection.executeFunction(
             function=function, database=env_conf['database'], schema=env_conf['schema'], role=env_conf['role'], warehouse=env_conf['warehouse'])
-        click.echo(results)
+        print(results)
 
 
 @app.command("describe")
