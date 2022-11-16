f1826af7e567fb47a67277c157a7cba4a6a974ae
Mark Keller <mark.keller@snowflake.com>
2022-11-16 00:38:35 -0800

    adding very basic test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,24 @@
+from __future__ import annotations
+
+
+import functools
 import pytest
+from typer import Typer
+
+from snowcli.cli import app
 from typer.testing import CliRunner
 
-from snowcli import app
+
+class SnowCLIRunner(CliRunner):
+    def __init__(self, app: Typer):
+        super().__init__()
+        self.app = app
+
+    @functools.wraps(CliRunner.invoke)
+    def invoke(self, *a, **kw):
+        return super().invoke(self.app, *a, **kw)
+
 
 @pytest.fixture
 def runner():
-    return CliRunner(app)
-
+    return SnowCLIRunner(app)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 """These tests verify that the CLI runs work as expected."""
+from __future__ import annotations
 
 
 def test_help_option(runner):
