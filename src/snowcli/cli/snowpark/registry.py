0c7f43c85010beab4203aa0590d64dc9549c9b21
Mihir Sathe <mihir.sathe@snowflake.com>
2023-04-26 17:23:17 -0700

    Make registry token compatible with the docker CLI

--- src/snowcli/cli/snowpark/registry.py
+++ src/snowcli/cli/snowpark/registry.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+import sys
 
 import typer
 from rich import print
@@ -37,4 +38,4 @@ def get_token(
             "token": token_data["data"]["sessionToken"],
             "expires_in": token_data["data"]["validityInSecondsST"],
         }
-        print("Session token for connection : \n", json.dumps(token))
+        sys.stdout.write(json.dumps(token))
