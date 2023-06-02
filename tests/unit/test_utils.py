b4912c9dc9122ce1371adbd5b082d8fada8d1441
Jan Sikorski <jan.sikorski@snowflake.com>
2023-06-02 12:35:44 +0200

    fixed type annotations

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from shutil import rmtree
-from typing import Generator
+from typing import Generator, Tuple
 from zipfile import ZipFile
 
 import pytest
@@ -44,7 +44,7 @@ class TestUtils:
 
     @pytest.mark.parametrize("arguments", positive_arguments_for_deploy_names)
     def test_get_deploy_names_correct(
-        self, arguments: tuple[tuple[str, str, str], dict]
+        self, arguments: Tuple[Tuple[str, str, str], dict]
     ):
         result = utils.get_deploy_names(*arguments[0])
 
