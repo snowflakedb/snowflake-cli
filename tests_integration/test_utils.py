507eb8bbdc89082d8819a807030227135ca306ea
Adam Stus <adam.stus@snowflake.com>
2023-07-14 11:39:43 +0200

    Integration tests for snow stage (#228)

--- tests_integration/test_utils.py
+++ tests_integration/test_utils.py
@@ -2,16 +2,36 @@ from typing import List, Dict
 from snowflake.connector.cursor import SnowflakeCursor
 
 
-def extract_data(mock_print) -> List[Dict[str, str]]:
-    return extract_data_from_cursor(mock_print.call_args.args[0])
+def row_from_mock(mock_print) -> List[Dict[str, str]]:
+    return row_from_cursor(mock_print.call_args.args[0])
 
 
-def extract_list_of_data(mock_print) -> List[List[Dict[str, str]]]:
-    return [
-        extract_data_from_cursor(args.args[0]) for args in mock_print.call_args_list
-    ]
+def rows_from_mock(mock_print) -> List[List[Dict[str, str]]]:
+    return [row_from_cursor(args.args[0]) for args in mock_print.call_args_list]
 
 
-def extract_data_from_cursor(cursor: SnowflakeCursor) -> List[Dict[str, str]]:
+def row_from_snowflake_session(
+    result: List[SnowflakeCursor],
+) -> List[Dict[str, str]]:
+    return row_from_cursor(result[-1])
+
+
+def row_from_cursor(cursor: SnowflakeCursor) -> List[Dict[str, str]]:
     column_names = [column.name for column in cursor.description]
     return [dict(zip(column_names, row)) for row in cursor.fetchall()]
+
+
+def contains_row_with(rows: List[Dict[str, str]], values: Dict[str, str]) -> bool:
+    values_items = values.items()
+    for row in rows:
+        if row.items() >= values_items:
+            return True
+    return False
+
+
+def not_contains_row_with(rows: List[Dict[str, str]], values: Dict[str, str]) -> bool:
+    values_items = values.items()
+    for row in rows:
+        if row.items() >= values_items:
+            return False
+    return True
