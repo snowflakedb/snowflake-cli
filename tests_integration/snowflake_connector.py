af9dfa69077e61db1c96d658c478453944e16ad1
Adam Stus <adam.stus@snowflake.com>
2023-07-17 10:36:24 +0200

    Revert "Create new database for each run of integration tests"
    This reverts commit 2b06e9804ee0e0381eb8f5f9322820663ede1f6e.

--- tests_integration/snowflake_connector.py
+++ tests_integration/snowflake_connector.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import os
 import pytest
-import uuid
 
 from snowflake import connector
 from snowcli.exception import EnvironmentVariableNotFoundError
@@ -11,41 +10,19 @@ _ENV_PARAMETER_PREFIX = "SNOWFLAKE_CONNECTIONS_INTEGRATION_"
 
 
 @pytest.fixture(scope="session")
-def create_database():
-    database_name = f"db_{uuid.uuid4().hex}"
-
+def snowflake_session():
     config = {
         "application": "INTEGRATION_TEST",
         "account": _get_from_env("ACCOUNT", True),
         "user": _get_from_env("USER", True),
         "password": _get_from_env("PASSWORD", True),
         "host": _get_from_env("HOST", True),
-        "role": _get_from_env("ROLE", True),
-    }
-    config = {k: v for k, v in config.items() if v is not None}
-    connection = connector.connect(**config)
-
-    connection.execute_string(f"create database {database_name}")
-    os.environ[f"{_ENV_PARAMETER_PREFIX}DATABASE"] = database_name
-
-    yield database_name
-
-    connection.execute_string(f"drop database {database_name}")
-    del os.environ[f"{_ENV_PARAMETER_PREFIX}DATABASE"]
-
-
-@pytest.fixture(scope="session")
-def snowflake_session(create_database):
-    config = {
-        "application": "INTEGRATION_TEST",
-        "account": _get_from_env("ACCOUNT", True),
-        "user": _get_from_env("USER", True),
-        "password": _get_from_env("PASSWORD", True),
-        "host": _get_from_env("HOST", True),
-        "database": _get_from_env("DATABASE", True),
-        "role": _get_from_env("ROLE", True),
+        "port": _get_from_env("PORT", True),
+        "protocol": _get_from_env("PROTOCOL", True),
+        "db": _get_from_env("DATABASE", False),
         "schema": _get_from_env("SCHEMA", False),
         "warehouse": _get_from_env("WAREHOUSE", False),
+        "role": _get_from_env("ROLE", False),
     }
     config = {k: v for k, v in config.items() if v is not None}
     connection = connector.connect(**config)
