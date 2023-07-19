90e7c99d6c7b0f4ef4f9fe59bcec44c54de66f4b
sfc-gh-mrojas <125514396+sfc-gh-mrojas@users.noreply.github.com>
2023-07-19 00:38:55 +0000

    adding additional options to avoid errors when reading options from config file

--- src/snowcli/connection_params.py
+++ src/snowcli/connection_params.py
@@ -6,8 +6,12 @@ class ConnectionParams:
     user: str
     password: str
     account: str
-    host: str = "snowflakecomputing.com"
+    host: str = 'snowflakecomputing.com'
     port: int = 443
-    protocol: str = "https"
+    protocol: str = 'https'
+    warehouse:str = None
+    role:str = None
+    schema:str = None
+    db:str = None
     # add a new parameter to support registry login
     session_parameters: str = '{"PYTHON_CONNECTOR_QUERY_RESULT_FORMAT": "json"}'
