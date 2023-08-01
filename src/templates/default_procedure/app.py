0a82cec886e9fe4454bceb47241ef2e15b8f4e6a
Chris Conner <chris.conner@snowflake.com>
2023-08-01 10:23:47 -0400

    Fixed the session issue and the config issue:
    https://github.com/Snowflake-Labs/snowcli/issues/285

--- src/templates/default_procedure/app.py
+++ src/templates/default_procedure/app.py
@@ -14,9 +14,9 @@ def hello(session: Session) -> str:
 if __name__ == "__main__":
     from snowcli.config import cli_config
 
-    session = Session.builder.configs(**cli_config.get_connection("dev")).create()
+    session = Session.builder.configs(cli_config.get_connection("dev")).create()
     if len(sys.argv) > 1:
-        print(hello(*sys.argv[1:]))  # type: ignore
+        print(hello(session, *sys.argv[1:]))  # type: ignore
     else:
-        print(hello())  # type: ignore
+        print(hello(session))  # type: ignore
     session.close()
