39383df0bfb5391e32e4407c2d2b3eed1b9ab291
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2023-08-01 11:32:05 +0200

    Generate usage documentation automatically (#270)
    * Generate usage documentation automatically
    
    * Add unit tests validating that all commands has properly documented options
    
    * fixup! Add unit tests validating that all commands has properly documented options
    
    * fixup! fixup! Add unit tests validating that all commands has properly documented options

---
+++ src/snowcli/docs/generator.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import logging
+from pathlib import Path
+from typing import Optional, List
+
+from click import Command
+from jinja2 import FileSystemLoader, Environment
+from typer.core import TyperArgument
+
+log = logging.getLogger(__name__)
+
+CMD_USAGE_TMPL = "usage.rst.jinja2"
+OVERVIEW_TMPL = "overview.rst.jinja2"
+
+
+def generate_docs(root: Path, command: Command, cmd_parts: Optional[List] = None):
+    """
+    Iterates recursively through commands info. Creates a file structure resembling
+    commands structure. For each terminal command creates a "usage" rst file.
+    """
+    if getattr(command, "hidden", False):
+        return
+
+    root.mkdir(exist_ok=True)
+    if cmd_parts is None:
+        _render_usage(command, root, cmd_parts, template_name=OVERVIEW_TMPL)
+
+    cmd_parts = cmd_parts or []
+    if hasattr(command, "commands"):
+        for command_name, command_info in command.commands.items():
+            generate_docs(root / command_name, command_info, [*cmd_parts, command_name])
+    else:
+        _render_usage(command, root, cmd_parts)
+
+
+def _render_usage(
+    command: Command,
+    root: Path,
+    path: Optional[List] = None,
+    template_name: str = CMD_USAGE_TMPL,
+):
+    # This is end command
+    command_name = command.name
+    env = Environment(loader=FileSystemLoader(Path(__file__).parent / "templates"))
+    template = env.get_template(template_name)
+    arguments = []
+    options = []
+    for param in command.params:
+        if isinstance(param, TyperArgument):
+            arguments.append(param)
+        else:
+            options.append(param)
+    file_path = root / f"usage-{command_name}.rst"
+    log.info("Creating %s", file_path)
+    with open(file_path, "w+") as fh:
+        fh.write(
+            template.render(
+                {
+                    "help": command.help,
+                    "name": command_name,
+                    "options": options,
+                    "arguments": arguments,
+                    "path": path,
+                }
+            )
+        )
