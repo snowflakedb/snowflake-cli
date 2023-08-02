79ecd3df7cb438b539e021656eb6abf24910a6d9
Jan Sikorski <jan.sikorski@snowflake.com>
2023-08-02 21:17:10 +0200

    fix for package commands

--- tests/test_package.py
+++ tests/test_package.py
@@ -28,7 +28,7 @@ class TestPackage:
         ],
     )
     @patch("tests.test_package.package.utils.requests")
-    def test_package_lookup(self, mock_requests, caplog, argument, monkeypatch):
+    def test_package_lookup(self, mock_requests, caplog, argument, monkeypatch, runner):
         mock_requests.get.return_value = self.mocked_anaconda_response(
             test_data.anaconda_response
         )
@@ -36,15 +36,16 @@ class TestPackage:
         monkeypatch.setattr("sys.stdin", io.StringIO("N"))
 
         with caplog.at_level(logging.DEBUG, logger=argument[2]):
-            result = package.package_lookup(argument[0], install_packages=True)
+            result = runner.invoke(["snowpark", "package", "lookup", argument[0], "--yes"])
 
+        assert result.exit_code == 0
         assert caplog.text
         assert argument[1] in caplog.text
 
     @patch("tests.test_package.package.utils.install_packages")
     @patch("tests.test_package.package.utils.parse_anaconda_packages")
     def test_package_lookup_with_install_packages(
-        self, mock_package, mock_install, caplog
+        self, mock_package, mock_install, caplog, runner
     ):
         mock_package = MagicMock(return_value=SplitRequirements([], []))
         mock_install.return_value = (
@@ -56,8 +57,9 @@ class TestPackage:
         )
 
         with caplog.at_level(logging.DEBUG, logger="snowcli.cli.snowpark.package"):
-            result = package.package_lookup("some-other-package", install_packages=True)
+            result = runner.invoke(["snowpark", "package", "lookup", "some-other-package", "--yes"])
 
+        assert result.exit_code == 0
         assert (
             "The package some-other-package is supported, but does depend on the following Snowflake supported native "
             'libraries. You should include the following in your packages: [<Requirement: "snowflake-snowpark-python">]'
@@ -65,15 +67,16 @@ class TestPackage:
         )
 
     @patch("tests.test_package.package.utils.requests")
-    def test_package_create(self, mock_requests, caplog, packages_directory):
+    def test_package_create(self, mock_requests, caplog, packages_directory, runner):
         mock_response = MagicMock()
         mock_response.status_code = 200
         mock_response.json.return_value = test_data.anaconda_response
         mock_requests.get.return_value = mock_response
 
         with caplog.at_level(logging.DEBUG, logger="snowcli.cli.snowpark.package"):
-            result = package.package_create("totally-awesome-package")
+            result = runner.invoke(["snowpark", "package", "create", "totally-awesome-package"])
 
+        assert result.exit_code == 0
         assert (
             f"Package totally-awesome-package.zip created. You can now upload it to a stage (`snow package upload -f totally-awesome-package.zip -s packages`) and reference it in your procedure or function."
             in caplog.text
