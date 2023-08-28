bfbb81625d3a2084b8eba7222029154a614d6a09
Zachary Blackwood <zachary.blackwood@snowflake.com>
2023-08-28 09:37:34 -0400

    Move test, handle /private/ vs /private/var/

--- tests/test_stage.py
+++ tests/test_stage.py
@@ -38,6 +38,13 @@ def test_stage_get_default_path(mock_execute, runner, mock_cursor):
     )
 
 
+def _resolve_tmp_dir(tmp_dir):
+    raw_path = str(Path(tmp_dir).resolve())
+    if raw_path.startswith("/private/"):
+        return raw_path.replace("/private/", "/")
+    return raw_path
+
+
 @mock.patch(f"{STAGE_MANAGER}._execute_query")
 def test_stage_put(mock_execute, runner, mock_cursor):
     mock_execute.return_value = mock_cursor(["row"], [])
@@ -57,7 +64,30 @@ def test_stage_put(mock_execute, runner, mock_cursor):
         )
     assert result.exit_code == 0, result.output
     mock_execute.assert_called_once_with(
-        f"put file://{Path(tmp_dir).resolve()}/* @stageName auto_compress=false parallel=42 overwrite=True"
+        f"put file://{_resolve_tmp_dir(tmp_dir)}/* @stageName auto_compress=false parallel=42 overwrite=True"
+    )
+
+
+@mock.patch(f"{STAGE_MANAGER}._execute_query")
+def test_stage_put_star(mock_execute, runner, mock_cursor):
+    mock_execute.return_value = mock_cursor(["row"], [])
+    with TemporaryDirectory() as tmp_dir:
+        result = runner.invoke_with_config(
+            [
+                "stage",
+                "put",
+                "-c",
+                "empty",
+                "--overwrite",
+                "--parallel",
+                42,
+                str(tmp_dir) + "/*.py",
+                "stageName",
+            ]
+        )
+    assert result.exit_code == 0, result.output
+    mock_execute.assert_called_once_with(
+        f"put file://{_resolve_tmp_dir(tmp_dir)}/*.py @stageName auto_compress=false parallel=42 overwrite=True"
     )
 
 
@@ -85,26 +115,3 @@ def test_stage_remove(mock_execute, runner, mock_cursor):
     )
     assert result.exit_code == 0, result.output
     mock_execute.assert_called_once_with("remove @stageName/my/file/foo.csv")
-
-
-@mock.patch(f"{STAGE_MANAGER}._execute_query")
-def test_stage_put_star(mock_execute, runner, mock_cursor):
-    mock_execute.return_value = mock_cursor(["row"], [])
-    with TemporaryDirectory() as tmp_dir:
-        result = runner.invoke_with_config(
-            [
-                "stage",
-                "put",
-                "-c",
-                "empty",
-                "--overwrite",
-                "--parallel",
-                42,
-                str(tmp_dir) + "/*.py",
-                "stageName",
-            ]
-        )
-    assert result.exit_code == 0, result.output
-    mock_execute.assert_called_once_with(
-        f"put file://{Path(tmp_dir).resolve()}/*.py @stageName auto_compress=false parallel=42 overwrite=True"
-    )
