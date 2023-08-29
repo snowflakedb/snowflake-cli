84c5749c43da8356b58e03725b1c7438e910d544
Adam Stus <adam.stus@snowflake.com>
2023-08-29 16:06:50 +0200

    Added missing decorators for commands, refactor procedure coverage commands

--- src/snowcli/cli/snowpark/procedure_coverage/__init__.py
+++
@@ -1,9 +0,0 @@
-import typer
-
-from snowcli.cli.common.flags import DEFAULT_CONTEXT_SETTINGS
-
-app: typer.Typer = typer.Typer(
-    name="coverage", context_settings=DEFAULT_CONTEXT_SETTINGS
-)
-
-from snowcli.cli.snowpark.procedure_coverage import clear, report
