87b809510d9655492e7c26a49817293138b3abbe
Paweł Job <pawel.job@snowflake.com>
2023-10-04 11:43:53 +0200

    [v1.1.1] Removing option duplicates (backward incompatible way) (#438)
    * Checking option duplicates (v1.1.1) (Cherry-pick from main branch)
    * Removing option duplicates from commands (v1.1.1)

--- src/snowcli/app/dev/commands_structure.py
+++ src/snowcli/app/dev/commands_structure.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+import json
 from dataclasses import dataclass, field
-from typing import Dict
+from typing import Dict, Optional, List
 
 from click import Command
 
@@ -11,11 +12,26 @@ class _Node:
     name: str
     children: Dict[str, _Node] = field(default_factory=dict)
     level: int = 0
+    options: Dict[str, List[str]] = field(default_factory=dict)
 
     def print(self):
         print("    " * self.level, self.name)
         for ch in self.children.values():
             ch.print()
+
+    def print_with_options(self):
+        options = self._prepare_options_dict()
+        pretty = json.dumps(options, indent=4)
+        print(pretty)
+
+    def _prepare_options_dict(self, options_dict={}):
+        options_dict["options"] = self.options
+
+        for ch in self.children.values():
+            options_dict[ch.name] = ch._prepare_options_dict(
+                options_dict.get(ch.name, {})
+            )
+        return options_dict
 
 
 def generate_commands_structure(command: Command, root: _Node | None = None):
@@ -25,6 +41,11 @@ def generate_commands_structure(command: Command, root: _Node | None = None):
     """
     if not root:
         root = _Node(name="snow")
+
+    if command.params:
+        root.options = {
+            param.human_readable_name: param.opts for param in command.params
+        }
 
     if hasattr(command, "commands"):
         for command_name, command_info in command.commands.items():
