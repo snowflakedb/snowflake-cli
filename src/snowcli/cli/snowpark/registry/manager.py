df08469c68dc263bd13f847f6fba59e0764c5cef
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2023-10-06 17:16:50 +0200

    Fix registry token command

--- src/snowcli/cli/snowpark/registry/manager.py
+++ src/snowcli/cli/snowpark/registry/manager.py
@@ -8,11 +8,13 @@ def get_token():
     Get token to authenticate with registry.
     """
     conn = snow_cli_global_context_manager.get_connection()
-
+    conn.execute_string(
+        "alter session set PYTHON_CONNECTOR_QUERY_RESULT_FORMAT = 'json'"
+    )
     # disable session deletion
     conn._all_async_queries_finished = lambda: False
     if conn._rest is None:
-        raise Exception("error in connection object")
+        raise Exception("Failed to connect to Snowflake to retrieve token.")
     # obtain and create the token
     token_data = conn._rest._token_request("ISSUE")
 
