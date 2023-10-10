6e8742854c1d30ef34568a14704eadba35dcb84b
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2023-10-10 11:37:20 +0200

    Suppress python connector output to support proper json formatting (#458)

--- tests/test_snow_connector.py
+++ tests/test_snow_connector.py
@@ -1,7 +1,6 @@
 import json
 import pytest
 
-from snowcli.snow_connector import connect_to_snowflake
 from tests.testing_utils.fixtures import *
 from unittest import mock
 
@@ -33,6 +32,8 @@ MOCK_CONNECTION = {
 def test_command_context_is_passed_to_snowflake_connection(
     mock_click, mock_connect, mock_cli_config, runner, cmd, expected, mock_cursor
 ):
+    from snowcli.snow_connector import connect_to_snowflake
+
     mock_ctx = mock.Mock()
     mock_ctx.command_path = cmd
     mock_click.get_current_context.return_value = mock_ctx
@@ -49,3 +50,17 @@ def test_returns_nice_error_in_case_of_connectivity_error(runner):
     assert result.exit_code == 1
     assert "Invalid connection configuration" in result.output
     assert "User is empty" in result.output
+
+
+@mock.patch("snowflake.connector")
+def test_no_output_from_connection(mock_connect, runner):
+    funny_text = "what's taters, my precious?"
+
+    def _mock(*args, **kwargs):
+        print(funny_text)
+        return mock.MagicMock()
+
+    mock_connect.connect = _mock
+
+    result = runner.invoke(["sql", "-q", "select 1"])
+    assert funny_text not in result.output
