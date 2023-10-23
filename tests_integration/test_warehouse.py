8bfcf4ae3bc61d04473e8a098d9acd324f9676f2
Jan Sikorski <jan.sikorski@snowflake.com>
2023-10-23 10:34:37 +0200

    Fixes after review

--- tests_integration/test_warehouse.py
+++ tests_integration/test_warehouse.py
@@ -4,7 +4,7 @@ from tests_integration.test_utils import row_from_cursor
 from tests_integration.snowflake_connector import snowflake_session
 
 
-# @pytest.mark.integration TODO: remove hash
+@pytest.mark.integration
 def test_warehouse_status_query(runner, snowflake_session):
     result = runner.invoke_integration(
         ["object", "show", "warehouses", "--format", "json"]
