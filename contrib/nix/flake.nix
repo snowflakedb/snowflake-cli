0a90f19074c2ca4437595acc71e3479b43a3bb65
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2023-10-25 13:28:01 -0700

    Nix flake: bump version and create live package
    As of this commit the flake provides two packages:
    
    * `snowcli` -- tracks live version of the package, getting src from the same repository
    * `snowcli-stable` -- tracks last released version pinned via git tag

--- contrib/nix/flake.nix
+++ contrib/nix/flake.nix
@@ -2,12 +2,20 @@
   description = "Flake that provides snowcli";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    src = {
-      url = "github:Snowflake-Labs/snowcli?ref=v1.1.0";  # Unpin if needed
+    src-stable = {
+      url = "github:Snowflake-Labs/snowcli?ref=v1.1.1-rc3"; # Pins to last stable version tag by hand
+      flake = false;
+    };
+    src-live = {
+      url = "github:Snowflake-Labs/snowcli"; # Tracks live version
       flake = false;
     };
     snowflake-connector-python-src = {
-      url = "github:snowflakedb/snowflake-connector-python?ref=v3.2.0";  # Unpin if needed
+      url = "github:snowflakedb/snowflake-connector-python?ref=v3.2.0"; # Unpin if needed
+      flake = false;
+    };
+    snowflake-connector-python-src-live = {
+      url = "github:snowflakedb/snowflake-connector-python?ref=v3.3.1"; # Unpin if needed
       flake = false;
     };
   };
@@ -26,53 +34,76 @@
           # system.
 
           formatter = pkgs.nixpkgs-fmt;
-          packages = rec {
-            default = snowcli;
-            flake-snowflake-connector-python = python3.pkgs.snowflake-connector-python.overrideAttrs (
-              finalAttrs: previousAttrs: {
-                src = inputs.snowflake-connector-python-src;
-                propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++
-                  (builtins.attrValues { inherit (python3.pkgs) sortedcontainers packaging platformdirs tomlkit keyring; });
-              }
-            );
-            snowcli = python3.pkgs.buildPythonApplication {
-              pname = "snowflake-cli-labs";
-              version = "1.1.0";
-              format = "pyproject";
-              inherit (inputs) src;
+          packages =
+            let
+              mkSnowflakeConnectoPython = src: python3.pkgs.snowflake-connector-python.overrideAttrs
+                (
+                  finalAttrs: previousAttrs: {
+                    inherit src;
+                    propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++
+                      (builtins.attrValues { inherit (python3.pkgs) sortedcontainers packaging platformdirs tomlkit keyring; });
+                  }
+                );
+              mkSnowCli = { version, snowflakeConnectorPkg, patchFile, src }: python3.pkgs.buildPythonApplication {
+                pname = "snowflake-cli-labs";
+                inherit version src;
+                format = "pyproject";
 
-              patches = [ ./pyproject.patch ];
+                patches = [ patchFile ];
 
-              # NOTE: for debugging purposes, to see effect of patch application
-              # postPatch = ''
-              #   cat pyproject.toml
-              # '';
+                # NOTE: for debugging purposes, to see effect of patch application
+                # postPatch = ''
+                #   cat pyproject.toml
+                # '';
 
-              nativeBuildInputs = with python3.pkgs; [
-                hatch-vcs
-                hatchling
-              ];
-              propagatedBuildInputs = with python3.pkgs; [
-                coverage
-                jinja2
-                rich
-                requests
-                requirements-parser
-                strictyaml
-                tomlkit
-                typer
-                chardet # needed by snowflake-connector-python
-                urllib3
-                gitpython
-              ] ++ [ flake-snowflake-connector-python ];
-              meta = {
-                mainProgram = "snow";
-                description = "Snowflake CLI";
-                homepage = "https://github.com/Snowflake-Labs/snowcli";
-                license = nixpkgs.lib.licenses.asl20;
+                nativeBuildInputs = with python3.pkgs; [
+                  hatch-vcs
+                  hatchling
+                ];
+                propagatedBuildInputs = with python3.pkgs; [
+                  coverage
+                  jinja2
+                  rich
+                  requests
+                  requirements-parser
+                  strictyaml
+                  tomlkit
+                  typer
+                  chardet # needed by snowflake-connector-python
+                  urllib3
+                  gitpython
+                  pluggy
+                  pyyaml
+                ] ++ [ snowflakeConnectorPkg ];
+                meta = {
+                  mainProgram = "snow";
+                  description = "Snowflake CLI";
+                  homepage = "https://github.com/Snowflake-Labs/snowcli";
+                  license = nixpkgs.lib.licenses.asl20;
+                };
+              };
+            in
+            rec {
+              default = snowcli;
+              # Pinned version
+              flake-snowflake-connector-python = mkSnowflakeConnectoPython inputs.snowflake-connector-python-src;
+              # Live version
+              flake-snowflake-connector-python-live = mkSnowflakeConnectoPython inputs.snowflake-connector-python-src-live;
+              # This is the live version
+              snowcli = mkSnowCli {
+                version = "2.0.0-dev";
+                snowflakeConnectorPkg = flake-snowflake-connector-python-live;
+                patchFile = ./patches/pyprojectLive.patch;
+                src = inputs.src-live;
+              };
+              # This is last released version
+              snowcli-stable = mkSnowCli {
+                version = "1.1.1";
+                snowflakeConnectorPkg = flake-snowflake-connector-python;
+                patchFile = ./patches/pyproject.patch;
+                src = inputs.src-stable;
               };
             };
-          };
         };
     };
 }
