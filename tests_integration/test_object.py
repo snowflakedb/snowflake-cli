c759de069041fa0f1aed5e69a78d673d2623575b
Jan Sikorski <jan.sikorski@snowflake.com>
2023-11-02 15:56:54 +0100

    Reformat

--- tests_integration/test_object.py
+++ tests_integration/test_object.py
@@ -24,30 +24,32 @@ def test_show(object_type, runner, snowflake_session):
 
 @pytest.mark.integration
 def test_object_table(runner, test_database, snowflake_session):
-    object_name = ObjectNameProvider("Test_Object_Table").create_and_get_next_object_name()
+    object_name = ObjectNameProvider(
+        "Test_Object_Table"
+    ).create_and_get_next_object_name()
     snowflake_session.execute_string(f"create table {object_name} (some_number NUMBER)")
 
     result_show = runner.invoke_integration(["object", "show", "table"])
     assert result_show.exit_code == 0
 
-    actual_tables = row_from_cursor(snowflake_session.execute_string(f"show tables")[-1])
-    assert result_show.json[0].keys()== actual_tables[0].keys()
+    actual_tables = row_from_cursor(
+        snowflake_session.execute_string(f"show tables")[-1]
+    )
+    assert result_show.json[0].keys() == actual_tables[0].keys()
     assert result_show.json[0]["name"].lower() == object_name.lower()
 
-    result_describe = runner.invoke_integration(["object","describe","table", object_name])
+    result_describe = runner.invoke_integration(
+        ["object", "describe", "table", object_name]
+    )
     assert result_describe.exit_code == 0
     assert result_describe.json[0]["name"] == "SOME_NUMBER"
 
-    result_drop = runner.invoke_integration(["object","drop","table", object_name])
+    result_drop = runner.invoke_integration(["object", "drop", "table", object_name])
     assert result_drop.exit_code == 0
-    assert result_drop.json[0]["status"].lower() == f"{object_name.lower()} successfully dropped."
-    assert len(row_from_cursor(snowflake_session.execute_string(f"show tables")[-1])) == 0
-
-
-
-
-
-
-
-
-
+    assert (
+        result_drop.json[0]["status"].lower()
+        == f"{object_name.lower()} successfully dropped."
+    )
+    assert (
+        len(row_from_cursor(snowflake_session.execute_string(f"show tables")[-1])) == 0
+    )
