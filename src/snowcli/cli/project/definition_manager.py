ac90f82ec09dbbe450b50285b0ecfd80f47ef74f
Bruno Dufour <bruno.dufour@snowflake.com>
2023-11-02 14:47:45 +0000

    Centralize logic to search for project roots (#522)

--- src/snowcli/cli/project/definition_manager.py
+++ src/snowcli/cli/project/definition_manager.py
@@ -29,59 +29,75 @@ class DefinitionManager:
         search_path = os.getcwd()
         if project_arg:
             search_path = os.path.abspath(project_arg)
-        config_files = self._find_definition_files(Path(search_path))
-        if not config_files:
+        project_root = DefinitionManager.find_project_root(Path(search_path))
+        if not project_root:
             raise MissingConfiguration(
                 f"Cannot find project definition (snowflake.yml). Please provide a path to the project or run this command in a valid project directory."
             )
-        self._project_config_paths = config_files
 
-    def _find_definition_files(self, project_path: Path) -> Optional[List[Path]]:
+        self.project_root = project_root
+        self._project_config_paths = self._find_definition_files(self.project_root)
+
+    @staticmethod
+    def _find_definition_files(project_root: Path) -> List[Path]:
+        base_config_file_path = (
+            project_root / DefinitionManager.BASE_DEFINITION_FILENAME
+        )
+        user_config_file_path = DefinitionManager._user_definition_file_if_available(
+            project_root
+        )
+        if user_config_file_path:
+            return [
+                base_config_file_path,
+                user_config_file_path,
+            ]
+        return [base_config_file_path]
+
+    @staticmethod
+    def find_project_root(search_path: Path) -> Optional[Path]:
         """
         Recurse up the directory tree from the given search path until we find
         a directory that contains a snowflake.yml file. We'll stop if we cross
         a filesystem boundary or hit the user's HOME directory.
         """
-        parent_path = project_path
-        starting_mount = _compat_is_mount(project_path)
-        while parent_path:
+        current_path = search_path
+        starting_mount = _compat_is_mount(search_path)
+        while current_path:
             if (
-                _compat_is_mount(project_path) != starting_mount
-                or parent_path.parent == parent_path
-                or parent_path == Path.home()
+                _compat_is_mount(current_path) != starting_mount
+                or current_path.parent == current_path
+                or current_path == Path.home()
             ):
                 return None
 
-            base_config_file_path = self._base_definition_file_if_available(parent_path)
-            if base_config_file_path:
-                user_config_file_path = self._user_definition_file_if_available(
-                    parent_path
-                )
-                self.project_root = parent_path
-                if user_config_file_path:
-                    return [
-                        base_config_file_path,
-                        user_config_file_path,
-                    ]
-                return [base_config_file_path]
+            if (
+                DefinitionManager._base_definition_file_if_available(current_path)
+                is not None
+            ):
+                # found snowflake.yml, end the search here
+                return current_path
 
-            parent_path = parent_path.parent
+            current_path = current_path.parent
+
         return None
 
-    def _definition_if_available(self, filename, project_path: Path) -> Optional[Path]:
+    @staticmethod
+    def _definition_if_available(filename, project_path: Path) -> Optional[Path]:
         file_path = Path(project_path) / filename
         if file_path.is_file():
             return file_path
         return None
 
-    def _base_definition_file_if_available(self, project_path: Path) -> Optional[Path]:
-        return self._definition_if_available(
-            self.BASE_DEFINITION_FILENAME, project_path
+    @staticmethod
+    def _base_definition_file_if_available(project_path: Path) -> Optional[Path]:
+        return DefinitionManager._definition_if_available(
+            DefinitionManager.BASE_DEFINITION_FILENAME, project_path
         )
 
-    def _user_definition_file_if_available(self, project_path: Path) -> Optional[Path]:
-        return self._definition_if_available(
-            self.USER_DEFINITION_FILENAME, project_path
+    @staticmethod
+    def _user_definition_file_if_available(project_path: Path) -> Optional[Path]:
+        return DefinitionManager._definition_if_available(
+            DefinitionManager.USER_DEFINITION_FILENAME, project_path
         )
 
     @functools.cached_property
