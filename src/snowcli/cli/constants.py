c78604c6dc68a52b396ab14d3d8a0b67390c9ba1
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2023-11-27 15:00:23 +0100

    Add support for more commonly used sf objects (#566)

--- src/snowcli/cli/constants.py
+++ src/snowcli/cli/constants.py
@@ -21,15 +21,22 @@ class ObjectType(Enum):
     COMPUTE_POOL = ObjectNames("compute-pool", "compute pool", "compute pools")
     DATABASE = ObjectNames("database", "database", "databases")
     FUNCTION = ObjectNames("function", "function", "functions")
-    JOB = ObjectNames("job", "job", "jobs")
+    INTEGRATION = ObjectNames("integration", "integration", "integrations")
+    # JOB = ObjectNames("job", "job", "jobs")
+    NETWORK_RULE = ObjectNames("network-rule", "network rule", "network rules")
     PROCEDURE = ObjectNames("procedure", "procedure", "procedures")
     ROLE = ObjectNames("role", "role", "roles")
     SCHEMA = ObjectNames("schema", "schema", "schemas")
     SERVICE = ObjectNames("service", "service", "services")
+    SECRET = ObjectNames("secret", "secret", "secrets")
     STAGE = ObjectNames("stage", "stage", "stages")
+    STREAM = ObjectNames("stream", "stream", "streams")
     STREAMLIT = ObjectNames("streamlit", "streamlit", "streamlits")
     TABLE = ObjectNames("table", "table", "tables")
+    TASK = ObjectNames("task", "task", "tasks")
+    USER = ObjectNames("user", "user", "users")
     WAREHOUSE = ObjectNames("warehouse", "warehouse", "warehouses")
+    VIEW = ObjectNames("view", "view", "views")
 
     def __str__(self):
         """This makes using this Enum easier in formatted string"""
@@ -37,5 +44,6 @@ class ObjectType(Enum):
 
 
 OBJECT_TO_NAMES = {o.value.cli_name: o.value for o in ObjectType}
+SUPPORTED_OBJECTS = sorted(OBJECT_TO_NAMES.keys())
 
 PACKAGES_DIR = Path(".packages")
