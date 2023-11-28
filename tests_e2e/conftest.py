e0fd7bb45830782dffb29220952cb9cba3fe4226
Adam Stus <adam.stus@snowflake.com>
2023-11-28 17:41:50 +0100

    Changed version to 2.0.0a2 and fixed e2e tests (#573)

--- tests_e2e/conftest.py
+++ tests_e2e/conftest.py
@@ -61,14 +61,14 @@ def _build_snowcli(venv_path: Path, test_root_path: Path) -> None:
 def _install_snowcli_with_external_plugin(
     venv_path: Path, test_root_path: Path
 ) -> None:
-    whl_file = _find_whl_file(os.listdir(test_root_path / f"../dist/"))
+    version = __about__.VERSION
     subprocess.check_call(
         [
             _python_path(venv_path),
             "-m",
             "pip",
             "install",
-            test_root_path / f"../dist/{whl_file}",
+            test_root_path / f"../dist/snowflake_cli_labs-{version}-py3-none-any.whl",
         ]
     )
     subprocess.check_call(
@@ -87,10 +87,3 @@ def _install_snowcli_with_external_plugin(
 
 def _python_path(venv_path: Path) -> Path:
     return venv_path / "bin" / "python"
-
-
-def _find_whl_file(files: List[str]) -> str:
-    for file in files:
-        if file.endswith(".whl"):
-            return file
-    raise RuntimeError(f"Not found whl file in: {files}")
