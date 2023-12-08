8e34cb570be9e8bf59d4dea9ff701a2221ca149c
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2023-12-08 15:37:57 +0100

    fixup! fixup! Use -i for stdin in snow sql

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -69,13 +69,6 @@ def test_sql_fails_if_query_and_stdin_provided(runner):
     )
 
 
-def test_sql_fails_for_both_query_and_file(runner):
-    with NamedTemporaryFile("r") as tmp_file:
-        result = runner.invoke(["sql", "-f", tmp_file.name, "-q", "query"])
-
-    assert_that_result_is_usage_error(result, "Both query and file provided")
-
-
 @mock.patch("snowcli.cli.common.cli_global_context.connect_to_snowflake")
 def test_sql_overrides_connection_configuration(mock_conn, runner, mock_cursor):
     mock_conn.return_value.execute_string.return_value = [mock_cursor(["row"], [])]
