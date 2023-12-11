6ef58102bb87a155d58a1dfabdac903205994271
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2023-12-11 12:22:08 +0100

    Use -i for stdin in snow sql (#592)

--- tests/testing_utils/result_assertions.py
+++ tests/testing_utils/result_assertions.py
@@ -4,7 +4,7 @@ from click.testing import Result
 def assert_that_result_is_usage_error(
     result: Result, expected_error_message: str
 ) -> None:
-    assert result.exit_code == 2
-    assert expected_error_message in result.output
+    assert result.exit_code == 2, result.exit_code
+    assert expected_error_message in result.output, result.output
     assert isinstance(result.exception, SystemExit)
     assert "traceback" not in result.output.lower()
