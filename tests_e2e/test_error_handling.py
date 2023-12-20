7e9cccf5a0795c7a774ef624c308316cfa90a723
Patryk Czajka <patryk.czajka@snowflake.com>
2023-12-20 15:53:39 +0100

    Snow 818820 early detect debug flag (#621)
    * Trigger DebugOption logic directly after the app() call
    
    * precommit formatting
    
    * add setuptools to e2e installation script
    
    * e2e tests
    
    * Parse all representations of --debug flag
    
    * review fix

---
+++ tests_e2e/test_error_handling.py
@@ -0,0 +1,37 @@
+import subprocess
+
+import pytest
+
+
+@pytest.mark.e2e
+def test_error_traceback_disabled_without_debug(snowcli, test_root_path):
+    traceback_msg = "Traceback (most recent call last)"
+
+    result = subprocess.run(
+        [
+            snowcli,
+            "--config-file",
+            test_root_path / "config" / "malformatted_config.toml",
+            "sql",
+            "-q",
+            "select 'Hello there'",
+        ],
+        capture_output=True,
+        text=True,
+    )
+    assert result.stderr == "" and not traceback_msg in result.stdout
+
+    result_debug = subprocess.run(
+        [
+            snowcli,
+            "--config-file",
+            test_root_path / "config" / "malformatted_config.toml",
+            "sql",
+            "-q",
+            "select 'Hello there'",
+            "--debug",
+        ],
+        capture_output=True,
+        text=True,
+    )
+    assert traceback_msg in result_debug.stderr
