46ae7c47f7cd8c1ed9dafe2bf3829ba00d62c537
Patryk Czajka <patryk.czajka@snowflake.com>
2023-12-21 14:49:48 +0100

    Create file handler

--- src/snowcli/cli/loggers.py
+++ src/snowcli/cli/loggers.py
@@ -1,9 +1,15 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
 
 import typer
 
 DEFAULT_LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s"
 DEBUG_LOG_FORMAT = "%(asctime)s %(levelname)s [%(name)s] %(message)s"
+FILE_LOG_FORMAT = "%(asctime)s %(levelname)s [%(name)s] %(message)s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+PCZAJKA_PATH = Path(__file__).parents[4] / "logs" / "snowcli.log"  # DEBUG
 
 
 def create_loggers(verbose: bool, debug: bool):
@@ -26,7 +32,10 @@ def create_loggers(verbose: bool, debug: bool):
         log_level = logging.ERROR
 
     console = logging.StreamHandler()
-    console.setFormatter(logging.Formatter(log_format, "%Y-%m-%d %H:%M:%S"))
+    console.setFormatter(logging.Formatter(log_format, DATE_FORMAT))
     logger.addHandler(console)
+    file = TimedRotatingFileHandler(filename=PCZAJKA_PATH, when="midnight")
+    file.setFormatter(logging.Formatter(FILE_LOG_FORMAT, DATE_FORMAT))
+    logger.addHandler(file)
     logger.setLevel(log_level)
     logging.getLogger("snowflake").setLevel(log_level)
