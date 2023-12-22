b6d5416667fb713f68fa6ba56693f1be7d6f1c43
Jan Sikorski <jan.sikorski@snowflake.com>
2023-12-22 16:39:12 +0100

    reformat

--- tests_e2e/test_error_handling.py
+++ tests_e2e/test_error_handling.py
@@ -6,7 +6,10 @@ import pytest
 @pytest.mark.e2e
 def test_error_traceback_disabled_without_debug(snowcli, test_root_path):
     traceback_msg = "Traceback (most recent call last)"
-
+    import os  # TODO remove thise line and three below
+    import stat
+    READABLE_BY_OTHERS = stat.S_IRGRP | stat.S_IROTH
+    print(f"*******FILE CHECK: {os.stat(test_root_path / 'config' / 'malformatted_config.toml') & READABLE_BY_OTHERS}")
     result = subprocess.run(
         [
             snowcli,
