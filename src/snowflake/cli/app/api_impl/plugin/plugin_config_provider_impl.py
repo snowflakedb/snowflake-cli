811e3076f89638871bff33c879ab383f9610b2a8
Pawel Job <pawel.job@snowflake.com>
2024-01-23 10:17:10 +0100

    [SNOW-1016326] Rename snowcli -> cli in configuration

--- src/snowflake/cli/app/api_impl/plugin/plugin_config_provider_impl.py
+++ src/snowflake/cli/app/api_impl/plugin/plugin_config_provider_impl.py
@@ -2,27 +2,25 @@ from typing import List
 
 from snowflake.cli.api import PluginConfigProvider
 from snowflake.cli.api.config import (
+    PLUGINS_SECTION_PATH,
     config_section_exists,
     get_config_section,
     get_config_value,
+    get_plugins_config,
 )
 from snowflake.cli.api.plugins.plugin_config import PluginConfig
 
 
 class PluginConfigProviderImpl(PluginConfigProvider):
     def get_enabled_plugin_names(self) -> List[str]:
-        config_path = ["snowcli", "plugins"]
         enabled_plugins = []
-        if config_section_exists(*config_path):
-            for (plugin_name, plugin_config_section) in get_config_section(
-                *config_path
-            ).items():
-                if plugin_config_section.get("enabled", False):
-                    enabled_plugins.append(plugin_name)
+        for (plugin_name, plugin_config_section) in get_plugins_config().items():
+            if plugin_config_section.get("enabled", False):
+                enabled_plugins.append(plugin_name)
         return enabled_plugins
 
     def get_config(self, plugin_name: str) -> PluginConfig:
-        config_path = ["snowcli", "plugins", plugin_name]
+        config_path = PLUGINS_SECTION_PATH + [plugin_name]
         plugin_config = PluginConfig(is_plugin_enabled=False, internal_config={})
         plugin_config.is_plugin_enabled = get_config_value(
             *config_path, key="enabled", default=False
