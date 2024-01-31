966588ad15f76bc747c446cb6251c3f70f270121
Marcin Raba <marcin.raba@snowflake.com>
2024-01-31 09:24:23 +0100

    SNOW-1019480-sql-filter-nonsql-statements: remove usefixtures from test

--- tests_integration/test_sql.py
+++ tests_integration/test_sql.py
@@ -103,8 +103,7 @@ def test_queries_are_streamed_to_output(
 
 
 @pytest.mark.integration
-@pytest.mark.usefixtures("snowflake_session, test_root_path")
-def test_trailing_comments_queries(runner):
+def test_trailing_comments_queries(runner, snowflake_session, test_root_path):
     trailin_comment_query = "select 1;\n\n-- trailing comment\n"
     result = runner.invoke_with_connection_json(["sql", "-q", trailin_comment_query])
     assert result.exit_code == 0
