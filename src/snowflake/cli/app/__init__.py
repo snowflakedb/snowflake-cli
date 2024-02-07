730001f44694f088612e69c68c95a9ef9e85444c
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-07 12:37:57 +0100

    Add os.umask duting app initialization

--- src/snowflake/cli/app/__init__.py
+++ src/snowflake/cli/app/__init__.py
@@ -1,4 +1,8 @@
 import logging
+import os
 
 # Suppress logging from Snowflake connector
 logging.getLogger("snowflake").setLevel(logging.ERROR)
+
+# Restrict permissions of all created files
+os.umask(0o077)
