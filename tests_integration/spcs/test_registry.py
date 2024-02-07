5640a4d46c10e0c687e493c884a2c21a0cfab3b3
David Wang <d.wang@snowflake.com>
2024-02-07 09:02:53 -0800

    SNOW-1011769: Convenience command 'spcs image-registry url' to get registry url based on current account. (#715)
    * SNOW-1011769: Adding 'spcs image-registry url' command.
    
    * SNOW-1011769: Fixing integration tests (seems like snapshot does not deal with colored output well)
    
    * SNOW-1011769: Updating error message for no repositories found when getting registry URL
    
    * SNOW-1011769: Updating test output
    
    * SNOW-1011769: Use urlparse to get registry url from repo url. Improving error name
    
    * SNOW-1011769: Updating tests to use row_from_snowflake_session

--- tests_integration/spcs/test_registry.py
+++ tests_integration/spcs/test_registry.py
@@ -1,5 +1,8 @@
 import pytest
 
+from tests_integration.test_utils import row_from_snowflake_session
+from tests_integration.testing_utils.naming_utils import ObjectNameProvider
+
 
 @pytest.mark.integration
 def test_token(runner):
@@ -11,3 +14,33 @@ def test_token(runner):
     assert result.json["token"]
     assert "expires_in" in result.json
     assert result.json["expires_in"]
+
+
+@pytest.mark.integration
+def test_get_registry_url(test_database, test_role, runner, snowflake_session):
+    # newly created test_role should have no access to image repositories and should not be able to get registry URL
+    test_repo = ObjectNameProvider("test_repo").create_and_get_next_object_name()
+    snowflake_session.execute_string(f"create image repository {test_repo}")
+
+    fail_result = runner.invoke_with_connection(
+        ["spcs", "image-registry", "url", "--role", test_role]
+    )
+    assert fail_result.exit_code == 1, fail_result.output
+    assert "No image repository found." in fail_result.output
+
+    # role should be able to get registry URL once granted read access to an image repository
+    repo_list_cursor = snowflake_session.execute_string("show image repositories")
+    expected_repo_url = row_from_snowflake_session(repo_list_cursor)[0][
+        "repository_url"
+    ]
+    expected_registry_url = "/".join(expected_repo_url.split("/")[:-3])
+    snowflake_session.execute_string(
+        f"grant usage on database {snowflake_session.database} to role {test_role};"
+        f"grant usage on schema {snowflake_session.schema} to role {test_role};"
+        f"grant read on image repository {test_repo} to role {test_role};"
+    )
+    success_result = runner.invoke_with_connection(
+        ["spcs", "image-registry", "url", "--role", test_role]
+    )
+    assert success_result.exit_code == 0, success_result.output
+    assert success_result.output.strip() == expected_registry_url
