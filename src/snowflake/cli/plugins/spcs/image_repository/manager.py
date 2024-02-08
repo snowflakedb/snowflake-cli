b6f1914e41b9957ded26fa36d028030a591ec540
David Wang <d.wang@snowflake.com>
2024-02-08 15:07:17 -0800

    using show_specific_object in image_repository.manager.get_repository_url

--- src/snowflake/cli/plugins/spcs/image_repository/manager.py
+++ src/snowflake/cli/plugins/spcs/image_repository/manager.py
@@ -1,15 +1,12 @@
-from typing import Dict
 from urllib.parse import urlparse
 
 from click import ClickException
 from snowflake.cli.api.constants import ObjectType
 from snowflake.cli.api.project.util import (
-    escape_like_pattern,
     is_valid_unquoted_identifier,
 )
 from snowflake.cli.api.sql_execution import SqlExecutionMixin
 from snowflake.cli.plugins.spcs.common import handle_object_already_exists
-from snowflake.connector.cursor import DictCursor
 from snowflake.connector.errors import ProgrammingError
 
 
@@ -23,40 +20,16 @@ class ImageRepositoryManager(SqlExecutionMixin):
     def get_role(self):
         return self._conn.role
 
-    def get_repository_row(self, repo_name: str) -> Dict:
-        if not is_valid_unquoted_identifier(repo_name):
-            raise ValueError(
-                f"repo_name '{repo_name}' is not a valid unquoted Snowflake identifier"
-            )
-
-        repo_name = repo_name.upper()
-
-        # because image repositories only support unquoted identifiers, SHOW LIKE should only return one or zero rows
-        repository_list_query = (
-            f"show image repositories like '{escape_like_pattern(repo_name)}'"
-        )
-
-        result_set = self._execute_schema_query(
-            repository_list_query, cursor_class=DictCursor
-        )
-        results = result_set.fetchall()
-
-        if len(results) == 0:
-            raise ClickException(
-                f"Image repository '{repo_name}' does not exist in database '{self.get_database()}' and schema '{self.get_schema()}' or not authorized."
-            )
-        elif len(results) > 1:
-            raise ClickException(
-                f"Found more than one image repository with name matching '{repo_name}'. This is unexpected."
-            )
-        return results[0]
-
     def get_repository_url(self, repo_name: str, with_scheme: bool = True):
         if not is_valid_unquoted_identifier(repo_name):
             raise ValueError(
                 f"repo_name '{repo_name}' is not a valid unquoted Snowflake identifier"
             )
-        repo_row = self.get_repository_row(repo_name)
+        repo_row = self.show_specific_object("image repositories", repo_name)
+        if repo_row is None:
+            raise ClickException(
+                f"Image repository '{repo_name}' does not exist in database '{self.get_database()}' and schema '{self.get_schema()}' or not authorized."
+            )
         if with_scheme:
             return f"https://{repo_row['repository_url']}"
         else:
