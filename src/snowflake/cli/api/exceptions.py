75f037942eea0f9b85ffd37c4d12067db76c5c47
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-12 14:30:12 +0100

    Snow 1038305 abstraction layer for file reading and writing (#758)
    * read_text()
    
    * open()
    
    * creating & permissions
    
    * refactor
    
    * copy IN PROGRESS
    
    * copy()
    
    * docstrings
    
    * mkdir; iterdir
    
    * temporary direcotry
    
    * fixes
    
    * limit kb -> mb
    
    * Add test for file size calculation
    
    * More detailed logs for copy; refactor test log check

--- src/snowflake/cli/api/exceptions.py
+++ src/snowflake/cli/api/exceptions.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import Optional
 
 from click.exceptions import ClickException
@@ -94,3 +95,15 @@ class SecretsWithoutExternalAccessIntegrationError(ClickException):
         super().__init__(
             f"{object_name} defined with secrets but without external integration."
         )
+
+
+class FileTooLargeError(ClickException):
+    def __init__(self, path: Path, size_limit_in_kb: int):
+        super().__init__(
+            f"File {path} is too large (size limit: {size_limit_in_kb} KB)"
+        )
+
+
+class DirectoryIsNotEmptyError(ClickException):
+    def __init__(self, path: Path):
+        super().__init__(f"Directory '{path}' is not empty")
