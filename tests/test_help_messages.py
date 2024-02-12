2ce64da5f095b19a38821d1409e27dfc09476863
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-12 10:20:24 +0100

    invoke -> check

--- tests/test_help_messages.py
+++ tests/test_help_messages.py
@@ -9,20 +9,20 @@ def test_help_messages(runner, snapshot):
     """
     IGNORE_PLUGINS = ["render"]
 
-    def _invoke_command(command):
+    def _check_command(command):
         result = runner.invoke(command + ["--help"])
         assert result.exit_code == 0
         assert result.output == snapshot(name=".".join(command))
 
-    def _invoke_all_commands(command, path):
-        _invoke_command(path)
+    def _check_all_commands(command, path):
+        _check_command(path)
         for subpath, subcommand in getattr(command, "commands", {}).items():
             path.append(subpath)
-            _invoke_all_commands(subcommand, path)
+            _check_all_commands(subcommand, path)
             path.pop()
 
     builtin_plugins = load_only_builtin_command_plugins()
     for plugin in builtin_plugins:
         spec = plugin.command_spec
         if not plugin.plugin_name in IGNORE_PLUGINS:
-            _invoke_all_commands(spec.command, spec.full_command_path.path_segments)
+            _check_all_commands(spec.command, spec.full_command_path.path_segments)
