4ea055fd96b19daf17375452d0e315fc66a658c0
David Wang <d.wang@snowflake.com>
2024-02-13 09:03:52 -0800

    SNOW-1011778: Adding 'spcs service upgrade' command (#773)
    * SNOW-1011778: Implemented upgrade command and manager function.
    
    * SNOW-1011778: Adding unit and integration tests for 'spcs service upgrade'
    
    * SNOW-1011778: Update release notes
    
    * SNOW-1011779: Formatting + refactoring fixes
    
    * SNOW-1011778: Removing unnecessary string processing on upgrade spec query
    
    * SNOW-1011778: Test cleanup

--- tests/spcs/test_services.py
+++ tests/spcs/test_services.py
@@ -1,5 +1,6 @@
 from textwrap import dedent
 from unittest.mock import Mock, patch
+import json
 
 from tests.spcs.test_common import SPCS_OBJECT_EXISTS_ERROR
 from snowflake.cli.api.constants import ObjectType
@@ -7,6 +8,29 @@ from snowflake.cli.plugins.spcs.services.manager import ServiceManager
 from tests.testing_utils.fixtures import *
 from snowflake.cli.api.project.util import to_string_literal
 from snowflake.cli.plugins.object.common import Tag
+
+SPEC_CONTENT = dedent(
+    """
+    spec:
+        containers:
+        - name: cloudbeaver
+          image: /spcs_demos_db/cloudbeaver:23.2.1
+        endpoints:
+        - name: cloudbeaver
+          port: 80
+          public: true
+
+    """
+)
+
+SPEC_DICT = {
+    "spec": {
+        "containers": [
+            {"name": "cloudbeaver", "image": "/spcs_demos_db/cloudbeaver:23.2.1"},
+        ],
+        "endpoints": [{"name": "cloudbeaver", "port": 80, "public": True}],
+    }
+}
 
 
 @patch(
@@ -19,21 +43,7 @@ def test_create_service(mock_execute_schema_query, other_directory):
     max_instances = 43
     tmp_dir = Path(other_directory)
     spec_path = tmp_dir / "spec.yml"
-    spec_path.write_text(
-        dedent(
-            """
-    spec:
-        containers:
-        - name: cloudbeaver
-          image: /spcs_demos_db/cloudbeaver:23.2.1
-        endpoints:
-        - name: cloudbeaver
-          port: 80
-          public: true
-    
-    """
-        )
-    )
+    spec_path.write_text(SPEC_CONTENT)
     auto_resume = True
     external_access_integrations = [
         "google_apis_access_integration",
@@ -62,9 +72,7 @@ def test_create_service(mock_execute_schema_query, other_directory):
         [
             "CREATE SERVICE test_service",
             "IN COMPUTE POOL test_pool",
-            'FROM SPECIFICATION $$ {"spec": {"containers": [{"name": "cloudbeaver", "image":',
-            '"/spcs_demos_db/cloudbeaver:23.2.1"}], "endpoints": [{"name": "cloudbeaver",',
-            '"port": 80, "public": true}]}} $$',
+            f"FROM SPECIFICATION $$ {json.dumps(SPEC_DICT)} $$",
             "MIN_INSTANCES = 42 MAX_INSTANCES = 43",
             "AUTO_RESUME = True",
             "EXTERNAL_ACCESS_INTEGRATIONS = (google_apis_access_integration,salesforce_api_access_integration)",
@@ -82,21 +90,7 @@ def test_create_service(mock_execute_schema_query, other_directory):
 def test_create_service_cli_defaults(mock_create, other_directory, runner):
     tmp_dir = Path(other_directory)
     spec_path = tmp_dir / "spec.yml"
-    spec_path.write_text(
-        dedent(
-            """
-    spec:
-        containers:
-        - name: cloudbeaver
-          image: /spcs_demos_db/cloudbeaver:23.2.1
-        endpoints:
-        - name: cloudbeaver
-          port: 80
-          public: true
-
-    """
-        )
-    )
+    spec_path.write_text(SPEC_CONTENT)
     result = runner.invoke(
         [
             "spcs",
@@ -128,21 +122,7 @@ def test_create_service_cli_defaults(mock_create, other_directory, runner):
 def test_create_service_cli(mock_create, other_directory, runner):
     tmp_dir = Path(other_directory)
     spec_path = tmp_dir / "spec.yml"
-    spec_path.write_text(
-        dedent(
-            """
-    spec:
-        containers:
-        - name: cloudbeaver
-          image: /spcs_demos_db/cloudbeaver:23.2.1
-        endpoints:
-        - name: cloudbeaver
-          port: 80
-          public: true
-
-    """
-        )
-    )
+    spec_path.write_text(SPEC_CONTENT)
     result = runner.invoke(
         [
             "spcs",
@@ -272,3 +252,50 @@ def test_logs(mock_execute_schema_query):
     )
     mock_execute_schema_query.assert_called_once_with(expected_query)
     assert result == cursor
+
+
+def test_read_yaml(other_directory):
+    tmp_dir = Path(other_directory)
+    spec_path = tmp_dir / "spec.yml"
+    spec_path.write_text(SPEC_CONTENT)
+    result = ServiceManager()._read_yaml(spec_path)
+    assert result == json.dumps(SPEC_DICT)
+
+
+@patch(
+    "snowflake.cli.plugins.spcs.services.manager.ServiceManager._execute_schema_query"
+)
+def test_upgrade_spec(mock_execute_schema_query, other_directory):
+    service_name = "test_service"
+    cursor = Mock(spec=SnowflakeCursor)
+    mock_execute_schema_query.return_value = cursor
+    tmp_dir = Path(other_directory)
+    spec_path = tmp_dir / "spec.yml"
+    spec_path.write_text(SPEC_CONTENT)
+    result = ServiceManager().upgrade_spec(service_name, spec_path)
+    expected_query = (
+        f"alter service {service_name} from specification $$ {json.dumps(SPEC_DICT)} $$"
+    )
+    actual_query = " ".join(mock_execute_schema_query.mock_calls[0].args[0].split())
+    assert expected_query == actual_query
+    assert result == cursor
+
+
+@patch("snowflake.cli.plugins.spcs.services.manager.ServiceManager.upgrade_spec")
+def test_upgrade_spec_cli(mock_upgrade_spec, mock_cursor, runner, other_directory):
+    cursor = mock_cursor(rows=[["Statement executed successfully"]], columns=["status"])
+    mock_upgrade_spec.return_value = cursor
+    service_name = "test_service"
+    tmp_dir = Path(other_directory)
+    spec_path = tmp_dir / "spec.yml"
+    spec_path.write_text(SPEC_CONTENT)
+
+    result = runner.invoke(
+        ["spcs", "service", "upgrade", service_name, "--spec-path", spec_path]
+    )
+
+    mock_upgrade_spec.assert_called_once_with(
+        service_name=service_name, spec_path=spec_path
+    )
+    assert result.exit_code == 0, result.output
+    assert "Statement executed successfully" in result.output
--- tests_integration/testing_utils/snowpark_services_utils.py
+++ tests_integration/testing_utils/snowpark_services_utils.py
@@ -10,6 +10,7 @@ from snowflake.connector import SnowflakeConnection
 from tests_integration.conftest import SnowCLIRunner
 from tests_integration.test_utils import contains_row_with, not_contains_row_with
 from tests_integration.testing_utils.assertions.test_result_assertions import (
+    assert_that_result_is_successful_and_executed_successfully,
     assert_that_result_is_successful_and_output_json_contains,
     assert_that_result_is_successful_and_output_json_equals,
 )
@@ -80,14 +81,7 @@ class SnowparkServicesTestSteps:
         assert not_contains_row_with(result.json, {"name": service_name.upper()})
 
     def describe_should_return_service(self, service_name: str) -> None:
-        result = self._setup.runner.invoke_with_connection_json(
-            [
-                "object",
-                "describe",
-                "service",
-                self._get_fqn(service_name),
-            ],
-        )
+        result = self._execute_describe(service_name)
         assert result.json
         assert result.json[0]["name"] == service_name.upper()  # type: ignore
 
@@ -129,6 +123,39 @@ class SnowparkServicesTestSteps:
             """
         ).strip()
         pytest.fail(error_message)
+
+    def upgrade_service_should_change_spec(self, service_name: str):
+        new_container_name = "goodbye-world"
+
+        describe_result = self._execute_describe(service_name)
+        assert describe_result.exit_code == 0, describe_result.output
+        assert (
+            new_container_name not in describe_result.json[0]["spec"]
+        ), f"Container name '{new_container_name}' found in output of DESCRIBE SERVICE before spec has been updated. This is unexpected."
+
+        spec_path = f"{self._setup.test_root_path}/spcs/spec/spec_upgrade.yml"
+        upgrade_result = self._setup.runner.invoke_with_connection_json(
+            [
+                "spcs",
+                "service",
+                "upgrade",
+                service_name,
+                "--spec-path",
+                spec_path,
+                *self._database_schema_args(),
+            ]
+        )
+        assert_that_result_is_successful_and_executed_successfully(
+            upgrade_result, is_json=True
+        )
+
+        describe_result = self._execute_describe(service_name)
+        with open(spec_path, "r") as f:
+            assert describe_result.exit_code == 0, describe_result.output
+            # do not assert direct equality because the spec field in output of DESCRIBE SERVICE has some extra info
+            assert (
+                new_container_name in describe_result.json[0]["spec"]
+            ), f"Container name '{new_container_name}' from spec_upgrade.yml not found in output of DESCRIBE SERVICE."
 
     def _execute_status(self, service_name: str):
         return self._setup.runner.invoke_with_connection_json(
@@ -203,3 +230,11 @@ class SnowparkServicesTestSteps:
 
     def _get_fqn(self, service_name) -> str:
         return f"{self.database}.{self.schema}.{service_name}"
+
+    def _database_schema_args(self):
+        return (
+            "--database",
+            self.database,
+            "--schema",
+            self.schema,
+        )
