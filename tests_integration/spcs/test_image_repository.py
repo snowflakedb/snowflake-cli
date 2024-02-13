8a51b1c08e63305d89fec4fbb81a7386ac17b31f
David Wang <d.wang@snowflake.com>
2024-02-13 00:45:44 -0800

    SNOW-1011779: Changing SPCS services integration test to use persistent service instead of job. (#741)

--- tests_integration/spcs/test_image_repository.py
+++ tests_integration/spcs/test_image_repository.py
@@ -11,7 +11,7 @@ INTEGRATION_REPOSITORY = "snowcli_repository"
 
 @pytest.mark.integration
 def test_list_images_tags(runner):
-    # test assumes the testing environment has been set up with /SNOWCLI_DB/PUBLIC/snowcli_repository/snowpark_test:1
+    # test assumes the testing environment has been set up with /SNOWCLI_DB/PUBLIC/snowcli_repository/snowpark_test_echo:1
     _list_images(runner)
     _list_tags(runner)
 
@@ -33,7 +33,7 @@ def _list_images(runner):
     assert contains_row_with(
         result.json,
         {
-            "image": f"/{INTEGRATION_DATABASE}/{INTEGRATION_SCHEMA}/{INTEGRATION_REPOSITORY}/snowpark_test"
+            "image": f"/{INTEGRATION_DATABASE}/{INTEGRATION_SCHEMA}/{INTEGRATION_REPOSITORY}/snowpark_test_echo"
         },
     )
 
@@ -46,7 +46,7 @@ def _list_tags(runner):
             "list-tags",
             "snowcli_repository",
             "--image_name",
-            f"/{INTEGRATION_DATABASE}/{INTEGRATION_SCHEMA}/{INTEGRATION_REPOSITORY}/snowpark_test",
+            f"/{INTEGRATION_DATABASE}/{INTEGRATION_SCHEMA}/{INTEGRATION_REPOSITORY}/snowpark_test_echo",
             "--database",
             INTEGRATION_DATABASE,
             "--schema",
@@ -57,7 +57,7 @@ def _list_tags(runner):
     assert contains_row_with(
         result.json,
         {
-            "tag": f"/{INTEGRATION_DATABASE}/{INTEGRATION_SCHEMA}/{INTEGRATION_REPOSITORY}/snowpark_test:1"
+            "tag": f"/{INTEGRATION_DATABASE}/{INTEGRATION_SCHEMA}/{INTEGRATION_REPOSITORY}/snowpark_test_echo:1"
         },
     )
 
