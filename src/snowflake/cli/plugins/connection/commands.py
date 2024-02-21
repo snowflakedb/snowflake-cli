91e8f5708e31b130d071eb24d782f004596f2f25
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2024-02-21 18:28:00 +0100

    Add session context validation in connection test (#812)

--- src/snowflake/cli/plugins/connection/commands.py
+++ src/snowflake/cli/plugins/connection/commands.py
@@ -19,12 +19,15 @@ from snowflake.cli.api.config import (
     connection_exists,
     get_config_section,
 )
+from snowflake.cli.api.constants import ObjectType
 from snowflake.cli.api.output.types import (
     CollectionResult,
     CommandResult,
     MessageResult,
     ObjectResult,
 )
+from snowflake.cli.plugins.object.manager import ObjectManager
+from snowflake.connector import ProgrammingError
 from snowflake.connector.config_manager import CONFIG_MANAGER
 
 app = SnowTyper(
@@ -228,7 +231,27 @@ def test(
     Tests the connection to Snowflake.
     """
 
+    # Test connection
     conn = cli_context.connection
+
+    # Test session attributes
+    om = ObjectManager()
+    try:
+        if conn.role:
+            om.use_role(new_role=conn.role)
+        if conn.database:
+            om.describe(
+                object_type=ObjectType.DATABASE.value.cli_name, name=conn.database
+            )
+        if conn.schema:
+            om.describe(object_type=ObjectType.SCHEMA.value.cli_name, name=conn.schema)
+        if conn.warehouse:
+            om.describe(
+                object_type=ObjectType.WAREHOUSE.value.cli_name, name=conn.warehouse
+            )
+    except ProgrammingError as err:
+        raise ClickException(str(err))
+
     result = {
         "Connection name": connection,
         "Status": "OK",
