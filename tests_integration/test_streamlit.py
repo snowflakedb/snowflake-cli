0927968ea679fc8a5b05685e95f40a78008971f1
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-22 10:54:39 +0100

    integration test

--- tests_integration/test_streamlit.py
+++ tests_integration/test_streamlit.py
@@ -160,6 +160,108 @@ def test_streamlit_is_visible_in_anaconda_channel():
     assert streamlit in result.snowflake
 
 
+@pytest.mark.integration
+def test_fully_qualified_name(
+    alter_snowflake_yml, test_database, project_directory, runner, snapshot
+):
+    default_schema = "PUBLIC"
+    different_schema = "TOTALLY_DIFFERENT_SCHEMA"
+    database = test_database.upper()
+    assert (
+        runner.invoke_with_connection(
+            ["sql", "-q", f"create schema {database}.{different_schema}"]
+        ).exit_code
+        == 0
+    )
+
+    # test fully qualified name as name
+    with project_directory("streamlit") as tmp_dir:
+        streamlit_name = "streamlit_fqn"
+        snowflake_yml = tmp_dir / "snowflake.yml"
+
+        # FQN with just schema
+        alter_snowflake_yml(
+            snowflake_yml,
+            parameter_path="streamlit.name",
+            value=f"{database}.{default_schema}.{streamlit_name}",
+        )
+        result = runner.invoke_with_connection_json(["streamlit", "deploy"])
+        assert result.exit_code == 0
+        assert result.json == {
+            "message": "Streamlit successfully deployed and available under "
+            f"https://app.snowflake.com/SFENGINEERING/snowcli_it/#/streamlit-apps/{database}.{default_schema}.{streamlit_name.upper()}",
+        }
+
+        # FQN with different schema - should not conflict
+        alter_snowflake_yml(
+            snowflake_yml,
+            parameter_path="streamlit.name",
+            value=f"{database}.{different_schema}.{streamlit_name}",
+        )
+        result = runner.invoke_with_connection_json(["streamlit", "deploy"])
+        assert result.exit_code == 0
+        assert result.json == {
+            "message": "Streamlit successfully deployed and available under "
+            f"https://app.snowflake.com/SFENGINEERING/snowcli_it/#/streamlit-apps/{database}.{different_schema}.{streamlit_name.upper()}",
+        }
+
+        # FQN with just schema provided - should update
+        alter_snowflake_yml(
+            snowflake_yml,
+            parameter_path="streamlit.name",
+            value=f"{different_schema}.{streamlit_name}",
+        )
+        result = runner.invoke_with_connection_json(["streamlit", "deploy"])
+        assert result.exit_code == 1
+        result = runner.invoke_with_connection_json(
+            ["streamlit", "deploy", "--replace"]
+        )
+        assert result.exit_code == 0
+        assert result.json == {
+            "message": "Streamlit successfully deployed and available under "
+            f"https://app.snowflake.com/SFENGINEERING/snowcli_it/#/streamlit-apps/{database}.{different_schema}.{streamlit_name.upper()}",
+        }
+
+    # should support fully qualified name via parameters
+    with project_directory("streamlit_fully_qualified_name") as tmp_dir:
+        streamlit_name = "streamlit_fqn_parameters"
+        snowflake_yml = tmp_dir / "snowflake.yml"
+
+        alter_snowflake_yml(
+            snowflake_yml,
+            parameter_path="streamlit.database",
+            value=database,
+        )
+        alter_snowflake_yml(
+            snowflake_yml,
+            parameter_path="streamlit.schema",
+            value=different_schema,
+        )
+
+        # error - redefined database
+        alter_snowflake_yml(
+            snowflake_yml,
+            parameter_path="streamlit.name",
+            value=f"{database}.{different_schema}.{streamlit_name}",
+        )
+        result = runner.invoke_with_connection(["streamlit", "deploy"])
+        assert result.exit_code == 1
+        assert result.output == snapshot(name="error database")
+
+        # success
+        alter_snowflake_yml(
+            snowflake_yml,
+            parameter_path="streamlit.name",
+            value=f"{different_schema}.{streamlit_name}",
+        )
+        result = runner.invoke_with_connection_json(["streamlit", "deploy"])
+        assert result.exit_code == 1
+        assert result.json == {
+            "message": "Streamlit successfully deployed and available under "
+            f"https://app.snowflake.com/SFENGINEERING/snowcli_it/#/streamlit-apps/{database}.{different_schema}.{streamlit_name.upper()}",
+        }
+
+
 @pytest.fixture
 def _new_streamlit_role(snowflake_session, test_database):
     role_name = f"snowcli_streamlit_role_{uuid.uuid4().hex}"
