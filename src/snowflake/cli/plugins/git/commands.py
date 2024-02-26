8cb44683aa3ddc1122c1a743b9b4be5db8ae4845
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-26 10:17:40 +0100

    Fix tests

--- src/snowflake/cli/plugins/git/commands.py
+++ src/snowflake/cli/plugins/git/commands.py
@@ -15,9 +15,11 @@ log = logging.getLogger(__name__)
 
 @app.command("draft1")
 def draft_command1(**options) -> CommandResult:
+    """ "Slot for actual command."""
     return MessageResult("Mornin'!")
 
 
 @app.command("draft2")
 def draft_command2(**options) -> CommandResult:
+    """Slot for actual command."""
     return MessageResult("Nice day for fishin', innit?")
