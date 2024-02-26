802491d98de5f813a8c1b7c508f0b99413302cb7
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-26 18:16:52 +0100

    tests

--- tests/git/test_git_commands.py
+++ tests/git/test_git_commands.py
@@ -1,3 +1,6 @@
+from unittest import mock
+
+
 def test_toplevel_help(runner):
     result = runner.invoke(["--help"])
     assert (
@@ -7,3 +10,23 @@ def test_toplevel_help(runner):
 
     result = runner.invoke(["git", "--help"])
     assert result.exit_code == 0, result.output
+
+
+@mock.patch("snowflake.connector.connect")
+def test_list_branches(mock_connector, runner, mock_ctx):
+    ctx = mock_ctx()
+    mock_connector.return_value = ctx
+    result = runner.invoke(["git", "list-branches", "repo_name"])
+
+    assert result.exit_code == 0, result.output
+    assert ctx.get_query() == "show git branches in repo_name"
+
+
+@mock.patch("snowflake.connector.connect")
+def test_list_tags(mock_connector, runner, mock_ctx):
+    ctx = mock_ctx()
+    mock_connector.return_value = ctx
+    result = runner.invoke(["git", "list-tags", "repo_name"])
+
+    assert result.exit_code == 0, result.output
+    assert ctx.get_query() == "show git tags in repo_name"
