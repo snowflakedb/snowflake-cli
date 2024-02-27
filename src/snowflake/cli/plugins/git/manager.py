ee2cbe97654d5f24ef0ad50e9f3cd2286a82589b
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-27 12:30:40 +0100

    Snow 1105625 git list branches tags (#839)
    * Add commands
    
    * tests
    
    * review
    
    * review
    
    * update e2e snapshots

---
+++ src/snowflake/cli/plugins/git/manager.py
@@ -0,0 +1,11 @@
+from snowflake.cli.api.sql_execution import SqlExecutionMixin
+
+
+class GitManager(SqlExecutionMixin):
+    def show_branches(self, repo_name: str):
+        query = f"show git branches in {repo_name}"
+        return self._execute_query(query)
+
+    def show_tags(self, repo_name: str):
+        query = f"show git tags in {repo_name}"
+        return self._execute_query(query)
