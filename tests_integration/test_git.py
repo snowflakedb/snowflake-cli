eaf972db868f0ce81a236ce601c37112dcfc229d
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-28 16:53:13 +0100

    test git flow - in progress

--- tests_integration/test_git.py
+++ tests_integration/test_git.py
@@ -1,11 +1,10 @@
-import time
-from unittest import mock
-
 import pytest
+
+from click import ClickException
 
 
 @pytest.fixture
-def git_repository(runner):
+def git_repository(runner, test_database):
     repo_name = "GITHUB_SNOWCLI_API_INTEGRATION"
     integration_name = "GITHUB_SNOWCLI_API_INTEGRATION"
 
@@ -41,13 +40,85 @@ def git_repository(runner):
 
 
 @pytest.mark.integration
-def test_flow(runner, git_repository):
-    result = runner.invoke_with_connection(["object", "list", "git-repository"])
-    print(result.output)
-    assert result.exit_code == 0
+def test_flow(runner, git_repository, snapshot):
+    if False:
+        # object list
+        result = runner.invoke_with_connection_json(
+            ["object", "list", "git-repository"]
+        )
+        assert result.exit_code == 0
+        _assert_object_with_name_in(result.json, name=git_repository)
+
+        # describe
+        result = runner.invoke_with_connection_json(
+            ["object", "describe", "git-repository", git_repository]
+        )
+        assert result.exit_code == 0
+        assert result.json[0]["name"] == git_repository
+
+        # fetch
+        result = runner.invoke_with_connection_json(["git", "fetch", git_repository])
+        assert result.exit_code == 0
+        assert result.json == [
+            {
+                "status": f"Git Repository {git_repository} is up to date. No change was fetched."
+            }
+        ]
+
+        # list branches
+        result = runner.invoke_with_connection_json(
+            ["git", "list-branches", git_repository]
+        )
+        assert result.exit_code == 0
+        _assert_object_with_name_in(result.json, name="main")
+
+        # list tags
+        result = runner.invoke_with_connection_json(
+            ["git", "list-tags", git_repository]
+        )
+        assert result.exit_code == 0
+        _assert_object_with_name_in(result.json, name="v2.0.0rc2")
+
+    # list files - error messages
+    result = runner.invoke_with_connection(["git", "list-files", git_repository])
+    assert (
+        result.exit_code == 1
+        and result.output == snapshot["list-files-not-stage-error"]
+    )
+    result = runner.invoke_with_connection(["git", "list-files"])
+
+    repository_path = f"@{git_repository}"
+    result = runner.invoke_with_connection(["git", "list-files", repository_path])
+    print(result.__dict__)
+
+    repository_path = f"@{git_repository}/branches/beach_which_does_not_exist"
+    result = runner.invoke_with_connection(["git", "list-files", repository_path])
+    print(result.__dict__)
+
+    repository_path = f"@{git_repository}/tags/tag_which_does_not_exist"
+    result = runner.invoke_with_connection(["git", "list-files", repository_path])
+    print(result.__dict__)
+
+    repository_path = f"@{git_repository}/branches/main"
+    result = runner.invoke_with_connection_json(["git", "list-files", repository_path])
+    # _assert_object_with_name_in()
+
+    # for later
+    # copy - error messages
+    result = runner.invoke_with_connection(
+        ["git", "copy", git_repository, "@some_stage"]
+    )
+    assert result.exit_code == 1 and result.output == snapshot["copy-not-stage-error"]
+
+
+def _assert_object_with_name_in(objects, *, name):
+    assert any(object["name"] == name for object in objects)
 
 
 def _integration_exists(runner, integration_name):
     result = runner.invoke_with_connection_json(["sql", "-q", "SHOW INTEGRATIONS"])
     assert result.exit_code == 0
-    return any(integration["name"] == integration_name for integration in result.json)
+    return any(
+        integration["name"].upper() == integration_name.upper()
+        for integration in result.json
+    )
