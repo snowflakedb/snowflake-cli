228fb3e58911c94d76cf4fa9e1476ecf23a7cc7f
Patryk Czajka <patryk.czajka@snowflake.com>
2024-03-01 17:38:32 +0100

    finish flow test

--- tests_integration/test_git.py
+++ tests_integration/test_git.py
@@ -1,6 +1,7 @@
 import pytest
-
-from click import ClickException
+from snowflake.connector.errors import ProgrammingError
+from pathlib import Path
+import tempfile
 
 
 @pytest.fixture
@@ -41,78 +42,150 @@ def git_repository(runner, test_database):
 
 @pytest.mark.integration
 def test_flow(runner, git_repository, snapshot):
-    if False:
-        # object list
-        result = runner.invoke_with_connection_json(
-            ["object", "list", "git-repository"]
-        )
-        assert result.exit_code == 0
-        _assert_object_with_name_in(result.json, name=git_repository)
+    # object list
+    result = runner.invoke_with_connection_json(["object", "list", "git-repository"])
+    assert result.exit_code == 0
+    _assert_object_with_key_value(result.json, key="name", value=git_repository)
 
-        # describe
-        result = runner.invoke_with_connection_json(
-            ["object", "describe", "git-repository", git_repository]
-        )
-        assert result.exit_code == 0
-        assert result.json[0]["name"] == git_repository
+    # describe
+    result = runner.invoke_with_connection_json(
+        ["object", "describe", "git-repository", git_repository]
+    )
+    assert result.exit_code == 0
+    assert result.json[0]["name"] == git_repository
 
-        # fetch
-        result = runner.invoke_with_connection_json(["git", "fetch", git_repository])
-        assert result.exit_code == 0
-        assert result.json == [
-            {
-                "status": f"Git Repository {git_repository} is up to date. No change was fetched."
-            }
-        ]
+    # fetch
+    result = runner.invoke_with_connection_json(["git", "fetch", git_repository])
+    assert result.exit_code == 0
+    assert result.json == [
+        {
+            "status": f"Git Repository {git_repository} is up to date. No change was fetched."
+        }
+    ]
 
-        # list branches
-        result = runner.invoke_with_connection_json(
-            ["git", "list-branches", git_repository]
-        )
-        assert result.exit_code == 0
-        _assert_object_with_name_in(result.json, name="main")
+    # list branches
+    result = runner.invoke_with_connection_json(
+        ["git", "list-branches", git_repository]
+    )
+    assert result.exit_code == 0
+    _assert_object_with_key_value(result.json, key="name", value="main")
 
-        # list tags
-        result = runner.invoke_with_connection_json(
-            ["git", "list-tags", git_repository]
-        )
-        assert result.exit_code == 0
-        _assert_object_with_name_in(result.json, name="v2.0.0rc2")
+    # list tags
+    result = runner.invoke_with_connection_json(["git", "list-tags", git_repository])
+    assert result.exit_code == 0
+    _assert_object_with_key_value(result.json, key="name", value="v2.0.0rc2")
 
     # list files - error messages
     result = runner.invoke_with_connection(["git", "list-files", git_repository])
-    assert (
-        result.exit_code == 1
-        and result.output == snapshot["list-files-not-stage-error"]
-    )
-    result = runner.invoke_with_connection(["git", "list-files"])
+    assert result.output == snapshot(name="list-files-not-stage-error")
 
-    repository_path = f"@{git_repository}"
-    result = runner.invoke_with_connection(["git", "list-files", repository_path])
-    print(result.__dict__)
+    try:
+        repository_path = f"@{git_repository}"
+        runner.invoke_with_connection(["git", "list-files", repository_path])
+    except ProgrammingError as err:
+        assert (
+            err.raw_msg
+            == "Files paths in git repositories must specify a scope. For example, a branch name, "
+            "a tag name, or a valid commit hash. Commit hashes are between 6 and 40 characters long."
+        )
 
-    repository_path = f"@{git_repository}/branches/beach_which_does_not_exist"
-    result = runner.invoke_with_connection(["git", "list-files", repository_path])
-    print(result.__dict__)
+    try:
+        repository_path = f"@{git_repository}/branches/branch_which_does_not_exist"
+        runner.invoke_with_connection(["git", "list-files", repository_path])
+    except ProgrammingError as err:
+        assert (
+            err.raw_msg
+            == "The specified branch 'branch_which_does_not_exist' cannot be found in the Git Repository."
+        )
 
-    repository_path = f"@{git_repository}/tags/tag_which_does_not_exist"
-    result = runner.invoke_with_connection(["git", "list-files", repository_path])
-    print(result.__dict__)
+    try:
+        repository_path = f"@{git_repository}/tags/tag_which_does_not_exist"
+        runner.invoke_with_connection(["git", "list-files", repository_path])
+    except ProgrammingError as err:
+        assert (
+            err.raw_msg
+            == "The specified tag 'tag_which_does_not_exist' cannot be found in the Git Repository."
+        )
 
+    # list-files - success
     repository_path = f"@{git_repository}/branches/main"
     result = runner.invoke_with_connection_json(["git", "list-files", repository_path])
-    # _assert_object_with_name_in()
-
-    # for later
-    # copy - error messages
-    result = runner.invoke_with_connection(
-        ["git", "copy", git_repository, "@some_stage"]
+    _assert_object_with_key_value(
+        result.json,
+        key="name",
+        value="github_snowcli_api_integration/branches/main/RELEASE-NOTES.md",
     )
-    assert result.exit_code == 1 and result.output == snapshot["copy-not-stage-error"]
+
+    # create stage for testing copy
+    stage_name = "a_perfect_stage_for_testing"
+    result = runner.invoke_with_connection(["sql", "-q", f"create stage {stage_name}"])
+
+    # copy - test error messages
+    result = runner.invoke_with_connection(
+        ["git", "copy", git_repository, f"@{stage_name}"]
+    )
+    assert result.output == snapshot(name="copy-not-stage-error")
+    try:
+        repository_path = f"@{git_repository}"
+        runner.invoke_with_connection(
+            ["git", "copy", repository_path, f"@{stage_name}"]
+        )
+    except ProgrammingError as err:
+        assert (
+            err.raw_msg
+            == "Files paths in git repositories must specify a scope. For example, a branch name, "
+            "a tag name, or a valid commit hash. Commit hashes are between 6 and 40 characters long."
+        )
+
+    try:
+        repository_path = f"@{git_repository}/branches/branch_which_does_not_exist"
+        runner.invoke_with_connection(
+            ["git", "copy", repository_path, f"@{stage_name}"]
+        )
+    except ProgrammingError as err:
+        assert (
+            err.raw_msg
+            == "The specified branch 'branch_which_does_not_exist' cannot be found in the Git Repository."
+        )
+
+    try:
+        repository_path = f"@{git_repository}/tags/tag_which_does_not_exist"
+        runner.invoke_with_connection(
+            ["git", "copy", repository_path, f"@{stage_name}"]
+        )
+    except ProgrammingError as err:
+        assert (
+            err.raw_msg
+            == "The specified tag 'tag_which_does_not_exist' cannot be found in the Git Repository."
+        )
+
+    # copy to stage - success
+    repository_path = f"@{git_repository}/branches/main"
+    result = runner.invoke_with_connection_json(
+        ["git", "copy", repository_path, f"@{stage_name}"]
+    )
+    assert result.exit_code == 0
+    _assert_object_with_key_value(result.json, key="file", value="RELEASE-NOTES.md")
+    result = runner.invoke_with_connection_json(["object", "stage", "list", stage_name])
+    _assert_object_with_key_value(
+        result.json, key="name", value=f"{stage_name}/RELEASE-NOTES.md"
+    )
+
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        # copy to local file system - success
+        repository_path = f"@{git_repository}/branches/main/"
+        result = runner.invoke_with_connection_json(
+            ["git", "copy", repository_path, tmp_dir]
+        )
+        assert result.exit_code == 0
+        assert (Path(tmp_dir) / "RELEASE-NOTES.md").exists()
+        import json
+
+        print(json.dumps(result.json, indent=2))
 
 
-def _assert_object_with_name_in(objects, *, name):
-    assert any(object["name"] == name for object in objects)
+def _assert_object_with_key_value(objects, *, key, value):
+    assert any(object[key] == value for object in objects)
 
 
 def _integration_exists(runner, integration_name):
