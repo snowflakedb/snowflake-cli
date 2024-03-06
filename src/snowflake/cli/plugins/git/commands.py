5f1711124a9d4761feee857caede53ce6b39bfa4
Patryk Czajka <patryk.czajka@snowflake.com>
2024-03-06 16:36:57 +0100

    Review fixes

--- src/snowflake/cli/plugins/git/commands.py
+++ src/snowflake/cli/plugins/git/commands.py
@@ -6,7 +6,6 @@ from click import ClickException
 from snowflake.cli.api.commands.flags import identifier_argument
 from snowflake.cli.api.commands.snow_typer import SnowTyper
 from snowflake.cli.api.output.types import CommandResult, QueryResult
-from snowflake.cli.api.secure_path import SecurePath
 from snowflake.cli.api.utils.path_utils import is_stage_path
 from snowflake.cli.plugins.git.manager import GitManager
 
@@ -21,8 +20,8 @@ log = logging.getLogger(__name__)
 def _repo_path_argument_callback(path):
     if not is_stage_path(path):
         raise ClickException(
-            f"REPOSITORY_PATH should be a path to git repository stage with scope provided."
-            " For example: @my_repo/branches/main"
+            "REPOSITORY_PATH should be a path to git repository stage with scope provided."
+            " For example: @my_repo/branches/main/"
         )
     return path
 
@@ -30,68 +29,79 @@ def _repo_path_argument_callback(path):
 RepoNameArgument = identifier_argument(sf_object="git repository", example="my_repo")
 RepoPathArgument = typer.Argument(
     metavar="REPOSITORY_PATH",
-    help="Path to git repository stage with scope provided. For example: @my_repo/branches/main",
+    help=(
+        "Path to git repository stage with scope provided."
+        " Path to the repository root must end with '/'."
+        " For example: @my_repo/branches/main/"
+    ),
     callback=_repo_path_argument_callback,
 )
 
 
 @app.command(
     "list-branches",
-    help="List all branches in the repository.",
     requires_connection=True,
 )
 def list_branches(
     repository_name: str = RepoNameArgument,
     **options,
 ) -> CommandResult:
+    """
+    List all branches in the repository.
+    """
     return QueryResult(GitManager().show_branches(repo_name=repository_name))
 
 
 @app.command(
     "list-tags",
-    help="List all tags in the repository.",
     requires_connection=True,
 )
 def list_tags(
     repository_name: str = RepoNameArgument,
     **options,
 ) -> CommandResult:
+    """
+    List all tags in the repository.
+    """
     return QueryResult(GitManager().show_tags(repo_name=repository_name))
 
 
 @app.command(
     "list-files",
-    help="List files from given state of git repository.",
     requires_connection=True,
 )
 def list_files(
     repository_path: str = RepoPathArgument,
     **options,
 ) -> CommandResult:
+    """
+    List files from given state of git repository.
+    """
     return QueryResult(GitManager().show_files(repo_path=repository_path))
 
 
 @app.command(
     "fetch",
-    help="Fetch changes from origin to snowflake repository.",
     requires_connection=True,
 )
 def fetch(
     repository_name: str = RepoNameArgument,
     **options,
 ) -> CommandResult:
+    """
+    Fetch changes from origin to snowflake repository.
+    """
     return QueryResult(GitManager().fetch(repo_name=repository_name))
 
 
 @app.command(
     "copy",
-    help="Copies all files from given state of repository to local directory or stage.",
     requires_connection=True,
 )
 def copy(
     repository_path: str = RepoPathArgument,
     destination_path: str = typer.Argument(
-        help="Target path for copy operation. Should be stage or a path to a local directory.",
+        help="Target path for copy operation. Should be a path to a directory on remote stage or local file system.",
     ),
     parallel: int = typer.Option(
         4,
@@ -99,16 +109,21 @@ def copy(
     ),
     **options,
 ):
+    """
+    Copies all files from given state of repository to local directory or stage.
+
+    If the source path ends with '/', the command copies contents of specified directory.
+    Otherwise, it creates a new directory or file in the destination directory.
+    """
     is_copy = is_stage_path(destination_path)
     if is_copy:
         cursor = GitManager().copy_files(
             source_path=repository_path, destination_path=destination_path
         )
     else:
-        target = SecurePath(Path(destination_path).resolve())
-        if not target.exists():
-            target.mkdir()
         cursor = GitManager().get(
-            stage_path=repository_path, dest_path=target.path, parallel=parallel
+            stage_path=repository_path,
+            dest_path=Path(destination_path).resolve(),
+            parallel=parallel,
         )
     return QueryResult(cursor)
--- tests/git/test_git_commands.py
+++ tests/git/test_git_commands.py
@@ -49,7 +49,7 @@ def test_list_files(mock_connector, runner, mock_ctx):
 def test_list_files_not_a_stage_error(runner):
     result = runner.invoke(["git", "list-files", "repo_name/branches/main"])
     assert result.exit_code == 1
-    _assert_error_message(result.output)
+    _assert_invalid_repo_path_error_message(result.output)
 
 
 @mock.patch("snowflake.connector.connect")
@@ -72,10 +72,9 @@ def test_copy_to_local_file_system(mock_connector, runner, mock_ctx, temp_dir):
 
     assert result.exit_code == 0, result.output
     assert local_path.exists()
-    # paths in generated SQL should end with '/'
     assert (
         ctx.get_query()
-        == f"get @repo_name/branches/main/ file://{local_path.resolve()}/ parallel=4"
+        == f"get @repo_name/branches/main file://{local_path.resolve()}/ parallel=4"
     )
 
 
@@ -87,23 +86,22 @@ def test_copy_to_remote_dir(mock_connector, runner, mock_ctx):
         ["git", "copy", "@repo_name/branches/main", "@stage_path/dir_in_stage"]
     )
 
-    # paths in generated SQL should end with '/'
     assert result.exit_code == 0, result.output
     assert (
         ctx.get_query()
-        == "copy files into @stage_path/dir_in_stage/ from @repo_name/branches/main/"
+        == "copy files into @stage_path/dir_in_stage/ from @repo_name/branches/main"
     )
 
 
 def test_copy_not_a_stage_error(runner):
     result = runner.invoke(["git", "copy", "repo_name", "@stage_path/dir_in_stage"])
     assert result.exit_code == 1
-    _assert_error_message(result.output)
+    _assert_invalid_repo_path_error_message(result.output)
 
 
-def _assert_error_message(output):
+def _assert_invalid_repo_path_error_message(output):
     assert "Error" in output
     assert (
         "REPOSITORY_PATH should be a path to git repository stage with scope" in output
     )
-    assert "provided. For example: @my_repo/branches/main" in output
+    assert "provided. For example: @my_repo/branches/main/" in output
