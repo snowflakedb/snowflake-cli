88ae9f24b971964b0cbf90ce3055d4ec56cf73b3
Patryk Czajka <patryk.czajka@snowflake.com>
2024-03-07 14:24:14 +0100

    initial version of setup command

--- src/snowflake/cli/plugins/git/commands.py
+++ src/snowflake/cli/plugins/git/commands.py
@@ -1,10 +1,12 @@
 import logging
 from pathlib import Path
+from typing import Optional
 
 import typer
 from click import ClickException
 from snowflake.cli.api.commands.flags import identifier_argument
 from snowflake.cli.api.commands.snow_typer import SnowTyper
+from snowflake.cli.api.console.console import cli_console
 from snowflake.cli.api.output.types import CommandResult, QueryResult
 from snowflake.cli.api.utils.path_utils import is_stage_path
 from snowflake.cli.plugins.git.manager import GitManager
@@ -38,6 +40,72 @@ RepoPathArgument = typer.Argument(
 )
 
 
+@app.command("setup", requires_connection=True)
+def setup(
+    repository_name: str = RepoNameArgument,
+    **options,
+) -> CommandResult:
+    """
+    Sets up a git repository object.
+
+    You will be prompted for:
+
+    * url - address of repository to be used for git clone operation
+
+    * secret - Snowflake secret containing authentication credentials. Not needed if origin repository does not require
+    authentication for RO operations (clone, fetch)
+
+    * API integration - object allowing Snowflake to interact with git repository.
+    """
+
+    manager = GitManager()
+
+    def _assure_repository_does_not_exist() -> None:
+        pass
+
+    def _get_secret() -> Optional[str]:
+        secret_needed = typer.confirm("Use secret for authentication?")
+        if not secret_needed:
+            return None
+
+        use_existing_secret = typer.confirm("Use existing secret?")
+        if use_existing_secret:
+            existing_secret = typer.prompt("Secret identifier")
+            return existing_secret
+
+        cli_console.step("Creating new secret")
+        secret_name = f"{repository_name}_secret"
+        username = typer.prompt("username")
+        password = typer.prompt("password/token", hide_input=True)
+        manager.create_secret(username=username, password=password, name=secret_name)
+        cli_console.step(f"Secret '{secret_name}' successfully created")
+        return secret_name
+
+    def _get_api_integration(secret: Optional[str], url: str) -> str:
+        use_existing_api = typer.confirm("Use existing api integration?")
+        if use_existing_api:
+            api_name = typer.prompt("API integration identifier")
+            return api_name
+
+        api_name = f"{repository_name}_api_integration"
+        manager.create_api_integration(name=api_name, allowed_prefix=url, secret=secret)
+        cli_console.step(f"API integration '{api_name}' successfully created.")
+        return api_name
+
+    _assure_repository_does_not_exist()
+    url = typer.prompt("Origin url")
+    secret = _get_secret()
+    api_integration = _get_api_integration(secret=secret, url=url)
+    return QueryResult(
+        manager.create(
+            repo_name=repository_name,
+            url=url,
+            api_integration=api_integration,
+            secret=secret,
+        )
+    )
+
+
 @app.command(
     "list-branches",
     requires_connection=True,
@@ -61,7 +129,7 @@ def list_tags(
     **options,
 ) -> CommandResult:
     """
-    List all tags in the repository.
+    List all tags in the repository.§
     """
     return QueryResult(GitManager().show_tags(repo_name=repository_name))
 
--- src/snowflake/cli/plugins/git/manager.py
+++ src/snowflake/cli/plugins/git/manager.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from snowflake.cli.plugins.object.stage.manager import StageManager
 from snowflake.connector.cursor import SnowflakeCursor
 
@@ -17,4 +19,37 @@ class GitManager(StageManager):
 
     def fetch(self, repo_name: str) -> SnowflakeCursor:
         query = f"alter git repository {repo_name} fetch"
+        return self._execute_query(query)
+
+    def create(
+        self, repo_name: str, api_integration: str, url: str, secret: str
+    ) -> SnowflakeCursor:
+        query = (
+            f"create git repository {repo_name}"
+            f" api_integration = {api_integration}"
+            f" origin = '{url}'"
+        )
+        if secret is not None:
+            query += f" git_credentials = {secret}"
+        return self._execute_query(query)
+
+    def create_secret(self, name: str, username: str, password: str) -> SnowflakeCursor:
+        query = (
+            f"create secret {name}"
+            f" type = password"
+            f" username = '{username}'"
+            f" password = '{password}'"
+        )
+        return self._execute_query(query)
+
+    def create_api_integration(
+        self, name: str, allowed_prefix: str, secret: Optional[str]
+    ) -> SnowflakeCursor:
+        query = (
+            f"create api integration {name}"
+            f" api_provider = git_https_api"
+            f" api_allowed_prefixes = ('{allowed_prefix}')"
+            f" allowed_authentication_secrets = ({secret if secret else ''})"
+            f" enabled = true"
+        )
         return self._execute_query(query)
