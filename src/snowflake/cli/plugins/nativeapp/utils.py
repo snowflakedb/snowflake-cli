73d7c328c41f5b17523cb880dabfbbb81de1d62f
Marcus Chok <marcus.chok@snowflake.com>
2024-03-08 16:59:19 -0500

    [SNOW-1039218] feat: add hidden app list-templates command

--- src/snowflake/cli/plugins/nativeapp/utils.py
+++ src/snowflake/cli/plugins/nativeapp/utils.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from sys import stdin, stdout
 
 
@@ -7,3 +8,16 @@ def needs_confirmation(needs_confirm: bool, auto_yes: bool) -> bool:
 
 def is_tty_interactive():
     return stdin.isatty() and stdout.isatty()
+
+
+def get_first_paragraph_from_markdown_file(file_path: Path) -> str:
+    with open(file_path, "r") as markdown_file:
+        paragraph_text = ""
+
+        for line in markdown_file:
+            stripped_line = line.strip()
+            if not stripped_line.startswith("#") and stripped_line:
+                paragraph_text = stripped_line
+                break
+
+        return paragraph_text
