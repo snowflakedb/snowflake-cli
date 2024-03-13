6ceb316eeddd99aa6cb66fe9329ff17a333bf9ce
Adam Stus <adam.stus@snowflake.com>
2024-03-13 09:50:30 +0100

    Copy dependencies to requirements.txt in pre commit (#890)

---
+++ .snyk/dependency-sync.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import tomllib
+
+
+def sync():
+    pyproject = tomllib.loads(Path("pyproject.toml").read_text())
+    dependencies = pyproject["project"]["dependencies"]
+    dev_dependencies = pyproject["project"]["optional-dependencies"]["development"]
+    with open(".snyk/req-auto-generated.txt", "w") as req:
+        req.write("# Auto generated\n")
+        for dep in dependencies:
+            req.write(f"{dep}\n")
+        for dep in dev_dependencies:
+            req.write(f"{dep}\n")
+
+
+if __name__ == "__main__":
+    sync()
