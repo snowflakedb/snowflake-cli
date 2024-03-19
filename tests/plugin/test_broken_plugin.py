0eaef443b8f25f4e233a77974df6ef7a4838120b
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2024-03-19 13:10:53 -0700

    fixup! Add is_default to connections list

--- tests/plugin/test_broken_plugin.py
+++ tests/plugin/test_broken_plugin.py
@@ -1,25 +1,12 @@
-from textwrap import dedent
-
 import pytest
 
 
-def test_broken_command_path_plugin(runner, test_root_path, _install_plugin, caplog):
+def test_broken_command_path_plugin(runner, test_root_path, _install_plugin, snapshot):
     config_path = test_root_path / "test_data" / "configs" / "broken_plugin_config.toml"
 
     result = runner.invoke(["--config-file", config_path, "connection", "list"])
 
-    assert (
-        caplog.messages[0]
-        == "Cannot register plugin [broken_plugin]: Invalid command path [snow broken run]. Command group [broken] does not exist."
-    )
-    assert result.output == dedent(
-        """+---------------------------------------+
-| connection_name | parameters          |
-|-----------------+---------------------|
-| test            | {'account': 'test'} |
-+---------------------------------------+
-    """
-    )
+    assert result.output == snapshot
 
 
 @pytest.fixture(scope="module")
--- tests/plugin/test_failing_plugin.py
+++ tests/plugin/test_failing_plugin.py
@@ -1,27 +1,14 @@
-from textwrap import dedent
-
 import pytest
 
 
-def test_failing_plugin(runner, test_root_path, _install_plugin, caplog):
+def test_failing_plugin(runner, test_root_path, _install_plugin, snapshot):
     config_path = (
         test_root_path / "test_data" / "configs" / "failing_plugin_config.toml"
     )
 
     result = runner.invoke(["--config-file", config_path, "connection", "list"])
 
-    assert (
-        caplog.messages[0]
-        == "Cannot register plugin [failing_plugin]: Some error in plugin"
-    )
-    assert result.output == dedent(
-        """+---------------------------------------+
-| connection_name | parameters          |
-|-----------------+---------------------|
-| test            | {'account': 'test'} |
-+---------------------------------------+
-    """
-    )
+    assert result.output == snapshot
 
 
 @pytest.fixture(scope="module")
