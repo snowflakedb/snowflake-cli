6a6b83f11988ec5477383916a11325ba4d0c9b3d
Pawel Job <pawel.job@snowflake.com>
2024-03-21 17:43:07 +0100

    [SNOW-1243569] Fixed snowpark build paths for builds with --project option

---
+++ src/snowflake/cli/plugins/snowpark/snowpark_package_paths.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+
+from snowflake.cli.api.project.schemas.snowpark.snowpark import Snowpark
+from snowflake.cli.api.secure_path import SecurePath
+
+_DEFINED_REQUIREMENTS = "requirements.txt"
+_REQUIREMENTS_SNOWFLAKE = "requirements.snowflake.txt"
+_REQUIREMENTS_OTHER = "requirements.other.txt"
+_PACKAGES_DIR = ".packages"
+
+
+@dataclass
+class SnowparkPackagePaths:
+    source: SecurePath
+    artifact_file: SecurePath
+    defined_requirements_file: SecurePath = SecurePath(_DEFINED_REQUIREMENTS)
+    snowflake_requirements_file: SecurePath = SecurePath(_REQUIREMENTS_SNOWFLAKE)
+    other_requirements_file: SecurePath = SecurePath(_REQUIREMENTS_OTHER)
+    downloaded_packages_dir: SecurePath = SecurePath(_PACKAGES_DIR)
+
+    @classmethod
+    def for_snowpark_project(
+        cls, project_root: SecurePath, snowpark_project_definition: Snowpark
+    ) -> "SnowparkPackagePaths":
+        defined_source_path = SecurePath(snowpark_project_definition.src)
+        return cls(
+            source=cls._get_snowpark_project_source_absolute_path(
+                project_root=project_root,
+                defined_source_path=defined_source_path,
+            ),
+            artifact_file=cls._get_snowpark_project_artifact_absolute_path(
+                project_root=project_root,
+                defined_source_path=defined_source_path,
+            ),
+            defined_requirements_file=project_root / _DEFINED_REQUIREMENTS,
+            snowflake_requirements_file=project_root / _REQUIREMENTS_SNOWFLAKE,
+            other_requirements_file=project_root / _REQUIREMENTS_OTHER,
+            downloaded_packages_dir=project_root / _PACKAGES_DIR,
+        )
+
+    @classmethod
+    def _get_snowpark_project_source_absolute_path(
+        cls, project_root: SecurePath, defined_source_path: SecurePath
+    ) -> SecurePath:
+        if defined_source_path.path.is_absolute():
+            return defined_source_path
+        return SecurePath((project_root / defined_source_path.path).path.resolve())
+
+    @classmethod
+    def _get_snowpark_project_artifact_absolute_path(
+        cls, project_root: SecurePath, defined_source_path: SecurePath
+    ) -> SecurePath:
+        source_path = cls._get_snowpark_project_source_absolute_path(
+            project_root=project_root, defined_source_path=defined_source_path
+        )
+        artifact_file = project_root / (source_path.path.name + ".zip")
+        return artifact_file
