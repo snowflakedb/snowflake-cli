6299b18f0bd76a8abbb68808ad7cf28a0fca5028
Patryk Czajka <patryk.czajka@snowflake.com>
2024-03-27 17:33:03 +0100

    fix package availability parsing in Anaconda

--- src/snowflake/cli/plugins/snowpark/package/anaconda.py
+++ src/snowflake/cli/plugins/snowpark/package/anaconda.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 
 import logging
-from typing import List
+from typing import Dict, List, Set
 
 import requests
 from click import ClickException
+from packaging.requirements import Requirement as PkgRequirement
 from packaging.version import parse
 from requests import HTTPError
-from snowflake.cli.plugins.snowpark.models import Requirement, SplitRequirements
+from snowflake.cli.plugins.snowpark.models import (
+    Requirement,
+    SplitRequirements,
+    WheelMetadata,
+)
 
 log = logging.getLogger(__name__)
+
+
+def _standarize_name(name: str) -> str:
+    return WheelMetadata.to_wheel_name_format(name.lower())
 
 
 class AnacondaChannel:
@@ -17,31 +26,43 @@ class AnacondaChannel:
         "https://repo.anaconda.com/pkgs/snowflake/channeldata.json"
     )
 
-    def __init__(self, packages):
-        self._packages = packages
+    def __init__(self, packages: Dict[str, Set[str]]):
+        """[packages] should be a dictionary mapping package name to set of its available versions"""
+        self._packages = {
+            _standarize_name(package_name): {parse(ver) for ver in versions}
+            for package_name, versions in packages.items()
+        }
 
     def is_package_available(
         self, package: Requirement, skip_version_check: bool = False
     ) -> bool:
         if not package.name:
             return False
-        package_name = package.name.lower()
+        package_name = _standarize_name(package.name)
         if package_name not in self._packages:
             return False
-        if package.specs and not skip_version_check:
-            latest_ver = parse(self._packages[package_name]["version"])
-            return all([parse(spec[1]) <= latest_ver for spec in package.specs])
-        return True
+        if skip_version_check or not package.specs:
+            return True
 
-    def package_version(self, package: Requirement):
-        return self._packages[package.name.lower()].get("version")
+        package_specifiers = PkgRequirement(package.line).specifier
+        return any(
+            version in package_specifiers for version in self._packages[package_name]
+        )
+
+    def package_latest_version(self, package: Requirement) -> str:
+        return str(max(self._packages[_standarize_name(package.name)]))
 
     @classmethod
     def from_snowflake(cls):
         try:
             response = requests.get(AnacondaChannel.snowflake_channel_url)
             response.raise_for_status()
-            return cls(packages=response.json()["packages"])
+            return cls(
+                packages={
+                    package["name"].lower(): {package["version"]}
+                    for package in response.json()["packages"]
+                }
+            )
         except HTTPError as err:
             raise ClickException(
                 f"Accessing Snowflake Anaconda channel failed. Reason {err}"
---
+++ tests/snowpark/test_anaconda.py
@@ -0,0 +1,31 @@
+from snowflake.cli.plugins.snowpark.models import Requirement
+from snowflake.cli.plugins.snowpark.package.anaconda import AnacondaChannel
+
+ANACONDA = AnacondaChannel(
+    packages={
+        "SHRUBBERY": {"1.2.1", "1.2.2"},
+        "dummy-pkg": {"0.1.1", "1.0.0", "1.1.0"},
+    }
+)
+
+
+def _r(line):
+    return Requirement.parse(line)
+
+
+def test_latest_version():
+    assert ANACONDA.package_latest_version(_r("shrubbery")) == "1.2.2"
+    assert ANACONDA.package_latest_version(_r("dummy_pkg")) == "1.1.0"
+    assert ANACONDA.package_latest_version(_r("dummy-pkg")) == "1.1.0"
+
+
+def test_package_availability():
+    assert ANACONDA.is_package_available(_r("shrubbery"))
+    assert ANACONDA.is_package_available(_r("DUMMY_pkg"))
+    assert not ANACONDA.is_package_available(_r("shrubbery>=2"))
+    assert not ANACONDA.is_package_available(_r("shrubbery<=1.0.0"))
+    assert ANACONDA.is_package_available(_r("shrubbery==1.2.1"))
+    assert ANACONDA.is_package_available(_r("shrubbery>1,<4"))
+    assert not ANACONDA.is_package_available(_r("shrubbery!=1.2.*"))
+    assert ANACONDA.is_package_available(_r("dummy-pkg==0.*"))
+    assert not ANACONDA.is_package_available(_r("weird-package"))
