117ca99e50baec5cfe41ee6ed9fdee6c7fb02c8f
Patryk Czajka <patryk.czajka@snowflake.com>
2024-03-27 18:00:11 +0100

    fix non-pep508 version formats

--- tests/snowpark/test_anaconda.py
+++ tests/snowpark/test_anaconda.py
@@ -5,6 +5,7 @@ ANACONDA = AnacondaChannel(
     packages={
         "SHRUBBERY": {"1.2.1", "1.2.2"},
         "dummy-pkg": {"0.1.1", "1.0.0", "1.1.0"},
+        "jpeg": {"9e", "9d", "9b"},
     }
 )
 
@@ -17,6 +18,8 @@ def test_latest_version():
     assert ANACONDA.package_latest_version(_r("shrubbery")) == "1.2.2"
     assert ANACONDA.package_latest_version(_r("dummy_pkg")) == "1.1.0"
     assert ANACONDA.package_latest_version(_r("dummy-pkg")) == "1.1.0"
+    assert ANACONDA.package_latest_version(_r("jpeg")) == "9e"
+    assert ANACONDA.package_latest_version(_r("weird-pkg")) is None
 
 
 def test_package_availability():
@@ -29,3 +32,7 @@ def test_package_availability():
     assert not ANACONDA.is_package_available(_r("shrubbery!=1.2.*"))
     assert ANACONDA.is_package_available(_r("dummy-pkg==0.*"))
     assert not ANACONDA.is_package_available(_r("weird-package"))
+    assert ANACONDA.is_package_available(_r("jpeg"))
+
+    # safe-fail for non-pep508 version formats
+    assert not ANACONDA.is_package_available(_r("jpeg==9d"))
