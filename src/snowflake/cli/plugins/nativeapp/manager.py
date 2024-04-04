59cbc8a1705491f5f9f27e646c2aeeb78fb9f9c4
Luis Montero Marin <luis.monteromarin@snowflake.com>
2024-04-04 14:48:42 -0600

    Prevent error when session token is provided but role is not

--- src/snowflake/cli/plugins/nativeapp/manager.py
+++ src/snowflake/cli/plugins/nativeapp/manager.py
@@ -49,6 +49,7 @@ from snowflake.cli.plugins.stage.diff import (
     sync_local_diff_with_stage,
 )
 from snowflake.connector import ProgrammingError
+from snowflake.connector.cursor import DictCursor
 
 
 def generic_sql_error_handler(
@@ -181,7 +182,7 @@ class NativeAppManager(SqlExecutionMixin):
         if self.definition.package and self.definition.package.role:
             return self.definition.package.role
         else:
-            return default_role()
+            return self._default_role
 
     @cached_property
     def package_distribution(self) -> str:
@@ -202,7 +203,20 @@ class NativeAppManager(SqlExecutionMixin):
         if self.definition.application and self.definition.application.role:
             return self.definition.application.role
         else:
-            return default_role()
+            return self._default_role
+
+    @cached_property
+    def _default_role(self) -> str:
+        role = default_role()
+        if role is None:
+            role = self._get_current_role()
+        return role
+
+    def _get_current_role(self) -> str:
+        role_result = self._execute_query(
+            "select current_role()", cursor_class=DictCursor
+        ).fetchone()
+        return role_result["CURRENT_ROLE()"]
 
     @cached_property
     def debug_mode(self) -> bool:
