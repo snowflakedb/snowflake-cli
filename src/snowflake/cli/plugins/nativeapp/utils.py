6acfe078d56487e4cae48e78e98ff8f56b000901
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2024-04-27 07:59:51 +0200

    Add pre-commit check for from future

--- src/snowflake/cli/plugins/nativeapp/utils.py
+++ src/snowflake/cli/plugins/nativeapp/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from os import PathLike
 from pathlib import Path
 from sys import stdin, stdout
--- tests/nativeapp/test_artifacts.py
+++ tests/nativeapp/test_artifacts.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 import os
+
 from pathlib import Path
 from typing import List, Optional
 
