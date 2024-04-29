07168bbfd0e28eff35a9703949f84b8ca6c3f372
Bhumika Goel <bhumika.goel@snowflake.com>
2024-04-29 16:56:57 -0500

    Feedback round 3 - property bags for processors

--- src/snowflake/cli/api/project/schemas/native_app/path_mapping.py
+++ src/snowflake/cli/api/project/schemas/native_app/path_mapping.py
@@ -1,30 +1,17 @@
 from __future__ import annotations
 
-from typing import List, Literal, Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 from pydantic import Field
 from snowflake.cli.api.project.schemas.updatable_model import UpdatableModel
-
-# Limited to python options only for now.
-# Also, processor name can only be "snowpark" for now.
-VirtualEnvTypeOptions = Literal["conda", "venv"]
-
-
-class VirtualEnvironment(UpdatableModel):
-    name: str = Field(
-        title="Name of the virtual environment to run the processor in.",
-    )
-    env_type: VirtualEnvTypeOptions = Field(
-        title="Type of the virtual environment to activate.",
-    )
 
 
 class AnnotationProcessor(UpdatableModel):
     name: str = Field(
         title="Name of the processor to be invoked to discover the annotated code."
     )
-    virtual_env: Optional[VirtualEnvironment] = Field(
-        title="The virtual environment to run the processor in. If none is provided, then the Snowflake CLI will try to get the current environment information.",
+    properties: Optional[Dict[str, Any]] = Field(
+        title="Properties to aid the invocation of the processor on the annotated code. If none are provided, then the Snowflake CLI will try to get the current environment information.",
         default=None,
     )
 
--- tests/nativeapp/test_annotation_processor_config.py
+++ tests/nativeapp/test_annotation_processor_config.py
@@ -2,7 +2,6 @@ import pytest
 from snowflake.cli.api.project.definition import (
     load_project_definition,
 )
-from snowflake.cli.api.project.errors import SchemaValidationError
 
 
 @pytest.mark.parametrize(
@@ -10,26 +9,19 @@ from snowflake.cli.api.project.errors import SchemaValidationError
 )
 def test_napp_project_with_annotation_processor(project_definition_files):
     project = load_project_definition(project_definition_files)
-    assert len(project.native_app.artifacts) == 3
+    assert len(project.native_app.artifacts) == 4
+
     result = project.native_app.artifacts[2]
     assert len(result.processors) == 3
     assert result.processors[0] == "snowpark_scala"
     assert result.processors[1].name == "snowpark_java"
-    assert result.processors[1].virtual_env is None
+    assert result.processors[1].properties is None
     assert result.processors[2].name == "snowpark"
-    assert result.processors[2].virtual_env.name == "snowpark_venv"
-    assert result.processors[2].virtual_env.env_type == "venv"
+    assert result.processors[2].properties["venv_path"] == "~/Users/jdoe/snowpark_venv"
+    assert result.processors[2].properties["env_type"] == "venv"
 
-
-@pytest.mark.parametrize(
-    "project_definition_files",
-    ["napp_with_annotation_processor_failure"],
-    indirect=True,
-)
-def test_napp_project_with_annotation_processor_failure(project_definition_files):
-    with pytest.raises(SchemaValidationError) as exc_info:
-        load_project_definition(project_definition_files)
-
-    assert "Your project definition is missing following fields: ('env_type',)" in str(
-        exc_info.value
-    )
+    result = project.native_app.artifacts[3]
+    assert len(result.processors) == 1
+    assert result.processors[0].name == "snowpark"
+    assert result.processors[0].properties["name"] == "snowpark_conda"
+    assert result.processors[0].properties["env_type"] == "conda"
