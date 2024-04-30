822ee31f6feedf6ac92b32e73e0f80bfe0a5b481
Jan Sikorski <jan.sikorski@snowflake.com>
2024-04-30 10:51:17 +0200

    Added docstring

--- src/snowflake/cli/api/project/schemas/updatable_model.py
+++ src/snowflake/cli/api/project/schemas/updatable_model.py
@@ -15,7 +15,15 @@ class UpdatableModel(BaseModel):
         except ValidationError as e:
             raise SchemaValidationError(e)
 
-    def update_from_dict(self, update_values: Dict[str, Any]) -> object:
+    def update_from_dict(self, update_values: Dict[str, Any]):
+        """
+        Takes a dictionary with values to override.
+        If the field type is subclass of a UpdatableModel, it's update_from_dict() method is called with
+        the value to be set.
+        If not, we use simple setattr to set new value.
+        Values provided are validated against original restrictions, so it's impossible to overwrite string field with
+        integer value etc.
+        """
         for field, value in update_values.items():
             if field in self.model_fields.keys():
                 if (
@@ -28,11 +36,16 @@ class UpdatableModel(BaseModel):
         return self
 
     def _is_field_an_updatable_model(self, name: str) -> bool:
-        typing_types = (Literal, list)
+        """
+        As some fields are set to Optional, we cannot rely on simple isSubclass() call, to determine
+        whether certain field can contain an UpdatableModel.
+        Therefore we need to check the args of type annotation, if any of them is a subclass of UpdatableModel
+        """
+        false_positives = (Literal, list)
         field_type = self.model_fields.get(name, None)
         if field_type:
             if (args := get_args(field_type.annotation)) and not any(
-                [get_origin(arg) in typing_types for arg in args]
+                [get_origin(arg) in false_positives for arg in args]
             ):
                 return any([issubclass(arg, UpdatableModel) for arg in args])
             return issubclass(type(field_type.annotation), UpdatableModel)
