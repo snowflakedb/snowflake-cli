39327edbbdfb91f7c953c6bfae617c98c0c96f70
Bhumika Goel <bhumika.goel@snowflake.com>
2024-04-30 11:57:31 -0500

    improve comments

--- src/snowflake/cli/api/project/schemas/native_app/path_mapping.py
+++ src/snowflake/cli/api/project/schemas/native_app/path_mapping.py
@@ -8,10 +8,10 @@ from snowflake.cli.api.project.schemas.updatable_model import UpdatableModel
 
 class AnnotationProcessor(UpdatableModel):
     name: str = Field(
-        title="Name of the processor to be invoked to discover the annotated code."
+        title="Name of a processor to invoke on a collection of artifacts."
     )
     properties: Optional[Dict[str, Any]] = Field(
-        title="Properties to aid the invocation of the processor on the annotated code. If none are provided, then the Snowflake CLI will try to get the current environment information.",
+        title="A set of key-value pairs used to configure the output of the processor. Consult a specific processor's documentation for more details on the supported properties.",
         default=None,
     )
 
