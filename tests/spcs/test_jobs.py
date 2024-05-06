eba840e44640cfbd7df1bf931f9ddeab7da1587f
zaramzamzam <patrick.teufert@easypark.net>
2024-05-06 12:19:55 +0200

    add test

--- tests/spcs/test_jobs.py
+++ tests/spcs/test_jobs.py
@@ -35,8 +35,49 @@ spec:
     assert ctx.get_query() == (
         "USE DATABASE MockDatabase\n"
         "USE MockDatabase.MockSchema\n"
-        "EXECUTE SERVICE\n"
+        "EXECUTE JOB SERVICE\n"
         "IN COMPUTE POOL testPool\n"
+        "FROM SPECIFICATION $$\n"
+        '{"spec": {"containers": [{"name": "main", "image": "public.ecr.aws/myrepo:latest"}]}}\n'
+        "$$\n"
+    )
+
+
+@pytest.mark.skip("Snowpark Container Services Job not supported.")
+@mock.patch("snowflake.connector.connect")
+def test_create_job_with_name(mock_connector, runner, mock_ctx):
+    ctx = mock_ctx()
+    mock_connector.return_value = ctx
+
+    test_spec = """
+spec:
+  containers:
+  - name: main
+    image: public.ecr.aws/myrepo:latest
+    """
+    with TemporaryDirectory() as temp_dir:
+        filepath = os.path.join(temp_dir, "test")
+        with open(filepath, "w") as fh:
+            fh.write(test_spec)
+        runner.invoke(
+            [
+                "spcs",
+                "job",
+                "create",
+                "--compute-pool",
+                "testPool",
+                "--spec-path",
+                filepath,
+                "--name",
+                "test_job_name",
+            ]
+        )
+    assert ctx.get_query() == (
+        "USE DATABASE MockDatabase\n"
+        "USE MockDatabase.MockSchema\n"
+        "EXECUTE JOB SERVICE\n"
+        "IN COMPUTE POOL testPool\n"
+        "NAME=test_job_name"
         "FROM SPECIFICATION $$\n"
         '{"spec": {"containers": [{"name": "main", "image": "public.ecr.aws/myrepo:latest"}]}}\n'
         "$$\n"
