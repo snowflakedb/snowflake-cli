3578d3a934fefb2bdc4c581cbc90500da3b6c87b
Marcin Raba <marcin.raba@snowflake.com>
2024-05-16 18:37:13 +0200

    SNOW-1304163: integration test

--- tests_integration/notebook/test_notebooks.py
+++ tests_integration/notebook/test_notebooks.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from snowflake.connector import ProgrammingError
 
@@ -23,3 +25,30 @@ def test_execute_notebook_failure(runner, test_database, snowflake_session):
         )
         assert result.exit_code == 1
         assert "invalid identifier 'FOO'" in err
+
+
+@pytest.mark.integration_experimental
+def test_create_notebook(runner, test_database, snowflake_session):
+    notebook_name = "my_notebook"
+    stage_name = "notebook_stage"
+    stage_path = f"@{stage_name}/{notebook_name}"
+    notebook_file = Path(__file__).parent / "test_data/notebook/my_notebook.ipynb"
+
+    snowflake_session.execute_string(
+        f"create stage {stage_name};"
+        f"put file://{notebook_file.absolute()} @{stage_name};"
+    )
+
+    result = runner.invoke_with_connection_json(
+        (
+            "notebook",
+            "create",
+            notebook_name,
+            "--notebook-file",
+            stage_path,
+            "--format",
+            "json",
+        )
+    )
+    assert result.exit_code == 0
+    assert result.json == {"message": "test!messages"}
