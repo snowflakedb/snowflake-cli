28e7c639ab12735e9f5f5750d3596d77c45c6398
Bhumika Goel <bhumika.goel@snowflake.com>
2024-05-21 15:33:14 -0500

    feedback round 3

--- tests/nativeapp/codegen/test_compiler.py
+++ tests/nativeapp/codegen/test_compiler.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from pathlib import Path
 
 import pytest
@@ -11,41 +10,47 @@ from snowflake.cli.plugins.nativeapp.codegen.snowpark.python_processor import (
     SnowparkAnnotationProcessor,
 )
 
-proj_def = ProjectDefinition(
-    **{
-        "definition_version": "1",
-        "native_app": {
-            "artifacts": [
-                {"dest": "./", "src": "app/*"},
-                "app/setup.sql",
-                {"dest": "./", "src": "app/*", "processors": ["DUMMY"]},
-                {"dest": "./", "src": "app/*", "processors": ["SNOWPARK"]},
-                {
-                    "dest": "./",
-                    "src": "app/*",
-                    "processors": [{"name": "SNOWPARK"}],
+
+@pytest.fixture()
+def test_proj_def():
+    return ProjectDefinition(
+        **{
+            "definition_version": "1",
+            "native_app": {
+                "artifacts": [
+                    {"dest": "./", "src": "app/*"},
+                    "app/setup.sql",
+                    {"dest": "./", "src": "app/*", "processors": ["DUMMY"]},
+                    {"dest": "./", "src": "app/*", "processors": ["SNOWPARK"]},
+                    {
+                        "dest": "./",
+                        "src": "app/*",
+                        "processors": [{"name": "SNOWPARK"}],
+                    },
+                ],
+                "name": "napp_test",
+                "package": {
+                    "scripts": [
+                        "package/001.sql",
+                    ]
                 },
-            ],
-            "name": "napp_test",
-            "package": {
-                "scripts": [
-                    "package/001.sql",
-                ]
             },
-        },
-    }
-)
-
-compiler = NativeAppCompiler(
-    project_definition=proj_def.native_app,
-    project_root=Path("some/dummy/path"),
-    deploy_root=Path("some/dummy/path"),
-)
+        }
+    )
 
 
-def test_try_create_processor_returns_none():
-    artifact_to_process = proj_def.native_app.artifacts[2]
-    result = compiler._try_create_processor(  # noqa: SLF001
+@pytest.fixture()
+def test_compiler(test_proj_def):
+    return NativeAppCompiler(
+        project_definition=test_proj_def.native_app,
+        project_root=Path("some/dummy/path"),
+        deploy_root=Path("some/dummy/path"),
+    )
+
+
+def test_try_create_processor_returns_none(test_proj_def, test_compiler):
+    artifact_to_process = test_proj_def.native_app.artifacts[2]
+    result = test_compiler._try_create_processor(  # noqa: SLF001
         processor_mapping=artifact_to_process.processors[0],
         artifact_to_process=artifact_to_process,
     )
@@ -53,19 +58,21 @@ def test_try_create_processor_returns_none():
 
 
 @pytest.mark.parametrize(
-    "artifact_to_process",
-    [proj_def.native_app.artifacts[3], proj_def.native_app.artifacts[4]],
+    "artifact_index",
+    [3, 4],
 )
-def test_try_create_processor_returns_processor(artifact_to_process):
-    result = compiler._try_create_processor(  # noqa: SLF001
-        processor_mapping=artifact_to_process.processors[0],
-        artifact_to_process=artifact_to_process,
+def test_try_create_processor_returns_processor(
+    artifact_index, test_proj_def, test_compiler
+):
+    mapping = test_proj_def.native_app.artifacts[artifact_index]
+    result = test_compiler._try_create_processor(  # noqa: SLF001
+        processor_mapping=mapping.processors[0],
+        artifact_to_process=mapping,
     )
     assert isinstance(result, SnowparkAnnotationProcessor)
 
 
-def test_find_and_execute_processors_exception():
-    test_proj_def = deepcopy(proj_def)
+def test_find_and_execute_processors_exception(test_proj_def, test_compiler):
     test_proj_def.native_app.artifacts = [
         {"dest": "./", "src": "app/*", "processors": ["DUMMY"]}
     ]
