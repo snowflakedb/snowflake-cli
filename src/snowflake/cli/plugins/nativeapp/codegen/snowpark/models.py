00e7fcb1ac4d1f65b042e8b44288875e7d379d6a
Bruno Dufour <bruno.dufour@snowflake.com>
2024-05-22 16:32:46 -0400

    Clarified lineno field

--- src/snowflake/cli/plugins/nativeapp/codegen/snowpark/models.py
+++ src/snowflake/cli/plugins/nativeapp/codegen/snowpark/models.py
@@ -21,7 +21,7 @@ class NativeAppExtensionFunction(_CallableBase):
         alias="type",
     )
     lineno: Optional[int] = Field(
-        title="The line number of the extension function", default=None
+        title="The start line number of the extension function (1-based)", default=None
     )
     name: Optional[str] = Field(
         title="The name of the extension function", default=None
