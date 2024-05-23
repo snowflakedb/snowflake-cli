bed0942f8a110953bf2101876898ad3d1c4a56fc
Patryk Czajka <patryk.czajka@snowflake.com>
2024-05-23 16:18:24 +0200

    refactor

--- tests/test_help_messages.py
+++ tests/test_help_messages.py
@@ -1,21 +1,29 @@
+from __future__ import annotations
+
+from typing import List
+
 import pytest
 from snowflake.cli.app.commands_registration.command_plugins_loader import (
     load_only_builtin_command_plugins,
 )
+from typer.core import TyperGroup
 
 
-def iter_through_all_commands_and_subcommands_paths():
+def iter_through_all_commands(command_groups_only: bool = False):
     """
     Generator iterating through all commands.
     Paths are yielded as List[str]
     """
     ignore_plugins = ["render"]
 
-    no_command = []
+    no_command: List[str] = []
     yield no_command
 
     def _iter_through_commands(command, path):
-        yield list(path)
+        print(path, type(command))
+        if not command_groups_only or isinstance(command, TyperGroup):
+            yield list(path)
+
         for subpath, subcommand in getattr(command, "commands", {}).items():
             path.append(subpath)
             yield from _iter_through_commands(subcommand, path)
@@ -32,10 +40,10 @@ def iter_through_all_commands_and_subcommands_paths():
 
 @pytest.mark.parametrize(
     "command",
-    iter_through_all_commands_and_subcommands_paths(),
-    ids=(".".join(cmd) for cmd in iter_through_all_commands_and_subcommands_paths()),
+    iter_through_all_commands(),
+    ids=(".".join(cmd) for cmd in iter_through_all_commands()),
 )
-def test_help_messages_commands_with_subcommands(runner, snapshot, command):
+def test_help_messages(runner, snapshot, command):
     """
     Check help messages against the snapshot
     """
@@ -44,29 +52,12 @@ def test_help_messages_commands_with_subcommands(runner, snapshot, command):
     assert result.output == snapshot
 
 
-def iter_through_all_commands_paths():
-    """
-    Generator iterating through all commands.
-    Paths are yielded as List[str]
-    """
-    ignore_plugins = ["render", "sql"]
-
-    no_command = []
-    yield no_command
-
-    builtin_plugins = load_only_builtin_command_plugins()
-    for plugin in builtin_plugins:
-        spec = plugin.command_spec
-        if not plugin.plugin_name in ignore_plugins:
-            yield spec.full_command_path.path_segments
-
-
 @pytest.mark.parametrize(
     "command",
-    iter_through_all_commands_paths(),
-    ids=(".".join(cmd) for cmd in iter_through_all_commands_paths()),
+    iter_through_all_commands(command_groups_only=True),
+    ids=(".".join(cmd) for cmd in iter_through_all_commands(command_groups_only=True)),
 )
-def test_help_messages_command_only(runner, snapshot, command):
+def test_help_messages_no_help_flag(runner, snapshot, command):
     """
     Check help messages against the snapshot
     """
