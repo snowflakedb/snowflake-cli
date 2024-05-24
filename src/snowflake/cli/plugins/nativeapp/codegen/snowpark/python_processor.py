ab0fce0c6b720ad76d6e1b1092ca62d7bce74502
Bruno Dufour <bruno.dufour@snowflake.com>
2024-05-24 16:52:02 -0400

    Fixed spelling

--- src/snowflake/cli/plugins/nativeapp/codegen/snowpark/python_processor.py
+++ src/snowflake/cli/plugins/nativeapp/codegen/snowpark/python_processor.py
@@ -44,7 +44,7 @@ from snowflake.cli.plugins.stage.diff import to_stage_path
 DEFAULT_TIMEOUT = 30
 TEMPLATE_PATH = Path(__file__).parent / "callback_source.py.jinja"
 SNOWPARK_LIB_NAME = "snowflake-snowpark-python"
-SNOWPARK_LIB_REGEX = rf"'{SNOWPARK_LIB_NAME}((<|<=|!=|==|>=|>|~=|===)[a-zA-Z0-9_.*+!-]+)?'"  # support PEP 508, even though not all of it is support in Snowflake yet
+SNOWPARK_LIB_REGEX = rf"'{SNOWPARK_LIB_NAME}((<|<=|!=|==|>=|>|~=|===)[a-zA-Z0-9_.*+!-]+)?'"  # support PEP 508, even though not all of it is supported in Snowflake yet
 STAGE_PREFIX = "@"
 
 
