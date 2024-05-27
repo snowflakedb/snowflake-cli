45432973a268b029335aacbd06fb94f6364ed72a
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2024-05-27 13:17:56 +0200

    Validate project definition file using proper version schema (#1106)

--- src/snowflake/cli/api/project/errors.py
+++ src/snowflake/cli/api/project/errors.py
@@ -6,14 +6,14 @@ from pydantic import ValidationError
 class SchemaValidationError(Exception):
     generic_message = "For field {loc} you provided '{loc}'. This caused: {msg}"
     message_templates = {
-        "string_type": "{msg} for field '{loc}', you provided '{input}'",
-        "extra_forbidden": "{msg}. You provided field '{loc}' with value '{input}' that is not present in the schema",
+        "string_type": "{msg} for field '{loc}', you provided '{input}'.",
+        "extra_forbidden": "{msg}. You provided field '{loc}' with value '{input}' that is not supported in given version.",
         "missing": "Your project definition is missing following fields: {loc}",
     }
 
     def __init__(self, error: ValidationError):
         errors = error.errors()
-        message = f"During evaluation of {error.title} schema following errors were encountered:\n"
+        message = f"During evaluation of {error.title} in project definition following errors were encountered:\n"
         message += "\n".join(
             [
                 self.message_templates.get(e["type"], self.generic_message).format(**e)
