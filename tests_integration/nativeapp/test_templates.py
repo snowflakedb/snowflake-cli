f5cbda9b83a190d48d9007620607a2eef417fcb0
Patryk Czajka <patryk.czajka@snowflake.com>
2024-05-27 13:32:28 +0200

    skip NADE test on Windows

--- tests_integration/nativeapp/test_templates.py
+++ tests_integration/nativeapp/test_templates.py
@@ -1,7 +1,9 @@
 import pytest
+from snowflake.connector.compat import IS_WINDOWS
 
 
 @pytest.mark.integration
+@pytest.mark.skipif(IS_WINDOWS, reason="Permissions issue on Windows")
 def test_list_templates_no_options_success(runner, snapshot):
     args = ["app", "list-templates"]
     result = runner.invoke(args)
