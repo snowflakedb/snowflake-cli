5d8d96bf5cb6081015771baee3a263c979ce6d52
Patryk Czajka <patryk.czajka@snowflake.com>
2024-05-28 17:03:22 +0200

    object

--- src/snowflake/cli/plugins/object/__init__.py
+++
@@ -1,9 +0,0 @@
-from snowflake.cli.api.commands.snow_typer import SnowTyper
-from snowflake.cli.plugins.object.commands import app as show_app
-
-app = SnowTyper(
-    name="object",
-    help="Manages Snowflake objects like warehouses and stages",
-)
-
-app.add_typer(show_app)  # type: ignore
