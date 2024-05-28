927cad0b64931c4a0a90b1f3e61d47ca652e9508
Patryk Czajka <patryk.czajka@snowflake.com>
2024-05-28 11:39:19 +0200

    NADE + plugins: fix unit tests failing without internet connection (#1117)
    * Fix unit tests failing without internet connection
    
    * nativeapps: move templates test to integration dir
    
    * move plugin tests to integration
    
    * fix imtegration tests
    
    * skip NADE test on Windows
    
    * fix test_failing_plugin

--- tests_integration/plugin/test_failing_plugin.py
+++ tests_integration/plugin/test_failing_plugin.py
@@ -3,16 +3,17 @@ from textwrap import dedent
 import pytest
 
 
+@pytest.mark.integration
 def test_failing_plugin(runner, test_root_path, _install_plugin, caplog):
     config_path = (
-        test_root_path / "test_data" / "configs" / "failing_plugin_config.toml"
+        test_root_path / "config" / "plugin_tests" / "failing_plugin_config.toml"
     )
 
     result = runner.invoke(["--config-file", config_path, "connection", "list"])
 
     assert (
-        caplog.messages[0]
-        == "Cannot register plugin [failing_plugin]: Some error in plugin"
+        "Cannot register plugin [failing_plugin]: Some error in plugin"
+        in caplog.messages
     )
     assert result.output == dedent(
         """\
