90bb4a0ccc6da55903c6d3e6fc838350b6cfcc9b
Patryk Czajka <patryk.czajka@snowflake.com>
2024-05-28 16:50:15 +0200

    stage: split commands and implementation

--- src/snowflake/cli/plugins/stage/commands.py
+++ src/snowflake/cli/plugins/stage/commands.py
@@ -31,12 +31,12 @@ from snowflake.cli.plugins.object.command_aliases import (
 from snowflake.cli.plugins.stage.diff import DiffResult, compute_stage_diff
 from snowflake.cli.plugins.stage.manager import OnErrorType, StageManager
 
+StageNameArgument = typer.Argument(..., help="Name of the stage.", show_default=False)
+
 app = SnowTyper(
     name="stage",
     help="Manages stages.",
 )
-
-StageNameArgument = typer.Argument(..., help="Name of the stage.", show_default=False)
 
 add_object_command_aliases(
     app=app,
@@ -50,18 +50,17 @@ add_object_command_aliases(
 
 
 @app.command("list-files", requires_connection=True)
-def stage_list_files(
+def stage_list_files_cmd(
     stage_name: str = StageNameArgument, pattern=PatternOption, **options
 ) -> CommandResult:
     """
     Lists the stage contents.
     """
-    cursor = StageManager().list_files(stage_name=stage_name, pattern=pattern)
-    return QueryResult(cursor)
+    return stage_list_files(stage_name, pattern, **options)
 
 
 @app.command("copy", requires_connection=True)
-def copy(
+def copy_cmd(
     source_path: str = typer.Argument(
         help="Source path for copy operation. Can be either stage path or local.",
         show_default=False,
@@ -88,6 +87,108 @@ def copy(
     Copies all files from target path to target directory. This works for both uploading
     to and downloading files from the stage.
     """
+    return copy(
+        source_path=source_path,
+        destination_path=destination_path,
+        overwrite=overwrite,
+        parallel=parallel,
+        recursive=recursive,
+        **options,
+    )
+
+
+@app.command("create", requires_connection=True)
+def stage_create_cmd(stage_name: str = StageNameArgument, **options) -> CommandResult:
+    """
+    Creates a named stage if it does not already exist.
+    """
+    return stage_create(stage_name=stage_name, **options)
+
+
+@app.command("remove", requires_connection=True)
+def stage_remove_cmd(
+    stage_name: str = StageNameArgument,
+    file_name: str = typer.Argument(
+        ...,
+        help="Name of the file to remove.",
+        show_default=False,
+    ),
+    **options,
+) -> CommandResult:
+    """
+    Removes a file from a stage.
+    """
+    return stage_remove(stage_name=stage_name, file_name=file_name, **options)
+
+
+@app.command("diff", hidden=True, requires_connection=True)
+def stage_diff_cmd(
+    stage_name: str = typer.Argument(
+        help="Fully qualified name of a stage",
+        show_default=False,
+    ),
+    folder_name: str = typer.Argument(
+        help="Path to local folder",
+        show_default=False,
+    ),
+    **options,
+) -> ObjectResult:
+    """
+    Diffs a stage with a local folder.
+    """
+    return stage_diff(stage_name=stage_name, folder_name=folder_name, **options)
+
+
+@app.command("execute", requires_connection=True)
+def execute_cmd(
+    stage_path: str = typer.Argument(
+        ...,
+        help="Stage path with files to be execute. For example `@stage/dev/*`.",
+        show_default=False,
+    ),
+    on_error: OnErrorType = OnErrorOption,
+    variables: Optional[List[str]] = VariablesOption,
+    **options,
+) -> CollectionResult:
+    """
+    Execute immediate all files from the stage path. Files can be filtered with glob like pattern,
+    e.g. `@stage/*.sql`, `@stage/dev/*`. Only files with `.sql` extension will be executed.
+    """
+    return execute(
+        stage_path=stage_path, on_error=on_error, variables=variables, **options
+    )
+
+
+def stage_list_files(
+    stage_name: str, pattern: Optional[str], **options
+) -> CommandResult:
+    cursor = StageManager().list_files(stage_name=stage_name, pattern=pattern)
+    return QueryResult(cursor)
+
+
+def copy(
+    source_path: str = typer.Argument(
+        help="Source path for copy operation. Can be either stage path or local.",
+        show_default=False,
+    ),
+    destination_path: str = typer.Argument(
+        help="Target directory path for copy operation. Should be stage if source is local or local if source is stage.",
+        show_default=False,
+    ),
+    overwrite: bool = typer.Option(
+        False,
+        help="Overwrites existing files in the target path.",
+    ),
+    parallel: int = typer.Option(
+        4,
+        help="Number of parallel threads to use when uploading files.",
+    ),
+    recursive: bool = typer.Option(
+        False,
+        help="Copy files recursively with directory structure.",
+    ),
+    **options,
+) -> CommandResult:
     is_get = is_stage_path(source_path)
     is_put = is_stage_path(destination_path)
 
@@ -116,67 +217,37 @@ def copy(
     )
 
 
-@app.command("create", requires_connection=True)
-def stage_create(stage_name: str = StageNameArgument, **options) -> CommandResult:
-    """
-    Creates a named stage if it does not already exist.
-    """
+def stage_create(stage_name: str, **options) -> CommandResult:
     cursor = StageManager().create(stage_name=stage_name)
     return SingleQueryResult(cursor)
 
 
-@app.command("remove", requires_connection=True)
-def stage_remove(
-    stage_name: str = StageNameArgument,
-    file_name: str = typer.Argument(
-        ...,
-        help="Name of the file to remove.",
-        show_default=False,
-    ),
-    **options,
-) -> CommandResult:
-    """
-    Removes a file from a stage.
-    """
-
+def stage_remove(stage_name: str, file_name: str, **options) -> CommandResult:
     cursor = StageManager().remove(stage_name=stage_name, path=file_name)
     return SingleQueryResult(cursor)
 
 
-@app.command("diff", hidden=True, requires_connection=True)
 def stage_diff(
-    stage_name: str = typer.Argument(
-        help="Fully qualified name of a stage",
-        show_default=False,
-    ),
-    folder_name: str = typer.Argument(
-        help="Path to local folder",
-        show_default=False,
-    ),
+    stage_name: str,
+    folder_name: str,
     **options,
 ) -> ObjectResult:
     """
     Diffs a stage with a local folder.
     """
+
     diff: DiffResult = compute_stage_diff(Path(folder_name), stage_name)
     return ObjectResult(str(diff))
 
 
-@app.command("execute", requires_connection=True)
 def execute(
-    stage_path: str = typer.Argument(
-        ...,
-        help="Stage path with files to be execute. For example `@stage/dev/*`.",
-        show_default=False,
-    ),
-    on_error: OnErrorType = OnErrorOption,
-    variables: Optional[List[str]] = VariablesOption,
-    **options,
-):
+    stage_path: str, on_error: OnErrorType, variables: Optional[List[str]], **options
+) -> CollectionResult:
     """
     Execute immediate all files from the stage path. Files can be filtered with glob like pattern,
     e.g. `@stage/*.sql`, `@stage/dev/*`. Only files with `.sql` extension will be executed.
     """
+
     results = StageManager().execute(
         stage_path=stage_path, on_error=on_error, variables=variables
     )
