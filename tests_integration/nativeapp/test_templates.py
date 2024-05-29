7a9d15fa118a5e2e5139500486a508ac96302966
Bruno Dufour <bruno.dufour@snowflake.com>
2024-05-29 09:59:23 -0400

    Correctly handle read-only files when cleaning up in `snow app list-templates` (#1138)

--- tests_integration/nativeapp/test_templates.py
+++ tests_integration/nativeapp/test_templates.py
@@ -1,12 +1,18 @@
 import pytest
-from snowflake.connector.compat import IS_WINDOWS
 
 
 @pytest.mark.integration
-@pytest.mark.skipif(IS_WINDOWS, reason="Permissions issue on Windows")
-def test_list_templates_no_options_success(runner, snapshot):
+def test_list_templates_no_options_success(runner):
     args = ["app", "list-templates"]
-    result = runner.invoke(args)
+    result = runner.invoke_json(args)
 
     assert result.exit_code == 0
-    assert result.output == snapshot
+    templates = result.json
+    assert len(templates) > 0
+
+    # Check that the basic templates are present, but explicitly avoid checking for an
+    # exact list so that adding new templates won't break the tests.
+    all_template_names = [t["template"] for t in templates]
+    assert "basic" in all_template_names
+    assert "streamlit-java" in all_template_names
+    assert "streamlit-python" in all_template_names
