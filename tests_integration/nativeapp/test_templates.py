64648847faa8f42fe5ca596de1ec7fb462bbefac
Bruno Dufour <bruno.dufour@snowflake.com>
2024-05-29 09:29:02 -0400

    Check more templates

--- tests_integration/nativeapp/test_templates.py
+++ tests_integration/nativeapp/test_templates.py
@@ -9,4 +9,10 @@ def test_list_templates_no_options_success(runner):
     assert result.exit_code == 0
     templates = result.json
     assert len(templates) > 0
-    assert "basic" in [t["template"] for t in templates]
+
+    # Check that the basic templates are present, but explicitly avoid checking for an
+    # exact list so that adding new templates won't break the tests.
+    all_template_names = [t["template"] for t in templates]
+    assert "basic" in all_template_names
+    assert "streamlit-java" in all_template_names
+    assert "streamlit-python" in all_template_names
