54d56ac9af69c82d690cad44062750339ce8b98e
Francois Campbell <francois.campbell@snowflake.com>
2024-06-10 11:15:42 -0400

    Add failing integration test, remove explicit deploy step

--- tests_integration/nativeapp/test_validate.py
+++ tests_integration/nativeapp/test_validate.py
@@ -21,12 +21,6 @@ def test_nativeapp_validate(runner, snowflake_session):
     assert result.exit_code == 0
 
     with pushd(Path(os.getcwd(), project_name)):
-        result = runner.invoke_with_connection_json(
-            ["app", "deploy"],
-            env=TEST_ENV,
-        )
-        assert result.exit_code == 0
-
         try:
             # validate the app's setup script
             result = runner.invoke_with_connection_json(
@@ -35,6 +29,43 @@ def test_nativeapp_validate(runner, snowflake_session):
             )
             assert result.exit_code == 0
             assert "Native App validation succeeded." in result.output
+
+        finally:
+            # teardown is idempotent, so we can execute it again with no ill effects
+            result = runner.invoke_with_connection_json(
+                ["app", "teardown", "--force"],
+                env=TEST_ENV,
+            )
+            assert result.exit_code == 0
+
+
+@pytest.mark.integration
+def test_nativeapp_validate_failing(runner, snowflake_session):
+    project_name = "myapp"
+    result = runner.invoke_json(
+        ["app", "init", project_name],
+        env=TEST_ENV,
+    )
+    assert result.exit_code == 0
+
+    with pushd(Path(os.getcwd(), project_name)):
+        # Create invalid SQL file
+        Path("app/empty.sql").touch()
+        Path("app/setup_script.sql").write_text(
+            "EXECUTE IMMEDIATE FROM './empty.sql';\n"
+        )
+
+        try:
+            # validate the app's setup script
+            result = runner.invoke_with_connection_json(
+                ["app", "validate"],
+                env=TEST_ENV,
+            )
+            assert result.exit_code == 1
+            assert (
+                "Snowflake Native App setup script failed validation." in result.output
+            )
+            assert "Empty SQL statement" in result.output
 
         finally:
             # teardown is idempotent, so we can execute it again with no ill effects
