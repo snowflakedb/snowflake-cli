40cf0de802be4b3bede3f276c77ec57f5ecc71e6
Bhumika Goel <bhumika.goel@snowflake.com>
2024-06-12 16:14:46 -0500

    [NADE] Bundle Change: Allow symlinking files, copying parent directories to deploy root (#1162)

--- src/snowflake/cli/plugins/nativeapp/artifacts.py
+++ src/snowflake/cli/plugins/nativeapp/artifacts.py
@@ -508,33 +508,47 @@ def delete(path: Path) -> None:
         spath.rmdir(recursive=True)  # remove dir and all contains
 
 
-def symlink_or_copy(
-    src: Path, dst: Path, deploy_root: Path, makedirs=True, overwrite=True
-) -> None:
+def symlink_or_copy(src: Path, dst: Path, deploy_root: Path) -> None:
     """
-    Tries to create a symlink to src at dst; failing that (i.e. in Windows
-    without Administrator / Developer Mode) copies the file from src to dst instead.
-    If makedirs is True, the directory hierarchy above dst is created if any
-    of those directories do not exist.
+    Symlinks files from src to dst. If the src contains parent directories, then copies the empty directory shell to the deploy root.
+    The directory hierarchy above dst is created if any of those directories do not exist.
     """
     ssrc = SecurePath(src)
     sdst = SecurePath(dst)
-    if makedirs:
-        sdst.parent.mkdir(parents=True, exist_ok=True)
+    sdst.parent.mkdir(parents=True, exist_ok=True)
 
     # Verify that the mapping isn't accidentally trying to create a file in the project source through symlinks.
     # We need to ensure we're resolving symlinks for this check to be effective.
+    # We are unlikely to hit this if calling the function through bundle map, keeping it here for other future use cases outside bundle.
     resolved_dst = dst.resolve()
     resolved_deploy_root = deploy_root.resolve()
-    if resolved_deploy_root not in resolved_dst.parents:
+    dst_is_deploy_root = resolved_deploy_root == resolved_dst
+    if (not dst_is_deploy_root) and (resolved_deploy_root not in resolved_dst.parents):
         raise NotInDeployRootError(dest_path=dst, deploy_root=deploy_root, src_path=src)
 
-    if overwrite:
+    absolute_src = resolve_without_follow(src)
+    if absolute_src.is_file():
         delete(dst)
-    try:
-        os.symlink(src, dst)
-    except OSError:
-        ssrc.copy(dst)
+        try:
+            os.symlink(absolute_src, dst)
+        except OSError:
+            ssrc.copy(dst)
+    else:
+        # 1. Create a new directory in the deploy root
+        dst.mkdir(exist_ok=True)
+        # 2. For all children of src, create their counterparts in dst now that it exists
+        for root, _, files in sorted(os.walk(absolute_src, followlinks=True)):
+            relative_root = Path(root).relative_to(absolute_src)
+            absolute_root_in_deploy = Path(dst, relative_root)
+            absolute_root_in_deploy.mkdir(parents=True, exist_ok=True)
+            for file in files:
+                absolute_file_in_project = Path(absolute_src, relative_root, file)
+                absolute_file_in_deploy = Path(absolute_root_in_deploy, file)
+                symlink_or_copy(
+                    src=absolute_file_in_project,
+                    dst=absolute_file_in_deploy,
+                    deploy_root=deploy_root,
+                )
 
 
 def resolve_without_follow(path: Path) -> Path:
