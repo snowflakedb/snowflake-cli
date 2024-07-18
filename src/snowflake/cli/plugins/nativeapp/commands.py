474469601f37c7e8a5c181aa6131aa35540dbca2
Guy Bloom <guy.bloom@snowflake.com>
2024-07-18 10:30:44 -0400

    Support project definition v2 in "snow app teardown" (#1326)
    * add v2 decorator to "app teardown" command
    
    * util to enable v2 feature flag
    
    * v2 coverage for teardown cascade integration test

--- src/snowflake/cli/plugins/nativeapp/commands.py
+++ src/snowflake/cli/plugins/nativeapp/commands.py
@@ -257,6 +257,7 @@ def app_open(
 
 @app.command("teardown", requires_connection=True)
 @with_project_definition()
+@nativeapp_definition_v2_to_v1
 def app_teardown(
     force: Optional[bool] = ForceOption,
     cascade: Optional[bool] = typer.Option(
---
+++ tests_integration/test_data/projects/napp_create_db_v2/app/README.md
@@ -0,0 +1,3 @@
+# README
+
+This is the v2 version of the napp_create_db_v1 project
