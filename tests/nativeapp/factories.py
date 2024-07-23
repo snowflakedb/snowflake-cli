4157ed09cc910e66e63e0cc065380249d08f88fd
Francois Campbell <francois.campbell@snowflake.com>
2024-07-23 13:42:51 -0400

    wip

--- tests/nativeapp/factories.py
+++ tests/nativeapp/factories.py
@@ -16,7 +16,6 @@ from __future__ import annotations
 from typing import cast
 
 import factory
-import yaml
 from snowflake.cli.api.project.schemas.entities.entities import Entity
 from snowflake.cli.api.project.schemas.native_app.application import Application
 from snowflake.cli.api.project.schemas.native_app.native_app import NativeApp
@@ -31,63 +30,47 @@ from snowflake.cli.api.project.schemas.project_definition import (
 from snowflake.cli.api.utils.types import Context
 
 
-class WithDumpYamlMixin:
-    dump_filename: str | None = None
+class FileFactory(factory.Factory):
+    filename = factory.Faker("file_name")
+    contents = factory.Faker("text")
 
     @classmethod
-    def _build(cls, model_class, *args, **kwargs):
-        kwargs.pop("dump_filename", None)
-        return super()._build(model_class, *args, **kwargs)
+    def _build(cls, model_class, *args, **kwargs: str) -> str:
+        return kwargs["filename"]
 
     @classmethod
-    def _create(cls, model_class, *args, **kwargs):
-        if cls.dump_filename is None:
-            raise ValueError(f"dump_filename must be set on {cls.__name__}")
-        filename = kwargs.pop("dump_filename")
-        obj = super()._create(model_class, *args, **kwargs)
-        _dump_yaml(obj, filename)
-        return obj
+    def _create(cls, model_class, *args, **kwargs: str):
+        filename = cls._build(model_class, *args, **kwargs)
+        with open(filename, "w") as file:
+            file.write(kwargs["contents"])
+        return filename
 
 
-class _NativeAppManifestVersionFactory(factory.DictFactory):
+class NativeAppManifestVersionFactory(factory.DictFactory):
     name = factory.Faker("word")
     label = factory.Faker("word")
     comment = factory.Faker("sentence")
 
 
-class _NativeAppManifestArtifactsFactory(factory.DictFactory):
-    setup_script_contents = "select 1;"
-    readme_contents = factory.Faker("sentence")
-
-    setup_script = "setup.sql"
-    readme = "README.md"
-
-    @classmethod
-    def _build(cls, model_class, *args, **kwargs: str):
-        kwargs.pop("setup_script_contents", None)
-        kwargs.pop("readme_contents", None)
-        return super()._build(model_class, *args, **kwargs)
-
-    @classmethod
-    def _create(cls, model_class, *args, **kwargs: str):
-        obj = super()._create(model_class, *args, **kwargs)
-        _dump_str(kwargs.pop("setup_script_contents"), kwargs["setup_script"])
-        _dump_str(kwargs.pop("readme_contents"), kwargs["readme"])
-        return obj
+class NativeAppManifestArtifactsFactory(factory.DictFactory):
+    setup_script = factory.SubFactory(
+        FileFactory, filename="setup.sql", contents="select 1;"
+    )
+    readme = factory.SubFactory(FileFactory, filename="README.md")
 
 
-class _NativeAppManifestConfigurationFactory(factory.DictFactory):
+class NativeAppManifestConfigurationFactory(factory.DictFactory):
     log_level = "INFO"
     trace_level = "ALWAYS"
 
 
-class _NativeAppManifestFactory(WithDumpYamlMixin, factory.DictFactory):
+class NativeAppManifestFactory(factory.DictFactory):
     dump_filename = "manifest.yml"  # Where to save the manifest
 
     manifest_version = "1"
-    version = factory.SubFactory(_NativeAppManifestVersionFactory)
-    artifacts = factory.SubFactory(_NativeAppManifestArtifactsFactory)
-    configuration = factory.SubFactory(_NativeAppManifestConfigurationFactory)
+    version = factory.SubFactory(NativeAppManifestVersionFactory)
+    artifacts = factory.SubFactory(NativeAppManifestArtifactsFactory)
+    configuration = factory.SubFactory(NativeAppManifestConfigurationFactory)
 
 
 class PackageFactory(factory.Factory):
@@ -105,7 +88,7 @@ class NativeAppFactory(factory.Factory):
         model = NativeApp
 
     class Params:
-        manifest = factory.SubFactory(_NativeAppManifestFactory)
+        manifest = factory.SubFactory(NativeAppManifestFactory)
 
     name = factory.Faker("word")
     artifacts: list = []
@@ -113,11 +96,10 @@ class NativeAppFactory(factory.Factory):
     application = factory.SubFactory(ApplicationFactory)
 
 
-class ProjectDefinitionBaseFactory(WithDumpYamlMixin, factory.Factory):
+class ProjectDefinitionBaseFactory(factory.Factory):
     class Meta:
         model = _ProjectDefinitionBase
 
-    dump_filename = "snowflake.yml"  # Where to save the project definition file
     definition_version = "0"  # This will fail validation, so it needs to be overridden
 
 
@@ -145,7 +127,7 @@ class DefinitionV20Factory(DefinitionV10Factory):
     entities: dict[str, Entity] = {}
 
 
-class MultiVersionDefinitionFactory(factory.declarations.BaseDeclaration):
+class ProjectDefinition(factory.declarations.BaseDeclaration):
     SUB_FACTORIES = {
         cast(ProjectDefinitionBaseFactory, cls).definition_version: factory.SubFactory(
             cls
@@ -170,15 +152,5 @@ class ProjectPropertiesFactory(factory.Factory):
     class Params:
         definition_version = "1.1"  # PDF v1.1 is the latest public version
 
-    project_definition = MultiVersionDefinitionFactory()
+    project_definition = ProjectDefinition()
     project_context: Context = {}
-
-
-def _dump_str(s: str, filename: str):
-    with open(filename, "w") as f:
-        f.write(s)
-
-
-def _dump_yaml(data: dict, filename: str):
-    with open(filename, "w") as f:
-        yaml.dump(data, stream=f)
