6c86d31fd8be663404673bab3b0cdcaea4d261b8
Cam Gorrie <cam.gorrie@snowflake.com>
2024-07-26 14:58:42 -0400

    large upload test

---
+++ tests_integration/nativeapp/test_large_upload.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2024 Snowflake Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import uuid
+from typing import Optional
+import os
+
+from snowflake.cli.api.project.util import generate_user_env
+from snowflake.connector.connection import SnowflakeConnection
+from snowflake.connector.constants import S3_CHUNK_SIZE
+from snowflake.connector.errors import ProgrammingError
+from snowflake.cli.api.secure_path import SecurePath
+from snowflake.cli.api.project.definition_manager import DefinitionManager
+from snowflake.cli.plugins.nativeapp.manager import NativeAppManager
+from snowflake.cli.plugins.nativeapp.v2_conversions.v2_to_v1_decorator import (
+    _pdf_v2_to_v1,
+)
+
+from tests.project.fixtures import *
+from tests_integration.test_utils import pushd, enable_definition_v2_feature_flag
+
+USER_NAME = f"user_{uuid.uuid4().hex}"
+TEST_ENV = generate_user_env(USER_NAME)
+
+THRESHOLD_BYTES = 100  # arbitrarily small to force chunking
+TEMP_FILE_SIZE_BYTES = int(S3_CHUNK_SIZE * 1.6)  # ensure our file is multi-part
+
+
+@pytest.mark.integration
+@enable_definition_v2_feature_flag
+@pytest.mark.parametrize(
+    "project_definition_files", ["integration", "integration_v2"], indirect=True
+)
+def test_large_upload_skips_reupload(
+    runner,
+    snowflake_session,
+    project_definition_files: List[Path],
+):
+    """
+    Ensure that files uploaded using a multi-part.
+    FIXME: this test will FAIL on Azure, currently.
+    """
+    project_dir = project_definition_files[0].parent
+    with pushd(project_dir):
+
+        # figure out what the source stage is resolved to
+        dm = DefinitionManager(project_dir)
+        is_v1 = hasattr(dm.project_definition, "native_app")
+        native_app = (
+            dm.project_definition.native_app
+            if is_v1
+            else _pdf_v2_to_v1(dm.project_definition)
+        )
+        manager = NativeAppManager(native_app, project_dir)
+
+        # deploy the application package
+        result = runner.invoke_with_connection_json(
+            ["app", "deploy"],
+            env=TEST_ENV,
+        )
+        assert result.exit_code == 0
+
+        temp_file = project_dir / "app" / "big.file"
+        try:
+            # generate a binary file w/random bytes and upload it in multi-part
+            with SecurePath(temp_file).open("wb") as f:
+                f.write(os.urandom(TEMP_FILE_SIZE_BYTES))
+            snowflake_session.execute_string(
+                f"put file://{temp_file} @{manager.stage_fqn}/app/ threshold={THRESHOLD_BYTES}"
+            )
+
+            # ensure that diff shows there is nothing to re-upload
+            result = runner.invoke_with_connection_json(
+                ["app deploy"],
+                env=TEST_ENV,
+            )
+            assert result.exit_code == 0
+            assert result.json == {
+                "modified": [],
+                "added": [],
+                "deleted": [],
+            }
+
+        finally:
+            # make sure our file has been deleted
+            temp_file.unlink(missing_ok=True)
+
+            # teardown is idempotent, so we can execute it again with no ill effects
+            result = runner.invoke_with_connection_json(
+                ["app", "teardown", "--force"],
+                env=TEST_ENV,
+            )
+            assert result.exit_code == 0
