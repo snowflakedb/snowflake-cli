d1b490ed9e646957d3cb5919c01eba6de1a2cae9
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2024-07-26 13:26:25 +0200

    fixup! fixup! fixup! fixup! Fix tests

--- src/snowflake/cli/api/project/schemas/entities/streamlit_entity.py
+++ src/snowflake/cli/api/project/schemas/entities/streamlit_entity.py
@@ -38,7 +38,8 @@ class StreamlitEntity(EntityBase, ObjectIdentifierModel(object_name="Streamlit")
     )
     # Possibly can be PathMapping
     artifacts: Optional[List[Path]] = Field(
-        title="List of additional files which should be included into deployment artifacts",
+        title="List of files which should be deployed. Each file needs exist locally. "
+        "Main file needs be included in artifacts.",
         default=None,
     )
 
