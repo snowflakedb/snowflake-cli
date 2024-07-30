e2f07814c04f8a5bbb2514917200bd8eac8c5c9a
Bruno Dufour <bruno.dufour@snowflake.com>
2024-07-29 20:17:21 -0400

    Updated snowflake-app-python interactions

--- src/snowflake/cli/_plugins/nativeapp/codegen/setup/native_app_setup_processor.py
+++ src/snowflake/cli/_plugins/nativeapp/codegen/setup/native_app_setup_processor.py
@@ -14,13 +14,20 @@
 
 from __future__ import annotations
 
+import filecmp
 import json
 import os.path
+import shutil
 from pathlib import Path
 from typing import List, Optional
 
+import yaml
 from click import ClickException
-from snowflake.cli._plugins.nativeapp.artifacts import BundleMap, find_setup_script_file
+from snowflake.cli._plugins.nativeapp.artifacts import (
+    BundleMap,
+    find_manifest_file,
+    find_setup_script_file,
+)
 from snowflake.cli._plugins.nativeapp.codegen.artifact_processor import (
     ArtifactProcessor,
     is_python_file_artifact,
@@ -39,6 +46,14 @@ from snowflake.cli.api.project.schemas.native_app.path_mapping import (
 
 DEFAULT_TIMEOUT = 30
 DRIVER_PATH = Path(__file__).parent / "setup_driver.py.source"
+
+
+def safe_set(d: dict, *keys: str, **kwargs) -> None:
+    curr = d
+    for k in keys[:-1]:
+        curr = curr.setdefault(k, {})
+
+    curr[keys[-1]] = kwargs.get("value")
 
 
 class NativeAppSetupProcessor(ArtifactProcessor):
@@ -73,25 +88,56 @@ class NativeAppSetupProcessor(ArtifactProcessor):
             )
             files_to_process.append(src_file)
 
-        sql_files_mapping = self._execute_in_sandbox(files_to_process)
-        self._generate_setup_sql(sql_files_mapping)
+        result = self._execute_in_sandbox(files_to_process)
+        if not result:
+            return  # nothing to do
+
+        logs = result.get("logs", [])
+        for msg in logs:
+            cc.message(f"LOG: {msg}")
+
+        warnings = result.get("warnings", [])
+        for msg in warnings:
+            cc.warning(msg)
+
+        if result.get("schema_version") == "1":
+            setup_script_mods = [
+                mod
+                for mod in result.get("modifications", [])
+                if mod.get("target") == "native_app:setup_script"
+            ]
+            self._edit_setup_sql(setup_script_mods)
+
+            manifest_mods = [
+                mod
+                for mod in result.get("modifications", [])
+                if mod.get("target") == "native_app:manifest"
+            ]
+            self._edit_manifest(manifest_mods)
+        else:
+            self._generate_setup_sql_legacy(result)
 
     def _execute_in_sandbox(self, py_files: List[Path]) -> dict:
         file_count = len(py_files)
         cc.step(f"Processing {file_count} setup file{'s' if file_count > 1 else ''}")
+
+        manifest_path = find_manifest_file(deploy_root=self._bundle_ctx.deploy_root)
+        temp_manifest_path = self.bundle_root / manifest_path.name
+        shutil.copyfile(manifest_path, temp_manifest_path)
 
         env_vars = {
             "_SNOWFLAKE_CLI_PROJECT_PATH": str(self._bundle_ctx.project_root),
             "_SNOWFLAKE_CLI_SETUP_FILES": os.pathsep.join(map(str, py_files)),
             "_SNOWFLAKE_CLI_APP_NAME": str(self._bundle_ctx.package_name),
             "_SNOWFLAKE_CLI_SQL_DEST_DIR": str(self.generated_root),
+            "_SNOWFLAKE_CLI_MANIFEST_PATH": str(temp_manifest_path),
         }
 
         try:
             result = execute_script_in_sandbox(
                 script_source=DRIVER_PATH.read_text(),
                 env_type=ExecutionEnvironmentType.VENV,
-                cwd=self._bundle_ctx.bundle_root,
+                cwd=self.bundle_root,
                 timeout=DEFAULT_TIMEOUT,
                 path=self.sandbox_root,
                 env_vars=env_vars,
@@ -102,18 +148,78 @@ class NativeAppSetupProcessor(ArtifactProcessor):
             )
 
         if result.returncode == 0:
-            sql_file_mappings = json.loads(result.stdout)
-            return sql_file_mappings
+            processor_result = json.loads(result.stdout)
+
+            if not filecmp.cmp(manifest_path, temp_manifest_path):
+                # manifest was edited, update the original in the deploy root
+                with self.edit_file(manifest_path) as f:
+                    f.edited_contents = temp_manifest_path.read_text()
+
+            return processor_result
         else:
             raise ClickException(
                 f"Failed to execute python setup script logic: {result.stderr}"
             )
 
-    def _generate_setup_sql(self, sql_file_mappings: dict) -> None:
-        if not sql_file_mappings:
-            # Nothing to generate
+    def _edit_setup_sql(self, modifications: List[dict]) -> None:
+        generated_root = self.generated_root
+        generated_root.mkdir(exist_ok=True, parents=True)
+
+        if not modifications:
             return
 
+        cc.step("Patching setup script")
+        setup_file_path = find_setup_script_file(
+            deploy_root=self._bundle_ctx.deploy_root
+        )
+
+        with self.edit_file(setup_file_path) as f:
+            prepended = []
+            appended = []
+
+            for mod in modifications:
+                for inst in mod.get("instructions", []):
+                    if inst.get("type") == "insert":
+                        default_loc = inst.get("default_location")
+                        if default_loc == "end":
+                            appended.append(self._setup_mod_inst_to_sql(inst))
+                        elif default_loc == "start":
+                            prepended.append(self._setup_mod_inst_to_sql(inst))
+
+            if prepended or appended:
+                f.edited_contents = "\n".join(prepended + [f.contents] + appended)
+
+    def _edit_manifest(self, modifications: List[dict]) -> None:
+        if not modifications:
+            return
+
+        cc.step("Patching manifest")
+        manifest_path = find_manifest_file(deploy_root=self._bundle_ctx.deploy_root)
+
+        with self.edit_file(manifest_path) as f:
+            manifest = yaml.safe_load(f.contents)
+
+            for mod in modifications:
+                for inst in mod.get("instructions", []):
+                    if inst.get("type") == "set":
+                        payload = inst.get("payload")
+                        if payload:
+                            key = payload.get("key")
+                            value = payload.get("value")
+                            safe_set(manifest, *key.split("."), value=value)
+            f.edited_contents = yaml.safe_dump(manifest, sort_keys=False)
+
+    def _setup_mod_inst_to_sql(self, mod_inst: dict) -> str:
+        payload = mod_inst["payload"]
+        if payload["type"] == "execute immediate":
+            file_path = payload.get("file_path")
+            if file_path:
+                sql_file_path = self.generated_root / file_path
+                return f"EXECUTE IMMEDIATE FROM '/{to_stage_path(sql_file_path.relative_to(self._bundle_ctx.deploy_root))}';"
+
+        raise ClickException("Invalid instructions received")
+
+    def _generate_setup_sql_legacy(self, result: dict):
         generated_root = self.generated_root
         generated_root.mkdir(exist_ok=True, parents=True)
 
@@ -121,37 +227,39 @@ class NativeAppSetupProcessor(ArtifactProcessor):
         setup_file_path = find_setup_script_file(
             deploy_root=self._bundle_ctx.deploy_root
         )
+
         with self.edit_file(setup_file_path) as f:
             new_contents = [f.contents]
 
-            if sql_file_mappings["schemas"]:
-                schemas_file = generated_root / sql_file_mappings["schemas"]
-                new_contents.insert(
-                    0,
-                    f"EXECUTE IMMEDIATE FROM '/{to_stage_path(schemas_file.relative_to(self._bundle_ctx.deploy_root))}';",
-                )
+            if result["prepend"]:
+                for sql_file in result["prepend"]:
+                    sql_file_path = generated_root / sql_file
+                    new_contents.insert(
+                        0,
+                        f"EXECUTE IMMEDIATE FROM '/{to_stage_path(sql_file_path.relative_to(self._bundle_ctx.deploy_root))}';",
+                    )
 
-            if sql_file_mappings["compute_pools"]:
-                compute_pools_file = generated_root / sql_file_mappings["compute_pools"]
-                new_contents.append(
-                    f"EXECUTE IMMEDIATE FROM '/{to_stage_path(compute_pools_file.relative_to(self._bundle_ctx.deploy_root))}';"
-                )
+            if result["append"]:
+                for sql_file in result["append"]:
+                    sql_file_path = generated_root / sql_file
+                    new_contents.append(
+                        f"EXECUTE IMMEDIATE FROM '/{to_stage_path(sql_file_path.relative_to(self._bundle_ctx.deploy_root))}';",
+                    )
 
-            if sql_file_mappings["services"]:
-                services_file = generated_root / sql_file_mappings["services"]
-                new_contents.append(
-                    f"EXECUTE IMMEDIATE FROM '/{to_stage_path(services_file.relative_to(self._bundle_ctx.deploy_root))}';"
-                )
-
-            f.edited_contents = "\n".join(new_contents)
+            if len(new_contents) > 1:
+                f.edited_contents = "\n".join(new_contents)
 
     @property
     def sandbox_root(self):
-        return self._bundle_ctx.bundle_root / "setup_py_venv"
+        return self.bundle_root / "venv"
 
     @property
     def generated_root(self):
         return self._bundle_ctx.generated_root / "setup_py"
+
+    @property
+    def bundle_root(self):
+        return self._bundle_ctx.bundle_root / "setup_py"
 
     def _create_or_update_sandbox(self):
         sandbox_root = self.sandbox_root
