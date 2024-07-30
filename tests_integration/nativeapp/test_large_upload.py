36b713402de91e369b28fed1cf1046a4eb63cfc3
Cam Gorrie <cam.gorrie@snowflake.com>
2024-07-30 13:46:44 -0400

    Mock env throughout the test

--- tests_integration/nativeapp/test_large_upload.py
+++ tests_integration/nativeapp/test_large_upload.py
@@ -40,6 +40,25 @@ TEST_ENV = generate_user_env(USER_NAME)
 THRESHOLD_BYTES = None
 TEMP_FILE_SIZE_BYTES = 200 * 1024 * 1024
 
+from contextlib import contextmanager
+
+
+@contextmanager
+def mocked_testenv():
+    """
+    Mocks all ways we can extract env vars to ensure our ctx.env is correct.
+    """
+    from os import getenv as original_getenv
+
+    def mock_getenv(key: str, default: str | None = None) -> str | None:
+        if key.lower() == "user":
+            return USER_NAME
+        return original_getenv(key, default)
+
+    with mock.patch.dict(os.environ, {"USER": USER_NAME}):  # for DefinitionManager
+        with mock.patch("os.getenv", side_effect=mock_getenv):  # for ctx.env
+            yield
+
 
 @pytest.mark.integration
 @enable_definition_v2_feature_flag
@@ -57,74 +76,68 @@ def test_large_upload_skips_reupload(
     """
     project_dir = project_definition_files[0].parent
     with pushd(project_dir):
+        with mocked_testenv():
+            # figure out what the source stage is resolved to
+            dm = DefinitionManager(project_dir)
+            native_app = (
+                dm.project_definition.native_app
+                if hasattr(dm.project_definition, "native_app")
+                else _pdf_v2_to_v1(dm.project_definition).native_app
+            )
+            stage_fqn = NativeAppManager(native_app, project_dir).stage_fqn
 
-        from os import getenv as original_getenv
+            # deploy the application package
+            result = runner.invoke_with_connection_json(
+                ["app", "deploy"],
+                env=TEST_ENV,
+            )
+            assert result.exit_code == 0
 
-        def mock_getenv(key: str, default: str | None = None) -> str | None:
-            if key.lower() == "user":
-                return USER_NAME
-            return original_getenv(key, default)
+            temp_file = project_dir / "app" / "big.file"
+            try:
+                # generate a binary file w/random bytes and upload it in multi-part
+                with SecurePath(temp_file).open("wb") as f:
+                    f.write(os.urandom(TEMP_FILE_SIZE_BYTES))
 
-        # figure out what the source stage is resolved to
-        with mock.patch.dict(os.environ, {"USER": USER_NAME}):  # for DefinitionManager
-            with mock.patch("os.getenv", side_effect=mock_getenv):  # for ctx.env
-                dm = DefinitionManager(project_dir)
-                native_app = (
-                    dm.project_definition.native_app
-                    if hasattr(dm.project_definition, "native_app")
-                    else _pdf_v2_to_v1(dm.project_definition).native_app
+                put_command = f"put file://{temp_file.resolve()} @{stage_fqn}/ auto_compress=false parallel=4 overwrite=True"
+                if THRESHOLD_BYTES is not None:
+                    put_command += f" threshold={THRESHOLD_BYTES}"
+
+                snowflake_session.execute_string(put_command)
+
+                # ensure that there is, in fact, a file with a multi-part md5sum
+                # FIXME: this will FAIL on both Azure and GCP; find a way to only test this on AWS?
+                result = runner.invoke_with_connection_json(
+                    ["stage", "list-files", stage_fqn]
                 )
-                stage_fqn = NativeAppManager(native_app, project_dir).stage_fqn
+                assert result.exit_code == 0
+                assert isinstance(result.json, list)
+                assert any(
+                    [
+                        parse_multipart_md5sum(row["md5"]) is not None
+                        for row in result.json
+                    ]
+                )
 
-        # deploy the application package
-        result = runner.invoke_with_connection_json(
-            ["app", "deploy"],
-            env=TEST_ENV,
-        )
-        assert result.exit_code == 0
+                # ensure that diff shows there is nothing to re-upload
+                result = runner.invoke_with_connection_json(
+                    ["app", "diff"],
+                    env=TEST_ENV,
+                )
+                assert result.exit_code == 0
+                assert result.json == {
+                    "modified": [],
+                    "added": [],
+                    "deleted": [],
+                }
 
-        temp_file = project_dir / "app" / "big.file"
-        try:
-            # generate a binary file w/random bytes and upload it in multi-part
-            with SecurePath(temp_file).open("wb") as f:
-                f.write(os.urandom(TEMP_FILE_SIZE_BYTES))
+            finally:
+                # make sure our file has been deleted
+                temp_file.unlink(missing_ok=True)
 
-            put_command = f"put file://{temp_file.resolve()} @{stage_fqn}/ auto_compress=false parallel=4 overwrite=True"
-            if THRESHOLD_BYTES is not None:
-                put_command += f" threshold={THRESHOLD_BYTES}"
-
-            snowflake_session.execute_string(put_command)
-
-            # ensure that there is, in fact, a file with a multi-part md5sum
-            # FIXME: this will FAIL on both Azure and GCP; find a way to only test this on AWS?
-            result = runner.invoke_with_connection_json(
-                ["stage", "list-files", stage_fqn]
-            )
-            assert result.exit_code == 0
-            assert isinstance(result.json, list)
-            assert any(
-                [parse_multipart_md5sum(row["md5"]) is not None for row in result.json]
-            )
-
-            # ensure that diff shows there is nothing to re-upload
-            result = runner.invoke_with_connection_json(
-                ["app", "diff"],
-                env=TEST_ENV,
-            )
-            assert result.exit_code == 0
-            assert result.json == {
-                "modified": [],
-                "added": [],
-                "deleted": [],
-            }
-
-        finally:
-            # make sure our file has been deleted
-            temp_file.unlink(missing_ok=True)
-
-            # teardown is idempotent, so we can execute it again with no ill effects
-            result = runner.invoke_with_connection_json(
-                ["app", "teardown", "--force"],
-                env=TEST_ENV,
-            )
-            assert result.exit_code == 0
+                # teardown is idempotent, so we can execute it again with no ill effects
+                result = runner.invoke_with_connection_json(
+                    ["app", "teardown", "--force"],
+                    env=TEST_ENV,
+                )
+                assert result.exit_code == 0
