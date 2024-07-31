ce9b982ef0559340352af0cd52d496712e82c389
Marcin Raba <marcin.raba@snowflake.com>
2024-07-31 15:42:58 +0200

    SNOW-1055755: use docker to build linux packages

---
+++ scripts/packaging/build_packages.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+set -o pipefail
+
+VERSION=$(hatch version)
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DEB_PGK="snowflke-cli-${VERSION}.deb"
+RPM_PGK="snowflke-cli-${VERSION}.rpm"
+
+fpm \
+  -s dir \
+  -t deb \
+  --name snow \
+  --version ${VERSION} \
+  -p DEB_PGK \
+  -C ./dist/snow \
+  --prefix /usr/lib/snowflake/snowflake-cli \
+  --after-install $THIS_DIR/ubuntu/after_install.sh \
+  --after-remove $THIS_DIR/ubuntu/after_remove.sh \
+  --force
+
+fpm \
+  -s dir \
+  -t rpm \
+  --name snow \
+  --version ${VERSION} \
+  -p RPM_PGK \
+  -C ./dist/snow \
+  --prefix /usr/lib/snowflake/snowflake-cli \
+  --after-install $THIS_DIR/ubuntu/after_install.sh \
+  --after-remove $THIS_DIR/ubuntu/after_remove.sh \
+  --force
