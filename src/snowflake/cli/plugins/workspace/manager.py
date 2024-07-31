3af9fd284c56ae82ca707eee9c74a65e84436b17
Guy Bloom <guy.bloom@snowflake.com>
2024-07-31 16:02:22 -0400

    cache entities

--- src/snowflake/cli/plugins/workspace/manager.py
+++ src/snowflake/cli/plugins/workspace/manager.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+from typing import Dict
 
 from snowflake.cli.api.exceptions import InvalidProjectDefinitionVersion20Error
 from snowflake.cli.api.project.schemas.entities.entities import (
+    Entity,
     v2_entity_model_to_entity_map,
 )
 from snowflake.cli.api.project.schemas.project_definition import DefinitionV20
@@ -13,12 +15,17 @@ class WorkspaceManager:
             raise InvalidProjectDefinitionVersion20Error(
                 project_definition.definition_version
             )
+        self._entities_cache: Dict[str, Entity] = {}
         self._pdf = project_definition
 
     def get_entity(self, key: str):
-        entity_schema = self._pdf.entities[key]  # TODO Verify
-        entity = v2_entity_model_to_entity_map[entity_schema.__class__]  # TODO Verify
-        return entity
+        if key not in self._entities_cache:
+            if key not in self._pdf.entities:
+                raise ValueError(f"No such entity key: {key}")
+            entity_model_cls = self._pdf.entities[key].__class__
+            entity_cls = v2_entity_model_to_entity_map[entity_model_cls]
+            self._entities_cache[key] = entity_cls()
+        return self._entities_cache[key]
 
     def bundle(self, key: str):
         entity = self.get_entity(key)
