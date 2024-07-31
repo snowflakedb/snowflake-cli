e51f87833fd5d7fb5d2db21b7dd35ee2a4426f86
Marcin Raba <marcin.raba@snowflake.com>
2024-07-31 17:09:39 +0200

    SNOW-1055755: fix rpm build

--- scripts/packaging/build_packages.sh
+++ scripts/packaging/build_packages.sh
@@ -3,29 +3,34 @@ set -o pipefail
 
 VERSION=$(hatch version)
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DEB_PGK="snowflke-cli-${VERSION}.deb"
-RPM_PGK="snowflke-cli-${VERSION}.rpm"
+ARCH="$(uname -m)"
+DEB_PGK="snowflake_cli_${VERSION}.${ARCH}.deb"
+RPM_PGK="snowflake_cli_${VERSION}.${ARCH}.rpm"
 
+echo "Building deb for version ${VERSION} on ${ARCH}..."
 fpm \
   -s dir \
   -t deb \
   --name snow \
   --version ${VERSION} \
-  -p ${DEB_PGK} \
-  -C ./dist/snow \
+  -a native \
   --prefix /usr/lib/snowflake/snowflake-cli \
   --after-install $THIS_DIR/ubuntu/after_install.sh \
   --after-remove $THIS_DIR/ubuntu/after_remove.sh \
-  --force
+  --force \
+  -C ./dist/snow \
+  -p ${DEB_PGK}
 
+echo "Building rpm for version ${VERSION} on ${ARCH}..."
 fpm \
   -s dir \
   -t rpm \
   --name snow \
   --version ${VERSION} \
-  -p ${RPM_PG} \
-  -C ./dist/snow \
+  -a native \
   --prefix /usr/lib/snowflake/snowflake-cli \
-  --after-install $THIS_DIR/ubuntu/after_install.sh \
-  --after-remove $THIS_DIR/ubuntu/after_remove.sh \
-  --force
+  --after-install $THIS_DIR/centos/after_install.sh \
+  --after-remove $THIS_DIR/centos/after_remove.sh \
+  --force \
+  -C ./dist/snow \
+  -p ${RPM_PKG}
