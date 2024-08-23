87dd3e6d5258a8ac0291040fbcff390b8b42eee0
Francois Campbell <francois.campbell@snowflake.com>
2024-08-23 12:41:22 -0400

    Add docstrings

--- tests_integration/nativeapp/conftest.py
+++ tests_integration/nativeapp/conftest.py
@@ -11,6 +11,15 @@ from tests_integration.conftest import SnowCLIRunner
 
 @pytest.fixture
 def nativeapp_project_directory(project_directory, nativeapp_teardown):
+    """Wrapper around the project_directory fixture specific to Native App testing.
+
+    This fixture provides a context manager that does the following:
+    - Automatically calls `snow app teardown` before exiting
+
+    Parameters for the returned context manager:
+    :param name: The name of the directory in tests_integration/test_data/projects to use.
+    """
+
     @contextmanager
     def _nativeapp_project_directory(name):
         with project_directory(name) as d:
@@ -22,6 +31,17 @@ def nativeapp_project_directory(project_directory, nativeapp_teardown):
 
 @pytest.fixture
 def nativeapp_teardown(runner: SnowCLIRunner):
+    """Runs `snow app teardown` before exiting.
+
+    This fixture provides a context manager that runs
+    `snow app teardown --force --cascade` before exiting,
+    regardless of any exceptions raised.
+
+    Parameters for the returned context manager:
+    :param project_dir: Path to the project directory (optional)
+    :param env: Environment variables to replace os.environ (optional)
+    """
+
     @contextmanager
     def _nativeapp_teardown(
         *,
