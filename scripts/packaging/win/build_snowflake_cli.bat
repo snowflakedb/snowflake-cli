0b1326b91d921fca1c6bdf9f426149d237f4e433
Marcin Raba <marcin.raba@snowflake.com>
2024-08-27 17:14:44 +0200

    SNOW-1055755: build_snowflake_cli.bat 3

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -5,9 +5,12 @@ echo %PATH%
 "C:\Program Files\Python310\python.exe" --version
 "C:\Program Files\Python310\python.exe" -m pip install --upgrade pip uv hatch
 
+@echo off
 FOR /F "delims=" %%I IN ('"C:\Program Files\Python310\python.exe" -m hatch version') DO CLI_VERSION=%%I
-
 echo %CLI_VERSION%
+@echo on
 
-
-REM "C:\Program Files\Python310\python.exe" -m hatch -e packaging run pyinstaller --name snow --onedir --clean --noconfirm --noconsole --contents-directory-snowflake-cli
+@echo off
+set CONTENTSDIR="snowflake-cli-%CLI_VERSION%"
+set ENTRYPOINT=src\\snowflake-cli\\_app\\__main__.py
+"C:\Program Files\Python310\python.exe" -m hatch -e packaging run pyinstaller --name snow --onedir --clean --noconfirm --noconsole --contents-directory=%CONTENTSDIR% %ENTRYPOINT%
