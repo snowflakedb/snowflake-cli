f618620f93d160099461c9b10fe543c52330de79
Michel El Nacouzi <michel.elnacouzi@snowflake.com>
2024-08-28 14:57:25 -0400

    Improve error messages on SQL execution

--- tests/nativeapp/test_post_deploy_for_package.py
+++ tests/nativeapp/test_post_deploy_for_package.py
@@ -20,6 +20,7 @@ from unittest import mock
 import pytest
 from snowflake.cli._plugins.nativeapp.exceptions import MissingScriptError
 from snowflake.cli._plugins.nativeapp.manager import NativeAppManager
+from snowflake.cli.api.exceptions import InvalidTemplate
 from snowflake.cli.api.project.definition_manager import DefinitionManager
 from snowflake.cli.api.project.errors import SchemaValidationError
 from snowflake.connector import ProgrammingError
@@ -172,4 +173,105 @@ def test_package_scripts_and_post_deploy_found(
         assert (
             "package.scripts and package.post_deploy fields cannot be used together"
             in err.value.message
+        )
+
+
+@pytest.mark.parametrize(
+    "template_syntax", [("<% ctx.env.test %>"), ("&{ ctx.env.test }")]
+)
+@mock.patch(SQL_EXECUTOR_EXECUTE)
+@mock.patch(SQL_EXECUTOR_EXECUTE_QUERIES)
+@mock.patch(CLI_GLOBAL_TEMPLATE_CONTEXT, new_callable=mock.PropertyMock)
+@mock.patch.dict(os.environ, {"USER": "test_user"})
+@mock_connection()
+def test_package_post_deploy_scripts_with_templates(
+    mock_conn,
+    mock_cli_ctx,
+    mock_execute_queries,
+    mock_execute_query,
+    project_directory,
+    template_syntax,
+):
+    mock_conn.return_value = MockConnectionCtx()
+    with project_directory("napp_post_deploy") as project_dir:
+        # edit scripts/package_post_deploy1.sql to include template variables
+        with open(project_dir / "scripts" / "package_post_deploy1.sql", "w") as f:
+            f.write(
+                dedent(
+                    f"""\
+                    -- package post-deploy script (1/2)
+
+                    select '{template_syntax}';
+                    """
+                )
+            )
+
+        dm = DefinitionManager(project_dir, {"ctx": {"env": {"test": "test_value"}}})
+        manager = NativeAppManager(
+            project_definition=dm.project_definition.native_app,
+            project_root=dm.project_root,
+        )
+        mock_cli_ctx.return_value = dm.template_context
+
+        manager.execute_package_post_deploy_hooks()
+
+        assert mock_execute_query.mock_calls == [
+            mock.call("use database myapp_pkg_test_user"),
+            mock.call("use database myapp_pkg_test_user"),
+        ]
+        assert mock_execute_queries.mock_calls == [
+            # Verify template variables were expanded correctly
+            mock.call(
+                dedent(
+                    """\
+                    -- package post-deploy script (1/2)
+
+                    select 'test_value';
+                    """
+                )
+            ),
+            mock.call("-- package post-deploy script (2/2)\n"),
+        ]
+
+
+@mock.patch(SQL_EXECUTOR_EXECUTE)
+@mock.patch(SQL_EXECUTOR_EXECUTE_QUERIES)
+@mock.patch(CLI_GLOBAL_TEMPLATE_CONTEXT, new_callable=mock.PropertyMock)
+@mock.patch.dict(os.environ, {"USER": "test_user"})
+@mock_connection()
+def test_package_post_deploy_scripts_with_mix_syntax_templates(
+    mock_conn,
+    mock_cli_ctx,
+    mock_execute_queries,
+    mock_execute_query,
+    project_directory,
+):
+    mock_conn.return_value = MockConnectionCtx()
+    with project_directory("napp_post_deploy") as project_dir:
+        # edit scripts/package_post_deploy1.sql to include template variables
+        with open(project_dir / "scripts" / "package_post_deploy1.sql", "w") as f:
+            f.write(
+                dedent(
+                    """\
+                    -- package post-deploy script (1/2)
+
+                    select '<% ctx.env.test %>';
+                    select '&{ ctx.env.test }';
+                    """
+                )
+            )
+
+        dm = DefinitionManager(project_dir, {"ctx": {"env": {"test": "test_value"}}})
+        manager = NativeAppManager(
+            project_definition=dm.project_definition.native_app,
+            project_root=dm.project_root,
+        )
+        mock_cli_ctx.return_value = dm.template_context
+
+        with pytest.raises(InvalidTemplate) as err:
+            manager.execute_package_post_deploy_hooks()
+
+        assert (
+            "The SQL query in scripts/package_post_deploy1.sql mixes &{ ... } syntax and <% ... %> syntax."
+            == str(err.value)
         )
