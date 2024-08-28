2552334dd6f369207df056fd0b25f359fef740c9
Michel El Nacouzi <michel.elnacouzi@snowflake.com>
2024-08-28 13:02:09 -0400

    Add templating processor for native apps (#1503)

---
+++ tests_integration/test_data/projects/napp_templates_processors_v1/app/setup_script.sql
@@ -0,0 +1,10 @@
+-- This is the setup script that runs while installing a Snowflake Native App in a consumer account.
+-- To write this script, you can familiarize yourself with some of the following concepts:
+-- Application Roles
+-- Versioned Schemas
+-- UDFs/Procs
+-- Extension Code
+-- Refer to https://docs.snowflake.com/en/developer-guide/native-apps/creating-setup-script for a detailed understanding of this file. 
+
+CREATE OR ALTER VERSIONED SCHEMA <% ctx.env.schema_name %>;
+EXECUTE IMMEDIATE from '/another_script.sql';
