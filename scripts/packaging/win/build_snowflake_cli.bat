379292dc05bc42000547b7998ccc92db3b613d88
Marcin Raba <marcin.raba@snowflake.com>
2024-09-02 17:07:03 +0200

    SNOW-1055755: windows build debug 3

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -8,17 +8,14 @@ echo %PATH%
 python.exe --version
 python.exe -m pip install --upgrade pip uv hatch
 
-REM @echo off
 FOR /F "delims=" %%I IN ('python.exe -m hatch version') DO CLI_VERSION=%%I
 echo %CLI_VERSION%
-REM @echo on
 
-REM @echo off
 set CONTENTSDIR="snowflake-cli-%CLI_VERSION%"
 set ENTRYPOINT=src\\snowflake\\cli\\_app\\__main__.py
 python.exe -m hatch -e packaging run pyinstaller --name snow --onedir --clean --noconfirm --noconsole --contents-directory=%CONTENTSDIR% %ENTRYPOINT%
 
-cd dist
+cd dist\snow
 dir .
 REM signtool sign /debug /sm /t http://timestamp.digicert.com /a snow.exe
 wmic product get name, version
