525dfe34a63f9183774a1de628d7502741e3aa1b
Marcin Raba <marcin.raba@snowflake.com>
2024-09-03 09:58:32 +0200

    SNOW-1055755: windows build debug - signtool

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -1,6 +1,6 @@
 @echo off
 echo %PATH%
-set PATH = "C:\Program Files\Python310\;c:\Program Files (x86)\Windows Kits\8.1\bin\x86\;%PATH%"
+set PATH=C:\Program Files\Python310\;c:\Program Files (x86)\Windows Kits\8.1\bin\x86\;%PATH%
 echo %PATH%
 
 @echo on
