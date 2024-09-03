cb1a7164be2a63aa5a75c6f6c1aa35718dcdc4c2
Marcin Raba <marcin.raba@snowflake.com>
2024-09-03 08:12:52 +0200

    SNOW-1055755: windows build debug - signtool

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -3,7 +3,7 @@ echo %PATH%
 "C:\Program Files\Python310\python.exe" --version
 "C:\Program Files\Python310\python.exe" -m pip install --upgrade pip uv hatch
 
-set PATH = C:\Program Files\Python310\;%PATH%
+set PATH = C:\Program Files\Python310\;c:\Program Files (x86)\Windows Kits\8.1\bin\x86\;%PATH%
 echo %PATH%
 python.exe --version
 python.exe -m pip install --upgrade pip uv hatch
@@ -12,15 +12,13 @@ FOR /F "delims=" %%I IN ('python.exe -m hatch version') DO CLI_VERSION=%%I
 echo %CLI_VERSION%
 
 set CONTENTSDIR="snowflake-cli-%CLI_VERSION%"
+echo %CONTENTSDIR%
 set ENTRYPOINT=src\\snowflake\\cli\\_app\\__main__.py
 python.exe -m hatch -e packaging run pyinstaller --name snow --onedir --clean --noconfirm --noconsole --contents-directory=%CONTENTSDIR% %ENTRYPOINT%
 
 cd dist\snow
 dir .
-REM signtool sign /debug /sm /t http://timestamp.digicert.com /a snow.exe
-wmic product get name, version
-where /r c:\ "signtool*"
-DIR "C:\Program Files (x86)\Windows Kits\10\bin\"
+signtool sign /debug /sm /t http://timestamp.digicert.com /a snow.exe
 
 
 REM candle /?
