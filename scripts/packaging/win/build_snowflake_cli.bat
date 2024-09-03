c4f783643ee3b62319092f75c979e07961cbac4b
Marcin Raba <marcin.raba@snowflake.com>
2024-09-03 09:49:07 +0200

    SNOW-1055755: windows build debug - signtool

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -1,19 +1,20 @@
-@echo on
+@echo off
 echo %PATH%
-"C:\Program Files\Python310\python.exe" --version
-"C:\Program Files\Python310\python.exe" -m pip install --upgrade pip uv hatch
+set PATH = "C:\Program Files\Python310\;c:\Program Files (x86)\Windows Kits\8.1\bin\x86\;%PATH%"
+echo %PATH%
 
-set PATH = C:\Program Files\Python310\;c:\Program Files (x86)\Windows Kits\8.1\bin\x86\;%PATH%
-echo %PATH%
+@echo on
 python.exe --version
 python.exe -m pip install --upgrade pip uv hatch
 
+@echo off
 FOR /F "delims=" %%I IN ('python.exe -m hatch version') DO CLI_VERSION=%%I
 echo %CLI_VERSION%
 
 set CONTENTSDIR="snowflake-cli-%CLI_VERSION%"
 echo %CONTENTSDIR%
 set ENTRYPOINT=src\\snowflake\\cli\\_app\\__main__.py
+@echo on
 python.exe -m hatch -e packaging run pyinstaller --name snow --onedir --clean --noconfirm --noconsole --contents-directory=%CONTENTSDIR% %ENTRYPOINT%
 
 cd dist\snow
