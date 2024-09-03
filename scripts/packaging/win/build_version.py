9897a256fe63e1818ef7b07c45449f61a91ba0d2
Marcin Raba <marcin.raba@snowflake.com>
2024-09-03 15:00:06 +0200

    SNOW-1055755: windows build debug - wix installer bundle - fix cli_version

---
+++ scripts/packaging/win/build_version.py
@@ -0,0 +1,11 @@
+from time import time
+
+from snowflake.cli.__about__ import VERSION
+
+if len(version := VERSION.split(".")) > 3:
+    version[-1] = str(int(time()))
+else:
+    version.append("0")
+
+
+print(f"{'.'.join(version)}")
