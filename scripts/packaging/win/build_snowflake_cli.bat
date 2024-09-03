9c226707d1527bd0cf7d7064eef1f65c0d533173
Marcin Raba <marcin.raba@snowflake.com>
2024-09-03 16:47:27 +0200

    SNOW-1055755: windows build debug - wix installer bundle - fix

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -30,12 +30,12 @@ signtool sign /debug /sm /t http://timestamp.digicert.com /a snow.exe
 cd ..\..
 candle ^
     -dSnowflakeCLIVersion=%CLI_VERSION% ^
-    scripts/packaging/win/snowflake_cli.wxs ^
-    scripts/packaging/win/snowflake_cli_exitdlg.wxs
+    scripts\packaging\win\snowflake_cli.wxs ^
+    scripts\packaging\win\snowflake_cli_exitdlg.wxs
 light snowflake_cli.wixobj ^
     snowflake_cli_exitdlg.wixobj ^
     -cultures:en-us ^
-    -loc scripts/packaging/win/snowflake_cli_en-us.wxl ^
+    -loc scripts\packaging\win\snowflake_cli_en-us.wxl ^
     -ext WixUIExtension ^
     -ext WixUtilExtension ^
     -o dist\snowflake-cli-windows_x86_64.msi
