2f1231236dd4f99474c090d4774341d666175008
Marcin Raba <marcin.raba@snowflake.com>
2024-09-10 16:40:54 +0200

    SNOW-1055755: windows build - install dotnet and wix 4c

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -4,15 +4,17 @@ set PATH=C:\Program Files\Python310\;c:\Program Files (x86)\Windows Kits\8.1\bin
 echo %PATH%
 
 @echo on
-dir /r "C:\Program Files (x86)"
 dir /r "C:\Program Files (x86)\WiX Toolset v3.11\bin"
+where candle
+where lighs
+where heat
 REM call scripts\packaging\win\dotnet-install.ps1 -Verbose
 REM dotnet tool install --global wix
 REM where wix
 REM wix --version
 REM wix extension add -g WixToolset.Util.wixext
 REM wix extension add -g WixToolset.UI.wixext
-
+exit
 python.exe --version
 python.exe -m pip install --upgrade pip uv hatch
 
