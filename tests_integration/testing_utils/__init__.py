7f5cea0db46429db37236d6a6f9d6f33efe91bef
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2024-09-13 11:54:52 +0200

    fixup! Unified testing utils, part 1

--- tests_integration/testing_utils/__init__.py
+++ tests_integration/testing_utils/__init__.py
@@ -16,5 +16,5 @@ from tests_integration.testing_utils.assertions.test_file_assertions import *
 from tests_integration.testing_utils.assertions.test_result_assertions import *
 from tests_integration.testing_utils.naming_utils import *
 from tests_integration.testing_utils.snowpark_utils import *
-from tests_integration.testing_utils.sql_utils import *
+from tests_integration.testing_utils.cursor_helpers import *
 from tests_common.path_utils import *
