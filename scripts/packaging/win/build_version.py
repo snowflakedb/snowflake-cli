05fe1bd3b1c6b0a3473074da60025901f469aa2e
Marcin Raba <marcin.raba@snowflake.com>
2024-09-17 15:57:31 +0200

    SNOW-1055755: wix3 installer - d4

--- scripts/packaging/win/build_version.py
+++ scripts/packaging/win/build_version.py
@@ -1,10 +1,21 @@
-from time import time
-
 from snowflake.cli.__about__ import VERSION
 
-if len(version := VERSION.split(".")) > 3:
-    version[-1] = str(int(time()))
-else:
-    version.append("0")
 
-print(".".join(version))
+def parse_version_for_windows_build() -> list[str]:
+    version = VERSION.split(".")
+    *msv, last = version
+
+    match last:
+        case last if last.isdigit():
+            version.append("0")
+        case last if "rc" in last:
+            version = msv + last.split("rc")
+        case last if "dev" in last:
+            version = msv + last.split("dev")
+
+    return version
+
+
+if __name__ == "__main__":
+    version = parse_version_for_windows_build()
+    print(".".join(version))
