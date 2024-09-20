68c136dfeb903528842be5cecce67dd4dbedef54
Francois Campbell <francois.campbell@snowflake.com>
2024-09-20 10:28:46 -0400

    Move version drop to app package

--- src/snowflake/cli/_plugins/nativeapp/version/version_processor.py
+++ src/snowflake/cli/_plugins/nativeapp/version/version_processor.py
@@ -15,30 +15,18 @@
 from __future__ import annotations
 
 from pathlib import Path
-from textwrap import dedent
 from typing import Dict, Optional
 
-import typer
-from click import ClickException
 from snowflake.cli._plugins.nativeapp.application_package_entity import (
     ApplicationPackageEntity,
-)
-from snowflake.cli._plugins.nativeapp.artifacts import (
-    find_version_info_in_manifest_file,
-)
-from snowflake.cli._plugins.nativeapp.exceptions import (
-    ApplicationPackageDoesNotExistError,
 )
 from snowflake.cli._plugins.nativeapp.manager import (
     NativeAppCommandProcessor,
     NativeAppManager,
 )
-from snowflake.cli._plugins.nativeapp.policy import PolicyBase
 from snowflake.cli._plugins.nativeapp.run_processor import NativeAppRunProcessor
 from snowflake.cli.api.console import cli_console as cc
 from snowflake.cli.api.project.schemas.v1.native_app.native_app import NativeApp
-from snowflake.cli.api.project.util import to_identifier
-from snowflake.connector import ProgrammingError
 
 
 class NativeAppVersionCreateProcessor(NativeAppRunProcessor):
@@ -89,76 +77,31 @@ class NativeAppVersionDropProcessor(NativeAppManager, NativeAppCommandProcessor)
     def process(
         self,
         version: Optional[str],
-        policy: PolicyBase,
-        is_interactive: bool,
+        force: bool,
+        interactive: bool,
         *args,
         **kwargs,
     ):
-        """
-        Drops a version defined in an application package. If --force is provided, then no user prompts will be executed.
-        """
-
-        # 1. Check for existing an existing application package
-        show_obj_row = self.get_existing_app_pkg_info()
-        if not show_obj_row:
-            raise ApplicationPackageDoesNotExistError(self.package_name)
-
-        # 2. Check distribution of the existing application package
-        actual_distribution = self.get_app_pkg_distribution_in_snowflake
-        if not self.verify_project_distribution(actual_distribution):
-            cc.warning(
-                f"Continuing to execute `snow app version drop` on application package {self.package_name} with distribution '{actual_distribution}'."
-            )
-
-        # 3. If the user did not pass in a version string, determine from manifest.yml
-        if not version:
-            cc.message(
-                dedent(
-                    f"""\
-                        Version was not provided through the Snowflake CLI. Checking version in the manifest.yml instead.
-                        This step will bundle your app artifacts to determine the location of the manifest.yml file.
-                    """
-                )
-            )
-            self.build_bundle()
-            version, _ = find_version_info_in_manifest_file(self.deploy_root)
-            if not version:
-                raise ClickException(
-                    "Manifest.yml file does not contain a value for the version field."
-                )
-
-        # Make the version a valid identifier, adding quotes if necessary
-        version = to_identifier(version)
-
-        cc.step(
-            dedent(
-                f"""\
-                    About to drop version {version} in application package {self.package_name}.
-                """
-            )
-        )
-
-        # If user did not provide --force, ask for confirmation
-        user_prompt = (
-            f"Are you sure you want to drop version {version} in application package {self.package_name}? Once dropped, this operation cannot be undone.",
-        )
-        if not policy.should_proceed(user_prompt):
-            if is_interactive:
-                cc.message("Not dropping version.")
-                raise typer.Exit(0)
-            else:
-                cc.message("Cannot drop version non-interactively without --force.")
-                raise typer.Exit(1)
-
-        # Drop the version
-        with self.use_role(self.package_role):
-            try:
-                self._execute_query(
-                    f"alter application package {self.package_name} drop version {version}"
-                )
-            except ProgrammingError as err:
-                raise err  # e.g. version is referenced in a release directive(s)
-
-        cc.message(
-            f"Version {version} in application package {self.package_name} dropped successfully."
+        return ApplicationPackageEntity.version_drop(
+            console=cc,
+            project_root=self.project_root,
+            deploy_root=self.deploy_root,
+            bundle_root=self.bundle_root,
+            generated_root=self.generated_root,
+            artifacts=self.artifacts,
+            package_name=self.package_name,
+            package_role=self.package_role,
+            package_distribution=self.package_distribution,
+            prune=True,
+            recursive=True,
+            paths=None,
+            print_diff=True,
+            validate=True,
+            stage_fqn=self.stage_fqn,
+            package_warehouse=self.package_warehouse,
+            post_deploy_hooks=self.package_post_deploy_hooks,
+            package_scripts=self.package_scripts,
+            version=version,
+            force=force,
+            interactive=interactive,
         )
