1a74b460e86d9e7855077869c861adffb773856b
Marcin Raba <marcin.raba@snowflake.com>
2024-09-25 09:52:22 +0200

    SNOW-1055755: macos postinstall issues - debug 18

--- scripts/packaging/macos/SnowflakeCLI.bash
+++ scripts/packaging/macos/SnowflakeCLI.bash
@@ -4,7 +4,7 @@ BASEDIR=$(dirname $0)
 osascript <<APPL_SCRIPT
 tell application "Terminal"
     if not (exists window 1) then reopen
-    do script "$BASEDIR/snow" in the last window
+    do script "$BASEDIR/snow/snow" in the last window
     set the bounds of the last window to {0, 0, 1400, 800}
     activate
 end tell
