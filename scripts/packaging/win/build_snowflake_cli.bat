a9ea0834cd142a181fd9630255e4630a3b0174df
Marcin Raba <marcin.raba@snowflake.com>
2024-09-26 12:25:31 +0200

    SNOW-1055755: win - move installer build to other node - s02e02

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -29,30 +29,3 @@ python.exe -m hatch -e packaging run ^
   %ENTRYPOINT%
 
 tar -a -c -f snowflake-cli-%CLI_VERSION%.zip dist\snow
-
-
-REM heat.exe dir dist\\snow\\_internal ^
-REM   -gg ^
-REM   -cg SnowflakeCLIInternalFiles ^
-REM   -dr TESTFILEPRODUCTDIR ^
-REM   -var var.SnowflakeCLIInternalFiles ^
-REM   -sfrag ^
-REM   -o _internal.wxs
-REM
-REM candle.exe ^
-REM   -arch x64 ^
-REM   -dSnowflakeCLIVersion=%CLI_VERSION% ^
-REM   -dSnowflakeCLIInternalFiles=dist\\snow\\_internal ^
-REM   scripts\packaging\win\snowflake_cli.wxs ^
-REM   scripts\packaging\win\snowflake_cli_exitdlg.wxs ^
-REM   _internal.wxs
-REM
-REM light.exe ^
-REM   -ext WixUIExtension ^
-REM   -ext WixUtilExtension ^
-REM   -cultures:en-us ^
-REM   -loc scripts\packaging\win\snowflake_cli_en-us.wxl ^
-REM   -out snowflake-cli-%CLI_VERSION%-x86_64.msi ^
-REM   snowflake_cli.wixobj ^
-REM   snowflake_cli_exitdlg.wixobj ^
-REM   _internal.wixobj
