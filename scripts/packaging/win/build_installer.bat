b0ce62b7989a4d33973dfa3cc45deac606162a0a
Patryk Czajka <patryk.czajka@snowflake.com>
2024-10-02 13:55:16 +0200

    move commands to one-line

--- scripts/packaging/win/build_installer.bat
+++ scripts/packaging/win/build_installer.bat
@@ -15,28 +15,28 @@ echo %CLI_VERSION%
 set CLI_VERSION=3.0.0.2
 set STAGE_URL=s3://sfc-eng-jenkins/repository/snowflake-cli/staging/dev/windows_x86_64/56041f1f1e5f229265dd28385d87a4e345038efc/snowflake-cli-3.0.0.2.zip
 
-aws s3 cp %STAGE_URL% .\wtf.zip && ^
+aws s3 cp %STAGE_URL% . && ^
 dir && ^
-tar -xf snowflake-cli-%CLI_VERSION%.zip
-
-signtool sign /debug /sm /t http://timestamp.digitcert.com /a dist\snow\snow.exe
-
+tar -xf snowflake-cli-%CLI_VERSION%.zip && ^
+^
+signtool sign /debug /sm /t http://timestamp.digitcert.com /a dist\snow\snow.exe && ^
+^
 heat.exe dir dist\snow\_internal ^
    -gg ^
    -cg SnowflakeCLIInternalFiles ^
    -dr TESTFILEPRODUCTDIR ^
    -var var.SnowflakeCLIInternalFiles ^
    -sfrag ^
-   -o _internal.wxs
-
+   -o _internal.wxs && ^
+^
 candle.exe ^
   -arch x64 ^
   -dSnowflakeCLIVersion=%CLI_VERSION% ^
   -dSnowflakeCLIInternalFiles=dist\\snow\\_internal ^
   scripts\packaging\win\snowflake_cli.wxs ^
   scripts\packaging\win\snowflake_cli_exitdlg.wxs ^
-  _internal.wxs
-
+  _internal.wxs && ^
+^
 light.exe ^
   -ext WixUIExtension ^
   -ext WixUtilExtension ^
@@ -45,6 +45,6 @@ light.exe ^
   -out snowflake-cli-%CLI_VERSION%-x86_64.msi ^
   snowflake_cli.wixobj ^
   snowflake_cli_exitdlg.wixobj ^
-  _internal.wixobj
-
+  _internal.wixobj && ^
+^
 signtool sign /debug /sm /t http://timestamp.digitcert.com /a snowflake-cli-%CLI_VERSION%-x86_64.msi
