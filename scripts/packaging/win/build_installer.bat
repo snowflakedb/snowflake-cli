68ca57abbd905233206649327e2c92bf23f2443d
Patryk Czajka <patryk.czajka@snowflake.com>
2024-10-02 16:47:06 +0200

    split commands

--- scripts/packaging/win/build_installer.bat
+++ scripts/packaging/win/build_installer.bat
@@ -22,23 +22,27 @@ set CLI_MSI=snowflake-cli-%CLI_VERSION%-x86_64.msi
 set STAGE_URL=s3://sfc-eng-jenkins/repository/snowflake-cli/staging/%RELEASE_TYPE%/windows_x86_64/%REVISION%
 
 cmd /c aws s3 cp %STAGE_URL%/%CLI_ZIP% .
-7z x %CLI_ZIP% && ^
-dir && ^
-signtool sign /debug /sm /t http://timestamp.digicert.com /a dist\snow\snow.exe && ^
+
+7z x %CLI_ZIP% &&
+
+signtool sign /debug /sm /t http://timestamp.digicert.com /a dist\snow\snow.exe
+
 heat.exe dir dist\snow\_internal ^
    -gg ^
    -cg SnowflakeCLIInternalFiles ^
    -dr TESTFILEPRODUCTDIR ^
    -var var.SnowflakeCLIInternalFiles ^
    -sfrag ^
-   -o _internal.wxs && ^
+   -o _internal.wxs
+
 candle.exe ^
   -arch x64 ^
   -dSnowflakeCLIVersion=%CLI_VERSION% ^
   -dSnowflakeCLIInternalFiles=dist\\snow\\_internal ^
   scripts\packaging\win\snowflake_cli.wxs ^
   scripts\packaging\win\snowflake_cli_exitdlg.wxs ^
-  _internal.wxs && ^
+  _internal.wxs
+
 light.exe ^
   -ext WixUIExtension ^
   -ext WixUtilExtension ^
@@ -47,6 +51,8 @@ light.exe ^
   -out %CLI_MSI% ^
   snowflake_cli.wixobj ^
   snowflake_cli_exitdlg.wixobj ^
-  _internal.wixobj && ^
-signtool sign /debug /sm /t http://timestamp.digicert.com /a %CLI_MSI% && ^
+  _internal.wixobj
+
+signtool sign /debug /sm /t http://timestamp.digicert.com /a %CLI_MSI%
+
 aws s3 cp %CLI_MSI% %STAGE_URL%/%CLI_MSI%
