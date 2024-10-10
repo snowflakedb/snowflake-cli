537109c1838cd485adc309f2a9ffd4aad49eb621
Pawel Job <pawel.job@snowflake.com>
2024-10-10 17:52:58 +0200

    Fix RELEASE_TYPE

--- scripts/packaging/win/build_installer.bat
+++ scripts/packaging/win/build_installer.bat
@@ -8,14 +8,16 @@ python.exe -c "import platform as p; print(f'{p.system()=}, {p.architecture()=}'
 python.exe -m pip install hatch
 FOR /F "delims=" %%I IN ('hatch run packaging:win-build-version') DO SET CLI_VERSION=%%I
 FOR /F "delims=" %%I IN ('git rev-parse %svnRevision%') DO SET REVISION=%%I
-@echo off
+FOR /F "delims=" %%I IN ('echo %releaseType%') DO SET RELEASE_TYPE=%%I
+
 echo CLI_VERSION = %CLI_VERSION%
 echo REVISION = %REVISION%`
-@echo on
+echo RELEASE_TYPE = %RELEASE_TYPE%`
 
 set CLI_ZIP=snowflake-cli-%CLI_VERSION%.zip
 set CLI_MSI=snowflake-cli-%CLI_VERSION%-x86_64.msi
 set STAGE_URL=s3://sfc-eng-jenkins/repository/snowflake-cli/staging/%RELEASE_TYPE%/windows_x86_64/%REVISION%
+set RELEASE_URL=s3://sfc-eng-jenkins/repository/snowflake-cli/%RELEASE_TYPE%/windows_x86_64/%REVISION%
 
 echo "[INFO] downloading artifacts"
 cmd /c aws s3 cp %STAGE_URL%/%CLI_ZIP% . || goto :error
@@ -56,7 +58,7 @@ signtool sign /debug /sm /d "Snowflake CLI" /t http://timestamp.digicert.com /a 
 signtool verify /v /pa %CLI_MSI% || goto :error
 
 echo "[INFO] uploading artifacts"
-cmd /c aws s3 cp %CLI_MSI% %STAGE_URL%/%CLI_MSI% || goto :error
+cmd /c aws s3 cp %CLI_MSI% %RELEASE_URL%/%CLI_MSI% || goto :error
 
 REM FINISH SCRIPT EXECUTION HERE
 GOTO :EOF
