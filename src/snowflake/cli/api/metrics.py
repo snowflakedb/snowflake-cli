f7b6f7ebe8ed3ea020bd86be64b3167ed095d957
Marcus Chok <marcus.chok@snowflake.com>
2024-10-29 14:24:56 -0400

    step->span, convert to dataclass, add limits, combine tests and add more cases

--- src/snowflake/cli/api/metrics.py
+++ src/snowflake/cli/api/metrics.py
@@ -14,18 +14,21 @@
 from __future__ import annotations
 
 import time
+import uuid
 from contextlib import contextmanager
-from itertools import count
-from typing import Dict, List, Optional, Union
+from dataclasses import dataclass, field
+from typing import ClassVar, Dict, Iterator, List, Optional
 
 
 class CLIMetricsInvalidUsageError(RuntimeError):
     """
     Indicative of bug in the code where a call to CLIMetrics was made erroneously
+
+    We do not want metrics errors to break the execution of commands,
+    so only raise this error in the event that an invariant was broken during setup
     """
 
-    def __init__(self, message: str):
-        super().__init__(message)
+    pass
 
 
 class _TypePrefix:
@@ -67,104 +70,66 @@ class CLICounterField:
     PACKAGE_SCRIPTS = f"{_TypePrefix.FEATURES}.{_DomainPrefix.APP}.package_scripts"
 
 
-class _CLIMetricsStep:
+@dataclass
+class CLIMetricsSpan:
     """
-    class for holding metrics step data and encapsulating related operations
+    class for holding metrics span data and encapsulating related operations
     """
-
-    _id_counter = count(start=1, step=1)
 
     # keys for dict representation
-    ID_KEY = "id"
-    NAME_KEY = "name"
-    PARENT_KEY = "parent"
-    PARENT_ID_KEY = "parent_id"
-    START_TIME_KEY = "start_time"
-    EXECUTION_TIME_KEY = "execution_time"
-    ERROR_KEY = "error"
+    ID_KEY: ClassVar[str] = "id"
+    NAME_KEY: ClassVar[str] = "name"
+    PARENT_KEY: ClassVar[str] = "parent"
+    PARENT_ID_KEY: ClassVar[str] = "parent_id"
+    START_TIME_KEY: ClassVar[str] = "start_time"
+    EXECUTION_TIME_KEY: ClassVar[str] = "execution_time"
+    ERROR_KEY: ClassVar[str] = "error"
 
-    def __init__(self, name: str, parent: _CLIMetricsStep | None = None):
-        if not name:
+    # ensure we get unique ids for each step for the parent-child link in case of steps with the same name
+    step_id: str = field(init=False, default_factory=lambda: uuid.uuid4().hex)
+    name: str
+    # start time of this step relative to when the command first started executing
+    start_time: float
+    parent: Optional[CLIMetricsSpan] = field(default=None)
+    execution_time: Optional[float] = field(init=False, default=None)
+    error: Optional[BaseException] = field(init=False, default=None)
+
+    # start time of the step from the monotonic clock in order to calculate execution time
+    _monotonic_start: float = field(init=False, default_factory=time.monotonic)
+
+    def __post_init__(self):
+        if not self.name:
             raise CLIMetricsInvalidUsageError("step name must not be empty")
 
-        # to disambiguate steps with same name
-        self._step_id: int = next(self._id_counter)
-        self._name: str = name
+    def finish(self, error: Optional[BaseException] = None) -> None:
+        """
+        Sets the execution time and (optionally) error raised for the span
 
-        self._parent: Optional[str] = parent.name if parent is not None else None
-        self._parent_id: Optional[int] = parent.step_id if parent is not None else None
-
-        self._monotonic_start: Optional[
-            float
-        ] = None  # monotonic time for execution time
-        self._start_time: Optional[float] = None  # timestamp
-        self._execution_time: Optional[float] = None
-
-        self._error: Optional[str] = None
-
-    def start(self) -> None:
-        if self._monotonic_start:
-            raise CLIMetricsInvalidUsageError("step has already started")
-        self._start_time = time.time()
-        self._monotonic_start = time.monotonic()
-
-    def end(self, error: Optional[BaseException] = None) -> None:
-        if self._execution_time:
-            raise CLIMetricsInvalidUsageError("step has already ended")
-        if not self._monotonic_start:
-            raise CLIMetricsInvalidUsageError("step has not started")
+        If already called, this method is a no-op
+        """
+        if self.execution_time is not None:
+            return
 
         if error:
-            self._error = type(error).__name__
+            self.error = error
 
-        self._execution_time = time.monotonic() - self._monotonic_start
+        self.execution_time = time.monotonic() - self._monotonic_start
 
-    @property
-    def step_id(self) -> int:
-        return self._step_id
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def parent(self) -> Optional[str]:
-        return self._parent
-
-    @property
-    def parent_id(self) -> Optional[int]:
-        return self._parent_id
-
-    @property
-    def start_time(self) -> float:
-        if not self._start_time:
-            raise CLIMetricsInvalidUsageError("start_time accessed before step started")
-        return self._start_time
-
-    @property
-    def execution_time(self) -> float:
-        if not self._execution_time:
-            raise CLIMetricsInvalidUsageError(
-                "execution_time accessed before step ended"
-            )
-        return self._execution_time
-
-    @property
-    def error(self) -> Optional[str]:
+    def to_dict(self) -> Dict:
         """
-        name of the error class that was raised during the execution of this step
+        Custom dict conversion function to be used for reporting telemetry, with only the required fields
         """
-        return self._error
 
-    def to_dict(self) -> Dict[str, Union[str, int, float, None]]:
         return {
             self.ID_KEY: self.step_id,
             self.NAME_KEY: self.name,
-            self.PARENT_KEY: self.parent,
-            self.PARENT_ID_KEY: self.parent_id,
             self.START_TIME_KEY: self.start_time,
+            self.PARENT_KEY: self.parent.name if self.parent is not None else None,
+            self.PARENT_ID_KEY: self.parent.step_id
+            if self.parent is not None
+            else None,
             self.EXECUTION_TIME_KEY: self.execution_time,
-            self.ERROR_KEY: self.error,
+            self.ERROR_KEY: type(self.error).__name__ if self.error else None,
         }
 
 
@@ -173,18 +138,29 @@ class CLIMetrics:
     Class to track various metrics across the execution of a command
     """
 
+    # limit for number of nested spans throughout execution
+    IN_PROGRESS_SPANS_DEPTH_LIMIT = 5
+    # limit for number of total spans being reported
+    COMPLETED_SPANS_TOTAL_LIMIT = 100
+
     def __init__(self):
         self._counters: Dict[str, int] = {}
-        # stack of current steps as command is executing
-        self._executing_steps: List[_CLIMetricsStep] = []
+        # stack of in progress steps as command is executing
+        self._in_progress_spans: List[CLIMetricsSpan] = []
         # list of finished steps for telemetry to process
-        self._finished_steps: List[_CLIMetricsStep] = []
+        self._completed_spans: List[CLIMetricsSpan] = []
+        # count of spans dropped due to reaching depth limit
+        self.num_spans_past_depth_limit: int = 0
+        # count of spans dropped due to reaching total limit
+        self.num_spans_past_total_limit: int = 0
+        # monotonic clock time of when this class was initialized to approximate when the command first started executing
+        self.monotonic_start = time.monotonic()
 
     def __eq__(self, other):
         if isinstance(other, CLIMetrics):
             return (
                 self._counters == other._counters
-                and self._finished_steps == other._finished_steps
+                and self._completed_spans == other._completed_spans
             )
         return False
 
@@ -208,96 +184,50 @@ class CLIMetrics:
             self._counters[name] += value
 
     @property
-    def _current_step(self) -> Optional[_CLIMetricsStep]:
-        return self._executing_steps[-1] if len(self._executing_steps) > 0 else None
+    def current_span(self) -> Optional[CLIMetricsSpan]:
+        return self._in_progress_spans[-1] if len(self._in_progress_spans) > 0 else None
 
     @contextmanager
-    def track_step(self, name: str):
+    def start_span(self, name: str) -> Iterator[CLIMetricsSpan]:
         """
-        Recommended general use API for tracking a step throughout the execution of a command
-        Assumes that parent steps contain the entirety of their child steps
+        Starts a new span that tracks various metrics throughout its execution
 
-        start_step and end_step are exposed in case the caller needs more flexibility
+        Assumes that parent steps contain the entirety of their child steps
+        Parent steps are automatically populated as the most recently executed step, like a stack
+
+        Only one span can be active at a time; once one span finishes its parent becomes the active span
+
+        Spans are not emitted in telemetry if depth/total limits are exceeded
 
         :raises CliMetricsInvalidUsageError: if the step name is empty
         """
-        step_id = self.start_step(name)
+        new_step = CLIMetricsSpan(
+            name=name,
+            start_time=time.monotonic() - self.monotonic_start,
+            parent=self.current_span,
+        )
+
+        if len(self._in_progress_spans) >= self.IN_PROGRESS_SPANS_DEPTH_LIMIT:
+            self.num_spans_past_depth_limit += 1
+        else:
+            self._in_progress_spans.append(new_step)
+
         try:
-            yield
+            yield new_step
         except BaseException as err:
-            self.end_step(step_id=step_id, error=err)
+            new_step.finish(error=err)
             raise
         else:
-            self.end_step(step_id=step_id)
+            new_step.finish()
+        finally:
+            if len(self._completed_spans) >= self.COMPLETED_SPANS_TOTAL_LIMIT:
+                self.num_spans_past_total_limit += 1
 
-    def start_step(self, name: str) -> int:
-        """
-        Command for manually tracking steps throughout the execution of a command
-        E.g. through different scopes/files for when a context manager is not enough
-        Requires a corresponding call to end_step with the (optional) exception caught
+            if new_step in self._in_progress_spans:
+                self._in_progress_spans.remove(new_step)
 
-        :returns: id of the started step which can be passed to end_step
-
-        :raises CliMetricsInvalidUsageError: if the step name is empty
-        """
-        parent_step = self._current_step
-        new_step = _CLIMetricsStep(name, parent_step)
-        self._executing_steps.append(new_step)
-        new_step.start()
-        return new_step.step_id
-
-    def end_step(
-        self,
-        step_id: Optional[int] = None,
-        step_name: Optional[str] = None,
-        error: Optional[BaseException] = None,
-    ) -> None:
-        """
-        Manually ends either the most recently started step or the step with the provided id/name
-
-        Context managers can automatically check if there were errors, but if manually
-        starting/ending steps, then the caller will need to catch and pass the error themselves
-
-        :raises CliMetricsInvalidUsageError: if step could not be found or there are no executing steps
-        """
-        if step_id is not None:
-            found_step = next(
-                (step for step in self._executing_steps if step.step_id == step_id),
-                None,
-            )
-        elif step_name is not None:
-            found_step = next(
-                (
-                    step
-                    for step in reversed(self._executing_steps)
-                    if step.name == step_name
-                ),
-                None,
-            )
-        else:
-            found_step = self._current_step
-
-        if not found_step:
-            if step_id or step_name:
-                raise CLIMetricsInvalidUsageError(
-                    f"step with {'id' if step_id else 'name'} '{step_id or step_name}' could not be ended because it could not be found"
-                )
-            else:
-                raise CLIMetricsInvalidUsageError(
-                    "current step could not be ended because no steps are executing"
-                )
-
-        found_step.end(error)
-        self._executing_steps.remove(found_step)
-        self._finished_steps.append(found_step)
-
-    def flush_steps(self, error: Optional[BaseException] = None) -> None:
-        """
-        Useful if you are using the manual start/end steps and an error
-        propagated up, requiring you to clear out all the executing steps
-        """
-        while self._current_step:
-            self.end_step(error=error)
+                if len(self._completed_spans) < self.COMPLETED_SPANS_TOTAL_LIMIT:
+                    self._completed_spans.append(new_step)
 
     @property
     def counters(self) -> Dict[str, int]:
@@ -305,11 +235,11 @@ class CLIMetrics:
         return self._counters.copy()
 
     @property
-    def steps(self) -> List[Dict[str, Union[str, int, float, None]]]:
+    def completed_spans(self) -> List[Dict]:
         """
-        returns the finished steps tracked throughout a command, sorted by start time
+        returns the completed spans tracked throughout a command, sorted by start time, for reporting telemetry
         """
         return [
             step.to_dict()
-            for step in sorted(self._finished_steps, key=lambda step: step.start_time)
+            for step in sorted(self._completed_spans, key=lambda step: step.start_time)
         ]
---
+++ tests/api/test_metrics_spans.py
@@ -0,0 +1,282 @@
+# Copyright (c) 2024 Snowflake Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import uuid
+
+import pytest
+from snowflake.cli.api.metrics import (
+    CLIMetrics,
+    CLIMetricsInvalidUsageError,
+    CLIMetricsSpan,
+)
+
+
+# helper for testing span depth limit edge case
+def create_nested_spans_recursively(metrics: CLIMetrics, num_spans: int = 1) -> None:
+    @metrics.start_span("nested_span")
+    def create_span():
+        nonlocal num_spans
+        num_spans -= 1
+
+        if num_spans > 0:
+            create_span()
+
+    create_span()
+
+
+# helper for testing span total limit edge case
+def create_spans_sequentially(metrics: CLIMetrics, num_spans: int = 1) -> None:
+    while num_spans > 0:
+        with metrics.start_span("sequential_span"):
+            pass
+        num_spans -= 1
+
+
+def test_metrics_spans_initialization_empty():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    assert metrics.current_span is None
+
+    # then
+    assert metrics.completed_spans == []
+    assert metrics.num_spans_past_total_limit == 0
+    assert metrics.num_spans_past_depth_limit == 0
+
+
+def test_metrics_spans_single_span_no_error_or_parent():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    with metrics.start_span("span1") as span1:
+        assert metrics.current_span is span1
+
+    assert metrics.current_span is None
+
+    # then
+    assert len(metrics.completed_spans) == 1
+    span1_dict = metrics.completed_spans[0]
+
+    assert uuid.UUID(
+        hex=span1_dict[CLIMetricsSpan.ID_KEY]
+    )  # will raise ValueError if not valid uuid
+    assert span1_dict[CLIMetricsSpan.NAME_KEY] == "span1"
+    assert span1_dict[CLIMetricsSpan.START_TIME_KEY] > 0
+    assert span1_dict[CLIMetricsSpan.EXECUTION_TIME_KEY] > 0
+    assert span1_dict[CLIMetricsSpan.ERROR_KEY] is None
+    assert span1_dict[CLIMetricsSpan.PARENT_KEY] is None
+    assert span1_dict[CLIMetricsSpan.PARENT_ID_KEY] is None
+
+
+def test_metrics_spans_finish_early_is_idempotent():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    with metrics.start_span("span1") as span1:
+        start_time = span1.start_time
+        span1.finish()
+        execution_time = span1.execution_time
+
+    # then
+    assert len(metrics.completed_spans) == 1
+    span1_dict = metrics.completed_spans[0]
+    assert span1_dict[CLIMetricsSpan.START_TIME_KEY] == start_time
+    assert span1_dict[CLIMetricsSpan.EXECUTION_TIME_KEY] == execution_time
+
+
+def test_metrics_spans_parent_with_one_child():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    with metrics.start_span("parent") as parent:
+        assert metrics.current_span is parent
+
+        with metrics.start_span("child") as child:
+            assert metrics.current_span is child
+
+        assert metrics.current_span is parent
+
+    assert metrics.current_span is None
+
+    # then
+    assert len(metrics.completed_spans) == 2
+    parent_dict, child_dict = metrics.completed_spans
+
+    assert parent_dict[CLIMetricsSpan.ID_KEY] != child_dict[CLIMetricsSpan.ID_KEY]
+    assert (
+        child_dict[CLIMetricsSpan.PARENT_ID_KEY] == parent_dict[CLIMetricsSpan.ID_KEY]
+    )
+
+    assert child_dict[CLIMetricsSpan.NAME_KEY] == "child"
+    assert (
+        child_dict[CLIMetricsSpan.PARENT_KEY]
+        == parent_dict[CLIMetricsSpan.NAME_KEY]
+        == "parent"
+    )
+
+    assert (
+        parent_dict[CLIMetricsSpan.EXECUTION_TIME_KEY]
+        >= child_dict[CLIMetricsSpan.EXECUTION_TIME_KEY]
+    )
+    assert (
+        parent_dict[CLIMetricsSpan.START_TIME_KEY]
+        <= child_dict[CLIMetricsSpan.START_TIME_KEY]
+    )
+
+
+def test_metrics_spans_parent_with_two_children_same_name():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    with metrics.start_span("parent") as parent:
+        assert metrics.current_span is parent
+
+        with metrics.start_span("child") as child1:
+            assert metrics.current_span is child1
+
+        assert metrics.current_span is parent
+
+        with metrics.start_span("child") as child2:
+            assert metrics.current_span is child2
+
+        assert metrics.current_span is parent
+
+    assert metrics.current_span is None
+
+    # then
+    assert len(metrics.completed_spans) == 3
+    parent_dict, child1_dict, child2_dict = metrics.completed_spans
+
+    assert (
+        parent_dict[CLIMetricsSpan.ID_KEY]
+        != child1_dict[CLIMetricsSpan.ID_KEY]
+        != child2_dict[CLIMetricsSpan.ID_KEY]
+    )
+
+    assert (
+        child1_dict[CLIMetricsSpan.PARENT_ID_KEY]
+        == child2_dict[CLIMetricsSpan.PARENT_ID_KEY]
+        == parent_dict[CLIMetricsSpan.ID_KEY]
+    )
+
+    assert (
+        child1_dict[CLIMetricsSpan.NAME_KEY]
+        == child2_dict[CLIMetricsSpan.NAME_KEY]
+        == "child"
+    )
+
+    assert (
+        child1_dict[CLIMetricsSpan.PARENT_KEY]
+        == child2_dict[CLIMetricsSpan.PARENT_KEY]
+        == parent_dict[CLIMetricsSpan.NAME_KEY]
+        == "parent"
+    )
+
+    assert (
+        parent_dict[CLIMetricsSpan.EXECUTION_TIME_KEY]
+        >= child1_dict[CLIMetricsSpan.EXECUTION_TIME_KEY]
+    )
+    assert (
+        parent_dict[CLIMetricsSpan.EXECUTION_TIME_KEY]
+        >= child2_dict[CLIMetricsSpan.EXECUTION_TIME_KEY]
+    )
+
+    assert (
+        parent_dict[CLIMetricsSpan.START_TIME_KEY]
+        <= child1_dict[CLIMetricsSpan.START_TIME_KEY]
+        <= child2_dict[CLIMetricsSpan.START_TIME_KEY]
+    )
+
+
+def test_metrics_spans_duplicate_name_spans_with_one_under_parent():
+    pass
+
+
+def test_metrics_spans_error_is_propagated():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    with pytest.raises(RuntimeError):
+        with metrics.start_span("step1"):
+            raise RuntimeError()
+
+    # then
+    assert len(metrics.completed_spans) == 1
+    step1_dict = metrics.completed_spans[0]
+    assert step1_dict[CLIMetricsSpan.ERROR_KEY] == "RuntimeError"
+
+
+def test_metrics_spans_empty_name_raises_error():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    with pytest.raises(CLIMetricsInvalidUsageError) as err:
+        with metrics.start_span(""):
+            pass
+
+    # then
+    assert err.match("step name must not be empty")
+
+
+def test_metrics_spans_passing_depth_limit_should_add_to_counter_and_not_emit():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    create_nested_spans_recursively(
+        metrics, num_spans=CLIMetrics.IN_PROGRESS_SPANS_DEPTH_LIMIT + 3
+    )
+
+    # then
+    assert len(metrics.completed_spans) == CLIMetrics.IN_PROGRESS_SPANS_DEPTH_LIMIT
+    assert metrics.num_spans_past_depth_limit == 3
+
+
+def test_metrics_spans_passing_total_limit_should_add_to_counter_and_not_emit():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    create_spans_sequentially(
+        metrics, num_spans=CLIMetrics.COMPLETED_SPANS_TOTAL_LIMIT + 5
+    )
+
+    # then
+    assert len(metrics.completed_spans) == CLIMetrics.COMPLETED_SPANS_TOTAL_LIMIT
+    assert metrics.num_spans_past_total_limit == 5
+
+
+def test_metrics_spans_passing_total_and_depth_limit_should_add_to_both_counters_and_not_emit():
+    # given
+    metrics = CLIMetrics()
+
+    # when
+    # the extra 10 spans are dropped from both the in progress and completed spans lists
+    create_nested_spans_recursively(
+        metrics, num_spans=CLIMetrics.IN_PROGRESS_SPANS_DEPTH_LIMIT + 10
+    )
+    create_spans_sequentially(metrics, num_spans=CLIMetrics.COMPLETED_SPANS_TOTAL_LIMIT)
+
+    # then
+    assert len(metrics.completed_spans) == CLIMetrics.COMPLETED_SPANS_TOTAL_LIMIT
+    assert (
+        metrics.num_spans_past_total_limit == CLIMetrics.IN_PROGRESS_SPANS_DEPTH_LIMIT
+    )
+    assert metrics.num_spans_past_depth_limit == 10
