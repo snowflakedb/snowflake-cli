e8a05b8da6628537fd571c805b5f58ba1616dd73
Francois Campbell <francois.campbell@snowflake.com>
2024-11-04 13:56:02 -0500

    Make test handle more cases

--- tests_integration/helpers/test_v1_to_v2.py
+++ tests_integration/helpers/test_v1_to_v2.py
@@ -10,7 +10,7 @@ from tests.nativeapp.factories import ProjectV11Factory
 def test_v1_to_v2_converts_templates_in_files(temp_dir, runner):
     native_app_name = "my_native_app_project"
 
-    expected_conversions = {
+    src_to_result = {
         # Reference to app field should be converted to v2
         "<% ctx.native_app.application.name %>": "<% ctx.entities.app.identifier %>",
         # Reference to nested app field should be converted to v2
@@ -26,24 +26,43 @@ def test_v1_to_v2_converts_templates_in_files(temp_dir, runner):
         # Reference to fn field should remain as is
         "<% fn.get_username('test') %>": "<% fn.get_username('test') %>",
     }
+    source_contents = "\n".join(src_to_result.keys())
+    expected_templated_contents = "\n".join(src_to_result.values())
 
-    file_with_templates = "app/file.txt"
+    files_using_template_processor = [
+        "templated.txt",
+        "app/templated.txt",
+        "app/manifest.yml",
+        "nested/dir/templated.txt",
+    ]
+    files_not_using_template_processor = [
+        "untemplated.txt",
+    ]
+
     ProjectV11Factory(
         pdf__native_app__name=native_app_name,
         pdf__native_app__package__name="my_pkg",
         pdf__native_app__application__name="my_app",
         pdf__native_app__artifacts=[
-            dict(src="app/*", processors=[TEMPLATES_PROCESSOR])
+            dict(src="templated.txt", processors=[TEMPLATES_PROCESSOR]),
+            dict(src="untemplated.txt"),
+            dict(src="app/*", processors=[TEMPLATES_PROCESSOR]),
+            dict(src="nested/*", processors=[TEMPLATES_PROCESSOR]),
         ],
         files={
-            "app/manifest.yml": "",  # It just needs to exist for the definition conversion
-            file_with_templates: "\n".join(expected_conversions.keys()),
+            filename: source_contents
+            for filename in (
+                files_using_template_processor + files_not_using_template_processor
+            )
         },
     )
 
     result = runner.invoke(["helpers", "v1-to-v2"], env={"FOO": "bar"})
     assert result.exit_code == 0, result.output
 
-    file_contents = Path(file_with_templates).read_text()
-    expected_file_contents = "\n".join(expected_conversions.values())
-    assert file_contents == expected_file_contents, file_contents
+    for filename in files_using_template_processor:
+        file_contents = Path(filename).read_text()
+        assert file_contents == expected_templated_contents, filename
+    for filename in files_not_using_template_processor:
+        file_contents = Path(filename).read_text()
+        assert file_contents == source_contents, filename
