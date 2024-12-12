b1c9131b650dfb3c0819ab997a3d50a593c5a52b
Jan Sikorski <jan.sikorski@snowflake.com>
2024-12-12 16:53:59 +0100

    outline

--- src/snowflake/cli/_plugins/snowpark/snowpark_entity.py
+++ src/snowflake/cli/_plugins/snowpark/snowpark_entity.py
@@ -4,14 +4,37 @@ from snowflake.cli._plugins.snowpark.snowpark_entity_model import (
     FunctionEntityModel,
     ProcedureEntityModel,
 )
+from snowflake.cli._plugins.workspace.context import ActionContext
 from snowflake.cli.api.entities.common import EntityBase
 
 T = TypeVar("T")
 
 
 class SnowparkEntity(EntityBase[Generic[T]]):
-    pass
 
+    @property
+    def project_root(self):
+        return self._workspace_ctx.project_root
+
+    def action_bundle(self, action_ctx: ActionContext, *args, **kwargs):
+        pass
+
+    def action_deploy(self, action_ctx: ActionContext, *args, **kwargs):
+        pass
+
+    def action_drop(self, action_ctx: ActionContext, *args, **kwargs):
+        pass
+
+    def action_describe(self, action_ctx: ActionContext, *args, **kwargs):
+        pass
+
+    def action_execute(self, action_ctx: ActionContext, *args, **kwargs):
+        pass
+
+    def get_deploy_sql(self):
+        pass
+
+    def get_usage_grant_sql
 
 class FunctionEntity(SnowparkEntity[FunctionEntityModel]):
     """
