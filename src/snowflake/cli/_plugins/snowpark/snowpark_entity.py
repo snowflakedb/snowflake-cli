4d4045ffeffff03ea30397c26b6b4aff5bfa21ff
Jan Sikorski <jan.sikorski@snowflake.com>
2024-12-19 14:31:30 +0100

    Tests

--- src/snowflake/cli/_plugins/snowpark/snowpark_entity.py
+++ src/snowflake/cli/_plugins/snowpark/snowpark_entity.py
@@ -1,10 +1,18 @@
 import functools
 from pathlib import Path
-from typing import Generic, TypeVar, List, Optional
+from typing import Generic, List, Optional, TypeVar
 
-from requests.packages import package
+from click import ClickException
+
 
 from snowflake.cli._plugins.snowpark import package_utils
+from snowflake.cli._plugins.snowpark.package.anaconda_packages import (
+    AnacondaPackages,
+    AnacondaPackagesManager,
+)
+from snowflake.cli._plugins.snowpark.package_utils import (
+    DownloadUnavailablePackagesResult,
+)
 from snowflake.cli._plugins.snowpark.snowpark_entity_model import (
     FunctionEntityModel,
     ProcedureEntityModel,
@@ -12,13 +20,11 @@ from snowflake.cli._plugins.snowpark.snowpark_entity_model import (
 from snowflake.cli._plugins.workspace.context import ActionContext
 from snowflake.cli.api.entities.common import EntityBase, get_sql_executor
 from snowflake.cli.api.secure_path import SecurePath
-from test_data.test_data import requirements, anaconda_response
-
+from snowflake.cli._plugins.snowpark.zipper import zip_dir
 T = TypeVar("T")
 
 
 class SnowparkEntity(EntityBase[Generic[T]]):
-
     @property
     def root(self):
         return self._workspace_ctx.project_root
@@ -32,7 +38,9 @@ class SnowparkEntity(EntityBase[Generic[T]]):
         return self.model.fqn
 
     @functools.cached_property
-    def _sql_executor(self): # maybe this could be moved to parent class, as it is used in streamlit entity as well
+    def _sql_executor(
+        self,
+    ):  # maybe this could be moved to parent class, as it is used in streamlit entity as well
         return get_sql_executor()
 
     @functools.cached_property
@@ -43,8 +51,8 @@ class SnowparkEntity(EntityBase[Generic[T]]):
     def model(self):
         return self._entity_model  # noqa
 
-    def action_bundle(self, action_ctx: ActionContext, *args, **kwargs):
-        pass
+    def action_bundle(self, action_ctx: ActionContext, output_dir: Path | None, ignore_anaconda: bool, skip_version_check: bool, index_url: str | None = None, allow_shared_libraries: bool = False,  *args, **kwargs):
+        return self.bundle(output_dir, ignore_anaconda, skip_version_check, index_url, allow_shared_libraries)
 
     def action_deploy(self, action_ctx: ActionContext, *args, **kwargs):
         pass
@@ -55,27 +63,52 @@ class SnowparkEntity(EntityBase[Generic[T]]):
     def action_describe(self, action_ctx: ActionContext, *args, **kwargs):
         return self._sql_executor.execute_query(self.get_describe_sql())
 
-    def action_execute(self, action_ctx: ActionContext, execution_arguments: List[str] = None, *args, **kwargs):
-        return self._sql_executor.execute_query(self.get_execute_sql(execution_arguments))
+    def action_execute(
+        self,
+        action_ctx: ActionContext,
+        execution_arguments: List[str] = None,
+        *args,
+        **kwargs,
+    ):
+        return self._sql_executor.execute_query(
+            self.get_execute_sql(execution_arguments)
+        )
 
-    def bundle(self, output_dir: Optional[Path]):
-        #0 Create a directory for the entity
+    def bundle(self, output_dir: Path | None, ignore_anaconda: bool, skip_version_check: bool, index_url: str | None = None, allow_shared_libraries: bool = False) -> List[Path]:
+        '''
+        Bundles the entity artifacts and dependencies into a directory.
+        Parameters:
+            output_dir: The directory to output the bundled artifacts to. Defaults to otput dir in project root
+            ignore_anaconda: If True, ignores anaconda chceck and tries to download all packages using pip
+            skip_version_check: If True, skips version check when downloading packages
+            index_url: The index URL to use when downloading packages, if none set - default pip index is used (in most cases- Pypi)
+            allow_shared_libraries: If not set to True, using dependency with .so/.dll files will raise an exception
+        Returns:
+        '''
+        # 0 Create a directory for the entity
         if not output_dir:
             output_dir = self.root / "output" / self.model.stage
         output_dir.mkdir(parents=True, exist_ok=True)
 
         output_files = []
 
-        #1 Check if requirements exits
+        # 1 Check if requirements exits
         if (self.root / "requirements.txt").exists():
+            download_results = self._process_requirements(
+                bundle_dir= output_dir,
+                archive_name= "dependencies.zip",
+                requirements_file=SecurePath(self.root / "requirements.txt"),
+                ignore_anaconda=ignore_anaconda,
+                skip_version_check=skip_version_check,
+                index_url=index_url,
+                allow_shared_libraries=allow_shared_libraries)
 
-        #2 If so- parse the requirements file and prepare
 
-        #3 get the artifacts list
+        # 3 get the artifacts list
         artifacts = self.model.artifacts
 
         for artifact in artifacts:
-            output_file = output_dir / artifact.name
+            output_file = output_dir / artifact.src.name
 
             if artifact.is_file():
                 SecurePath(artifact).copy(output_file)
@@ -86,16 +119,16 @@ class SnowparkEntity(EntityBase[Generic[T]]):
 
         return output_files
 
+        pass
 
-        pass
     def get_deploy_sql(self):
-        pass
+        raise NotImplementedError
 
     def get_describe_sql(self):
-        return f"DESCRIBE {self.identifier}"
+        raise NotImplementedError
 
     def get_drop_sql(self):
-        return f"DROP {self.identifier}"
+        raise NotImplementedError
 
     def get_execute_sql(self):
         raise NotImplementedError
@@ -103,29 +136,105 @@ class SnowparkEntity(EntityBase[Generic[T]]):
     def get_usage_grant_sql(self):
         pass
 
-    def _process_requirements(self, requirements_file: Optional[Path]):
-        with SecurePath.temporary_directory as tmp_dir:
+    def _process_requirements( #TODO: maybe leave all the logic with requirements here - so download, write requirements file etc.
+        self,
+        bundle_dir: Path,
+        archive_name: str,  # TODO: not the best name, think of something else
+        requirements_file: Optional[SecurePath],
+        ignore_anaconda: bool,
+        skip_version_check: bool = False,
+        index_url: Optional[str] = None,
+        allow_shared_libraries: bool = False,
+    ) -> DownloadUnavailablePackagesResult:
+        '''
+        Processes the requirements file and downloads the dependencies
+        Parameters:
+
+        '''
+        anaconda_packages_manager = AnacondaPackagesManager()
+        with SecurePath.temporary_directory() as tmp_dir:
             requirements = package_utils.parse_requirements(requirements_file)
-            anaconda_packages
+            anaconda_packages = (
+                AnacondaPackages.empty()
+                if ignore_anaconda
+                else anaconda_packages_manager.find_packages_available_in_snowflake_anaconda()
+            )
+            download_result = package_utils.download_unavailable_packages(
+                requirements=requirements,
+                target_dir=tmp_dir,
+                anaconda_packages=anaconda_packages,
+                skip_version_check=skip_version_check,
+                pip_index_url=index_url,
+            )
+
+            if download_result.anaconda_packages:
+                anaconda_packages.write_requirements_file_in_snowflake_format(
+                    file_path=SecurePath(bundle_dir / "requirements.txt"),
+                    requirements=download_result.anaconda_packages,
+                )
+
+            if download_result.downloaded_packages_details:
+                if (
+                    package_utils.detect_and_log_shared_libraries(
+                        download_result.downloaded_packages_details
+                    )
+                    and not allow_shared_libraries
+                ):
+                    raise ClickException(
+                        "Some packages contain shared (.so/.dll) libraries. "
+                        "Try again with allow_shared_libraries_flag."
+                    )
+
+                zip_dir(
+                    source=tmp_dir,
+                    dest_zip=bundle_dir / archive_name,
+                )
+
+        return download_result
+
 
 
 class FunctionEntity(SnowparkEntity[FunctionEntityModel]):
     """
     A single UDF
     """
+    def get_describe_sql(self):
+        return f"DESCRIBE FUNCTION {self.identifier}"
+
+    def get_drop_sql(self):
+        return f"DROP FUNCTION {self.identifier}"
+
+    def get_deploy_sql(self):
+        pass
 
     def get_execute_sql(self, execution_arguments: List[str] = None, *args, **kwargs):
         if not execution_arguments:
             execution_arguments = []
-        return f"SELECT {self.fqn}({', '.join([str(arg) for arg in execution_arguments])})"
+        return (
+            f"SELECT {self.fqn}({', '.join([str(arg) for arg in execution_arguments])})"
+        )
+
 
 
 class ProcedureEntity(SnowparkEntity[ProcedureEntityModel]):
     """
     A stored procedure
     """
+    def get_describe_sql(self):
+        return f"DESCRIBE PROCEDURE {self.identifier}"
 
-    def get_execute_sql(self, execution_arguments: List[str] = None, ):
+    def get_drop_sql(self):
+        return f"DROP PROCEDURE {self.identifier}"
+
+    def get_deploy_sql(self):
+        pass
+
+    def get_execute_sql(
+        self,
+        execution_arguments: List[str] = None,
+    ):
         if not execution_arguments:
             execution_arguments = []
-        return f"CALL {self.fqn}({', '.join([str(arg) for arg in execution_arguments])})"
+        return (
+            f"CALL {self.fqn}({', '.join([str(arg) for arg in execution_arguments])})"
+        )
--- tests/snowpark/test_snowpark_entity.py
+++ tests/snowpark/test_snowpark_entity.py
@@ -3,20 +3,22 @@ from unittest import mock
 
 import pytest
 import yaml
-from markdown_it.rules_inline import entity
 
 from snowflake.cli._plugins.snowpark.snowpark_entity import FunctionEntity
 from snowflake.cli._plugins.snowpark.snowpark_entity_model import FunctionEntityModel
-from snowflake.cli._plugins.workspace.context import WorkspaceContext, ActionContext
+from snowflake.cli._plugins.workspace.context import ActionContext, WorkspaceContext
+from snowflake.cli._plugins.snowpark.package.anaconda_packages import AnacondaPackages, AvailablePackage
 from testing_utils.mock_config import mock_config_key
 
 CONNECTOR = "snowflake.connector.connect"
 CONTEXT = ""
 EXECUTE_QUERY = "snowflake.cli.api.sql_execution.BaseSqlExecutor.execute_query"
-
+ANACONDA_PACKAGES = "snowflake.cli._plugins.snowpark.package.anaconda_packages.AnacondaPackagesManager.find_packages_available_in_snowflake_anaconda"
 
 @pytest.fixture
-def example_function_workspace(project_directory): #TODO: try to make a common fixture for all entities
+def example_function_workspace(
+    project_directory,
+):  # TODO: try to make a common fixture for all entities
     with mock_config_key("enable_native_app_children", True):
         with project_directory("snowpark_functions_v2") as pdir:
             with Path(pdir / "snowflake.yml").open() as definition_file:
@@ -33,9 +35,7 @@ def example_function_workspace(project_directory): #TODO: try to make a common f
                 )
 
                 return (
-                    FunctionEntity(
-                        workspace_ctx=workspace_context, entity_model=model
-                    ),
+                    FunctionEntity(workspace_ctx=workspace_context, entity_model=model),
                     ActionContext(
                         get_entity=lambda *args: None,
                     ),
@@ -47,32 +47,58 @@ def test_action_describe(mock_execute, example_function_workspace):
     entity, action_context = example_function_workspace
     result = entity.action_describe(action_context)
 
-    mock_execute.assert_called_with("DESCRIBE IDENTIFIER('func1')")
+    mock_execute.assert_called_with("DESCRIBE FUNCTION IDENTIFIER('func1')")
+
 
 @mock.patch(EXECUTE_QUERY)
 def test_action_drop(mock_execute, example_function_workspace):
     entity, action_context = example_function_workspace
     result = entity.action_drop(action_context)
 
-    mock_execute.assert_called_with("DROP IDENTIFIER('func1')")
+    mock_execute.assert_called_with("DROP FUNCTION IDENTIFIER('func1')")
 
-@pytest.mark.parametrize("execution_arguments", [None, ["arg1", "arg2"], ["foo",42,"bar"]])
+
+@pytest.mark.parametrize(
+    "execution_arguments", [None, ["arg1", "arg2"], ["foo", 42, "bar"]]
+)
 @mock.patch(EXECUTE_QUERY)
-def test_action_execute(mock_execute, execution_arguments, example_function_workspace, snapshot):
+def test_action_execute(
+    mock_execute, execution_arguments, example_function_workspace, snapshot
+):
     entity, action_context = example_function_workspace
     result = entity.action_execute(action_context, execution_arguments)
 
     mock_execute.assert_called_with(snapshot)
 
-def test_describe_sql(example_function_workspace):
-    entity, _ = example_function_workspace
-    assert entity.get_describe_sql() == "DESCRIBE IDENTIFIER('func1')"
+@mock.patch(ANACONDA_PACKAGES)
+def test_bundle(mock_anaconda, example_function_workspace):
+    mock_anaconda.return_value = AnacondaPackages({
+        "pandas": AvailablePackage("pandas",  "1.2.3"),
+        "numpy": AvailablePackage("numpy",  "1.2.3"),
+        "snowflake_snowpark_python": AvailablePackage("snowflake_snowpark_python", "1.2.3")
+    })
+    entity, action_context = example_function_workspace
+    entity.action_bundle(action_context, None, False, False, None, False)
 
-def test_drop_sql(example_function_workspace):
-    entity, _ = example_function_workspace
-    assert entity.get_drop_sql() == "DROP IDENTIFIER('func1')"
+    output = entity.root / "output" / entity._entity_model.stage  # noqa
+    assert output.exists()
+    assert (output / "app.py").exists()
 
-@pytest.mark.parametrize("execution_arguments", [None, ["arg1", "arg2"], ["foo",42,"bar"]])
-def test_function_get_execute_sql(execution_arguments, example_function_workspace, snapshot):
+def test_describe_function_sql(example_function_workspace):
+    entity, _ = example_function_workspace
+    assert entity.get_describe_sql() == "DESCRIBE FUNCTION IDENTIFIER('func1')"
+
+
+def test_drop_function_sql(example_function_workspace):
+    entity, _ = example_function_workspace
+    assert entity.get_drop_sql() == "DROP FUNCTION IDENTIFIER('func1')"
+
+
+@pytest.mark.parametrize(
+    "execution_arguments", [None, ["arg1", "arg2"], ["foo", 42, "bar"]]
+)
+def test_function_get_execute_sql(
+    execution_arguments, example_function_workspace, snapshot
+):
     entity, _ = example_function_workspace
     assert entity.get_execute_sql(execution_arguments) == snapshot
