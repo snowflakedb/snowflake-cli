5541742bf3cd968b2cf22696859223a7d284f9e1
Marcin Raba <marcin.raba@snowflake.com>
2025-01-21 16:28:58 +0100

    SNOW-1793933 build binaries attempt 3

--- scripts/packaging/setup_darwin_x86.sh
+++ scripts/packaging/setup_darwin_x86.sh
@@ -4,11 +4,16 @@ set -o pipefail
 echo "Setting up the Snowflake CLI build environment"
 
 brew install pyenv
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+
 pyenv versions
-pyenv install 3.11
+pyenv install -s 3.11
 pyenv shell 3.11
 
 python -m venv venv
 . venv/bin/activate
 pip install -U pip hatch uv pyinstaller
-hatch run packaging:build-binaries
+# hatch run packaging:build-binaries
