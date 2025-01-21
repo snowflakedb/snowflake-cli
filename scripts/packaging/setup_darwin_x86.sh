d292fdf5a77afb1dc438e67ad478ec4e69f9f0f4
Marcin Raba <marcin.raba@snowflake.com>
2025-01-21 11:28:49 +0100

    SNOW-1793933 build binaries attempt 1

--- scripts/packaging/setup_darwin_x86.sh
+++ scripts/packaging/setup_darwin_x86.sh
@@ -2,3 +2,12 @@
 set -o pipefail
 
 echo "Setting up the Snowflake CLI build environment"
+
+pyenv versions
+pyenv install 3.10
+pyenv shell 3.10
+
+python -m venv venv
+. venv/bin/activate
+pip install -U pip hatch uv pyinstaller
+hatch run packaging:build-binaries
