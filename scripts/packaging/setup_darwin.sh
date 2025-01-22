fdaa5797179a7ae6ed3e5f38e7ce71f68b53e5a1
Marcin Raba <marcin.raba@snowflake.com>
2025-01-22 15:39:26 +0100

    SNOW-1793933 darwin build script tuning 17

--- scripts/packaging/setup_darwin.sh
+++ scripts/packaging/setup_darwin.sh
@@ -3,27 +3,40 @@ set -o pipefail
 
 echo "Setting up the Snowflake CLI build environment"
 
-function ensure_pyenv_installation() {
+ensure_pyenv_installation() {
   if ! command -v pyenv &>/dev/null; then
     echo "pyenv not found, installing..."
     arch -$(uname -m) brew install pyenv
   else
     echo "pyenv already installed"
   fi
+}
 
+activate_pyenv() {
   export PYENV_ROOT="$HOME/.pyenv"
   [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init - bash)"
 }
 
-function ensure_python_installation() {
+ensure_hatch_installation() {
+  if ! command -v hatch &>/dev/null; then
+    echo "hatch not found, installing..."
+    arch -$(uname -m) install hatch
+  else
+    echo "hatch already installed"
+    arch -$(uname -m) brew upgrade hatch
+  fi
+}
+
+ensure_python_installation() {
   pyenv versions
   pyenv install -s 3.10
   pyenv install -s 3.11
   pyenv global 3.11
   python --version
 }
-# python -m venv venv
-# . venv/bin/activate
-# pip install -U pip hatch uv pyinstaller
-# hatch run packaging:package-darwin-binaries
+
+# ensure_pyenv_installation
+# activate_pyenv
+# ensure_python_installation
+# ensure_hatch_installation
