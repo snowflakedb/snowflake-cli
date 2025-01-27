fd4a6ba6ddf59274360995086f61959df8f11ae3
Patryk Czajka <patryk.czajka@snowflake.com>
2025-01-27 15:03:59 +0100

    integration tests

--- tests_integration/test_notebooks.py
+++ tests_integration/test_notebooks.py
@@ -1,40 +1,39 @@
 import pytest
 
 
-@pytest.mark.parametrize("notebook_id", ["notebook1", "notebook2"])
 @pytest.mark.integration
-def test_deploy_by_id(runner, project_directory, test_database, notebook_id):
-    expected_output_prefix = (
-        f"Deploying notebook {notebook_id}\n"
-        f"  Creating stage notebooks if not exists\n"
-        f"  Uploading {notebook_id}/my_notebook.ipynb to @notebooks/{notebook_id}\n"
-        f"  Creating notebook\n"
-    )
+def test_deploy_by_id(runner, project_directory, test_database):
+    def expected_output_prefix(notebook_id):
+        return (
+            f"Deploying notebook {notebook_id}\n"
+            f"  Creating stage notebooks if not exists\n"
+            f"  Uploading {notebook_id}/my_notebook.ipynb to @notebooks/{notebook_id}\n"
+            f"  Creating notebook\n"
+        )
 
     with project_directory("notebooks_multiple_v2"):
-        result = runner.invoke_with_connection(["notebook", "deploy", notebook_id])
-        assert result.exit_code == 0, result.output
-        assert result.output.startswith(expected_output_prefix)
+        for notebook_id in ["notebook1", "notebook2"]:
+            result = runner.invoke_with_connection(["notebook", "deploy", notebook_id])
+            assert result.exit_code == 0, result.output
+            assert result.output.startswith(expected_output_prefix(notebook_id))
 
-        result = runner.invoke_with_connection(["notebook", "deploy", notebook_id])
-        assert result.exit_code == 1, result.output
-        assert (
-            f"Notebook {notebook_id} already exists. Consider using --replace."
-            in result.output
-        )
-
-        result = runner.invoke_with_connection(
-            ["notebook", "deploy", notebook_id, "--replace"]
+        # default stage file paths should be different even though filenames are equal
+        result = runner.invoke_with_connection_json(
+            ["stage", "list-files", "notebooks"]
         )
         assert result.exit_code == 0, result.output
-        assert result.output.startswith(expected_output_prefix)
+        file_paths = set(data["name"] for data in result.json)
+        assert file_paths == {
+            "notebooks/notebook1/my_notebook.ipynb",
+            "notebooks/notebook2/my_notebook.ipynb",
+        }
 
 
 @pytest.mark.integration
 def test_deploy_single_notebook(runner, project_directory, test_database):
     expected_output_prefix = (
-        "Deploying notebook my_notebook\n"
-        "  Creating stage notebooks if not exists\n"
+        "Deploying notebook custom_identifier\n"
+        "  Creating stage custom_stage if not exists\n"
         "  Uploading notebook.ipynb to @custom_stage/particular_notebook_path\n"
         "  Creating notebook\n"
     )
@@ -44,10 +43,19 @@ def test_deploy_single_notebook(runner, project_directory, test_database):
         assert result.exit_code == 0, result.output
         assert result.output.startswith(expected_output_prefix)
 
+        result = runner.invoke_with_connection_json(
+            ["stage", "list-files", "custom_stage"]
+        )
+        assert result.exit_code == 0, result.output
+        assert (
+            result.json[0]["name"]
+            == "custom_stage/particular_notebook_path/notebook.ipynb"
+        )
+
         result = runner.invoke_with_connection(["notebook", "deploy"])
         assert result.exit_code == 1, result.output
         assert (
-            "Notebook my_notebook already exists. Consider using --replace."
+            "Notebook custom_identifier already exists. Consider using --replace."
             in result.output
         )
 
