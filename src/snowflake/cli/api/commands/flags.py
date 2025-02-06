847f85b1d982e48fd9cc19f07568d904ea7ac780
Patryk Czajka <patryk.czajka@snowflake.com>
2025-02-06 14:41:28 +0100

    Flags spelling fixes

--- src/snowflake/cli/api/commands/flags.py
+++ src/snowflake/cli/api/commands/flags.py
@@ -538,8 +538,8 @@ def project_definition_option(is_optional: bool):
         None,
         "-p",
         "--project",
-        help=f"Path where Snowflake project resides. "
-        f"Defaults to current working directory.",
+        help=f"Path where the Snowflake project resides. "
+        f"Defaults to the current working directory.",
         callback=project_path_callback,
         show_default=False,
     )
