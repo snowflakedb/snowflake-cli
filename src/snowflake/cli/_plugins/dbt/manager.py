eb9f6745105393b10208a86bcc6ef424a7bc7b7c
Jakub Wilkowski <jakub.wilkowski@snowflake.com>
2025-02-13 18:18:20 +0100

    feat: [SNOW-1890085] add dbt command group; add dbt list command

---
+++ src/snowflake/cli/_plugins/dbt/manager.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2025 Snowflake Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from snowflake.cli.api.sql_execution import SqlExecutionMixin
+from snowflake.connector.cursor import SnowflakeCursor
+
+
+class DBTManager(SqlExecutionMixin):
+    def list(self) -> SnowflakeCursor:  # noqa: A003
+        query = f"show dbt"
+        return self.execute_query(query)
