872965f9e7bc58713c1bece0c9b04fc8fec129af
Jakub Wilkowski <jakub.wilkowski@snowflake.com>
2025-02-14 13:18:44 +0100

    feat: [SNOW-1890085] add dbt execute command

--- src/snowflake/cli/_plugins/dbt/commands.py
+++ src/snowflake/cli/_plugins/dbt/commands.py
@@ -16,6 +16,7 @@ from __future__ import annotations
 
 import logging
 
+import typer
 from snowflake.cli._plugins.dbt.manager import DBTManager
 from snowflake.cli.api.commands.snow_typer import SnowTyperFactory
 from snowflake.cli.api.output.types import CommandResult, QueryResult
@@ -39,3 +40,28 @@ def list_dbts(
     List all dbt projects on Snowflake.
     """
     return QueryResult(DBTManager().list())
+
+
+@app.command(
+    "execute",
+    requires_connection=True,
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True},
+)
+def execute(
+    ctx: typer.Context,
+    dbt_command: str = typer.Argument(
+        help="dbt command to execute, i. e. run, compile, seed...",
+    ),
+    name: str = typer.Option(
+        default=...,
+        help="Name of the dbt object to execute command on.",
+    ),
+    **options,
+) -> CommandResult:
+    """
+    Execute command on dbt in Snowflake project.
+    """
+    # ctx.args are parameters that were not captured as known cli params (those are in **options).
+    # as a consequence, we don't support passing params known to snowflake cli further to dbt
+    dbt_cli_args = ctx.args
+    return QueryResult(DBTManager().execute(dbt_command, name, *dbt_cli_args))
