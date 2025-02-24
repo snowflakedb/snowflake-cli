f109fe4fba043f233319fa22f6249bb93cec5c24
Patryk Czajka <patryk.czajka@snowflake.com>
2025-02-24 15:04:50 +0100

    fix windows

--- scripts/packaging/win/build_snowflake_cli.bat
+++ scripts/packaging/win/build_snowflake_cli.bat
@@ -12,18 +12,19 @@ del rustup-init.exe
 set PATH=%PATH%;%USERPROFILE%\.cargo\bin\
 
 @echo off
-FOR /F "delims=" %%I IN ('hatch run packaging:win-build-version') DO SET CLI_VERSION=%%I
+FOR /F "delims=" %%I IN ('hatch run packaging:win-build-version') DO SET CLI_VERSION_WIN=%%I
+echo %CLI_VERSION_WIN%
+FOR /F "delims=" %%I IN ('hatch version') DO SET CLI_VERSION=%%I
 echo %CLI_VERSION%
 
-set ENTRYPOINT=src\\snowflake\\cli\\_app\\__main__.py
-
 RMDIR /S /Q dist
-RMDIR /S /Q build
 DEL /Q *.wixobj
 
 @echo on
 python.exe -m hatch -e packaging run build-isolated-binary
-dir dist
 dir dist\binary
+mkdir dist\snow
+move dist\binary\snow-%CLI_VERSION%.exe dist\snow\snow.exe
+.\dist\snow\snow.exe --help
 
-tar -a -c -f snowflake-cli-%CLI_VERSION%.zip dist\snow
+tar -a -c -f snowflake-cli-%CLI_VERSION_WIN%.zip dist\snow
