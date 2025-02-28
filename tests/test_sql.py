7c27e07a2ad35198afe470f4da91fcc7f9e065ad
Patryk Czajka <patryk.czajka@snowflake.com>
2025-02-28 14:17:27 +0100

    Improve diagnostic report help messages (#2088)
    * fix diagnostic report behavior
    
    * revert behavioral change
    
    * revert cd
    
    * update snapshots
    
    * fix unit test

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -154,7 +154,7 @@ def test_sql_overrides_connection_configuration(mock_conn, runner, mock_cursor):
     mock_conn.assert_called_once_with(
         temporary_connection=False,
         enable_diag=False,
-        diag_log_path=Path("/tmp"),
+        diag_log_path=Path("/tmp").absolute(),
         connection_name="connectionName",
         account="accountnameValue",
         user="usernameValue",
