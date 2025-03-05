e94eda84d143535c1cfbfd9072ed84a298f21bce
Patryk Czajka <patryk.czajka@snowflake.com>
2025-03-05 10:47:14 +0100

    Revert "Temporarily disable schema existence check for object create (#1949)"
    This reverts commit 9305955fb0783fcdf1d49cd7f4ca3f9b0050d00e.

--- src/snowflake/cli/api/rest_api.py
+++ src/snowflake/cli/api/rest_api.py
@@ -155,9 +155,8 @@ class RestApi:
             raise SchemaNotDefinedException(
                 "Schema not defined in connection. Please try again with `--schema` flag."
             )
-        # temporarily disable this check due to an issue on server side: SNOW-1747450
-        # if not self._schema_exists(db_name=db, schema_name=schema):
-        #     raise SchemaNotExistsException(f"Schema '{schema}' does not exist.")
+        if not self._schema_exists(db_name=db, schema_name=schema):
+            raise SchemaNotExistsException(f"Schema '{schema}' does not exist.")
         if self.get_endpoint_exists(
             url := f"{SF_REST_API_URL_PREFIX}/databases/{self.conn.database}/schemas/{self.conn.schema}/{plural_object_type}/"
         ):
