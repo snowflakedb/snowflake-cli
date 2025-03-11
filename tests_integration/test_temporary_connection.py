a07baa96a55d1bc6492fbcc259afa661e86c30e3
Adam Stus <adam.stus@snowflake.com>
2025-03-11 09:37:24 +0100

    Fixed invalid private key on Windows

--- tests_integration/test_temporary_connection.py
+++ tests_integration/test_temporary_connection.py
@@ -11,25 +11,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import tempfile
-import pytest
 import os
-from unittest import mock
+
+import pytest
+
+from snowflake.cli.api.secure_path import SecurePath
 
 
 @pytest.mark.integration
-def test_temporary_connection(runner, _temporary_connection_env, snapshot):
-    with tempfile.TemporaryDirectory() as tmp_dir:
+def test_temporary_connection(runner, snapshot):
+    with SecurePath.temporary_directory() as tmp_path:
         if os.environ.get("SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_FILE", None):
             private_key_path = os.environ.get(
                 "SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_FILE"
             )
         else:
-            private_key_path = str(os.path.join(tmp_dir, "private_key.p8"))
-            with open(private_key_path, "w") as f:
+            private_key_secure_path = tmp_path / "private_key.p8"
+            with private_key_secure_path.open(mode="w") as f:
                 f.write(
                     os.environ.get("SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_RAW")
                 )
+            private_key_path = private_key_secure_path.path
         result = runner.invoke(
             [
                 "sql",
@@ -48,40 +50,5 @@ def test_temporary_connection(runner, _temporary_connection_env, snapshot):
                 private_key_path,
             ]
         )
-        assert result.exit_code == 0
+        assert result.exit_code == 0, result.output
         assert result.output == snapshot
-
-
-@pytest.fixture
-def _temporary_connection_env():
-    private_key_file = os.environ.get(
-        "SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_FILE",
-        os.environ.get("SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_PATH", None),
-    )
-    private_key_raw = os.environ.get(
-        "SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_RAW", None
-    )
-
-    if not private_key_file and not private_key_raw:
-        pytest.fail(
-            "One of SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_FILE or SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_RAW must be set"
-        )
-
-    env = {
-        "SNOWFLAKE_CONNECTIONS_INTEGRATION_ACCOUNT": os.environ[
-            "SNOWFLAKE_CONNECTIONS_INTEGRATION_ACCOUNT"
-        ],
-        "SNOWFLAKE_CONNECTIONS_INTEGRATION_USER": os.environ[
-            "SNOWFLAKE_CONNECTIONS_INTEGRATION_USER"
-        ],
-        "SNOWFLAKE_CONNECTIONS_INTEGRATION_HOST": os.environ[
-            "SNOWFLAKE_CONNECTIONS_INTEGRATION_HOST"
-        ],
-    }
-    if private_key_file:
-        env["SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_FILE"] = private_key_file
-    if private_key_raw:
-        env["SNOWFLAKE_CONNECTIONS_INTEGRATION_PRIVATE_KEY_RAW"] = private_key_raw
-
-    with mock.patch.dict(os.environ, env, clear=True):
-        yield env
