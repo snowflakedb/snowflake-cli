41c1893f6e13aa86764b19568d16e34fffe538bc
Marcin Raba <marcin.raba@snowflake.com>
2025-03-13 12:12:09 +0100

    SNOW-1282073: repl poc 2

---
+++ tests/test_repl.py
@@ -0,0 +1,10 @@
+from unittest import mock
+
+
+@mock.patch("snowflake.cli._plugins.sql.manager.SqlExecutionMixin._execute_string")
+def test_repl_input_handling(mock_execute, runner, mock_cursor, snapshot):
+    mock_execute.return_value = (mock_cursor(["row"], []) for _ in range(1))
+    query = "select 1;"
+    result = runner.invoke(["sql"], input=query)
+    assert result.exit_code == 0
+    snapshot.assert_match(result.output)
