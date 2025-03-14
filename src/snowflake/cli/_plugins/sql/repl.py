dfa53ffb6d8fb4f24d07cd6d58788dbd082f2b46
Marcin Raba <marcin.raba@snowflake.com>
2025-03-14 12:27:38 +0100

    SNOW-1282073: repl poc - syntax highlighting
    multiline breaks history

--- src/snowflake/cli/_plugins/sql/repl.py
+++ src/snowflake/cli/_plugins/sql/repl.py
@@ -2,6 +2,8 @@ from prompt_toolkit import PromptSession
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.key_binding.key_bindings import KeyBindings
+from prompt_toolkit.lexers import PygmentsLexer
+from pygments.lexers.sql import SqlLexer
 from rich.console import Console
 from rich.panel import Panel
 from rich.prompt import Prompt
@@ -25,8 +27,11 @@ key_bindings = KeyBindings()
 @key_bindings.add("enter")
 def _(event):
     buffer = event.app.current_buffer
-    if buffer.text and not buffer.text.endswith("\n"):
-        buffer.insert_text("\n")
+    if buffer.text:
+        if not buffer.text.endswith("\n"):
+            buffer.insert_text("\n")
+        if buffer.text.strip().endswith(";"):
+            event.app.exit(result=buffer.text.strip())
     else:
         event.app.exit(result=buffer.text)
 
@@ -48,19 +53,6 @@ class Repl:
         self.data = data or {}
         self.retain_comments = retain_comments
 
-    def run(self):
-        try:
-            self.prompt_session = PromptSession(
-                history=FileHistory(HISTORY_FILE),
-                completer=sql_completer,
-                multiline=True,
-                wrap_lines=True,
-                key_bindings=key_bindings,
-            )
-            self._repl_loop()
-        except (KeyboardInterrupt, EOFError):
-            console.print("\n[bold orange]Leaving REPL bye ...")
-
     @property
     def _welcome_banner(self) -> Panel:
         return Panel(
@@ -68,27 +60,31 @@ class Repl:
             style="bold",
         )
 
-    def _repl_loop(self):
-        console.print(self._welcome_banner)
+    def run(self):
+        try:
+            self.prompt_session = PromptSession(
+                history=FileHistory(HISTORY_FILE),
+                lexer=PygmentsLexer(SqlLexer),
+                completer=sql_completer,
+                multiline=True,
+                wrap_lines=True,
+                key_bindings=key_bindings,
+            )
+            console.print(self._welcome_banner)
+            self._repl_loop()
+        except (KeyboardInterrupt, EOFError):
+            console.print("\n[bold orange]Leaving REPL bye ...")
 
+    def _repl_loop(self):
         while True:
             try:
                 user_input = self.prompt_session.prompt(" > ").strip()
 
+                if not user_input.strip():
+                    continue
+
                 if user_input.lower() in EXIT_KEYWORDS:
                     break
-
-                # highlighted = Syntax(user_input, "sql", line_numbers=False)
-                # console.print(highlighted)
-                _, cursors = self.sql_manager.execute(
-                    query=user_input,
-                    files=None,
-                    std_in=False,
-                    data=self.data,
-                    retain_comments=self.retain_comments,
-                )
-
-                print_result(MultipleResults(QueryResult(c) for c in cursors))
 
             except KeyboardInterrupt:  # a.k.a Ctrl-C
                 continue
@@ -99,3 +95,13 @@ class Repl:
 
             except Exception as e:
                 console.print(f"[bold orange] Error occurred: {e}")
+
+            else:
+                _, cursors = self.sql_manager.execute(
+                    query=user_input,
+                    files=None,
+                    std_in=False,
+                    data=self.data,
+                    retain_comments=self.retain_comments,
+                )
+                print_result(MultipleResults(QueryResult(c) for c in cursors))
