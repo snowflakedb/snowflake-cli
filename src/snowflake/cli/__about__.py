cdcae908b0e42c0fb1fdcb855b648cfc255da011
Patryk Czajka <patryk.czajka@snowflake.com>
2025-03-24 17:38:43 +0100

    Snow 1999047 fix binaries visible as pypi in telemetry (#2141)

--- src/snowflake/cli/__about__.py
+++ src/snowflake/cli/__about__.py
@@ -14,4 +14,16 @@
 
 from __future__ import annotations
 
+from enum import Enum, unique
+
 VERSION = "3.6.0rc1"
+
+
+@unique
+class CLIInstallationSource(Enum):
+    BINARY = "binary"
+    PYPI = "pypi"
+
+
+# This variable is changed in binary release script
+INSTALLATION_SOURCE = CLIInstallationSource.PYPI
