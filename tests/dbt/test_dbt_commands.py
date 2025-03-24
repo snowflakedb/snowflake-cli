8564cb7ec2daf88328e436d4d488806733cdca29
Jakub Wilkowski <jakub.wilkowski@snowflake.com>
2025-03-24 09:15:03 +0100

    feat: [SNOW-1890085] remove options for dbt_version and dbt_adapter_version

--- tests/dbt/test_dbt_commands.py
+++ tests/dbt/test_dbt_commands.py
@@ -17,17 +17,7 @@ from __future__ import annotations
 from unittest import mock
 
 import pytest
-import yaml
 from snowflake.cli.api.identifiers import FQN
-
-
-@pytest.fixture
-def mock_connect(mock_ctx):
-    with mock.patch("snowflake.connector.connect") as _fixture:
-        ctx = mock_ctx()
-        _fixture.return_value = ctx
-        _fixture.mocked_ctx = _fixture.return_value
-        yield _fixture
 
 
 class TestDBTList:
@@ -67,8 +57,6 @@ class TestDBTDeploy:
         source_path = tmp_path_factory.mktemp("dbt_project")
         dbt_file = source_path / "dbt_project.yml"
         dbt_file.touch()
-        with dbt_file.open(mode="w") as fd:
-            yaml.dump({"version": "1.2.3"}, fd)
         yield source_path
 
     @pytest.fixture
@@ -101,7 +89,6 @@ class TestDBTDeploy:
                 "deploy",
                 "TEST_PIPELINE",
                 f"--source={dbt_project_path}",
-                "--dbt-adapter-version=3.4.5",
             ]
         )
 
@@ -109,9 +96,7 @@ class TestDBTDeploy:
         assert (
             mock_connect.mocked_ctx.get_query()
             == """CREATE DBT PROJECT TEST_PIPELINE
-FROM @MockDatabase.MockSchema.dbt_TEST_PIPELINE_stage
-DBT_VERSION='1.2.3'
-DBT_ADAPTER_VERSION='3.4.5'"""
+FROM @MockDatabase.MockSchema.dbt_TEST_PIPELINE_stage"""
         )
         stage_fqn = FQN.from_string(f"dbt_TEST_PIPELINE_stage").using_context()
         mock_create.assert_called_once_with(stage_fqn, temporary=True)
@@ -136,8 +121,6 @@ DBT_ADAPTER_VERSION='3.4.5'"""
                 "deploy",
                 "TEST_PIPELINE",
                 f"--source={dbt_project_path}",
-                "--dbt-version=2.3.4",
-                "--dbt-adapter-version=3.4.5",
             ]
         )
 
@@ -145,9 +128,7 @@ DBT_ADAPTER_VERSION='3.4.5'"""
         assert (
             mock_connect.mocked_ctx.get_query()
             == """CREATE DBT PROJECT TEST_PIPELINE
-FROM @MockDatabase.MockSchema.dbt_TEST_PIPELINE_stage
-DBT_VERSION='2.3.4'
-DBT_ADAPTER_VERSION='3.4.5'"""
+FROM @MockDatabase.MockSchema.dbt_TEST_PIPELINE_stage"""
         )
 
     @pytest.mark.parametrize("exists", (True, False))
@@ -172,7 +153,6 @@ DBT_ADAPTER_VERSION='3.4.5'"""
                 "TEST_PIPELINE",
                 f"--source={dbt_project_path}",
                 "--force",
-                "--dbt-adapter-version=3.4.5",
             ]
         )
 
@@ -193,36 +173,11 @@ DBT_ADAPTER_VERSION='3.4.5'"""
                 "deploy",
                 "TEST_PIPELINE",
                 f"--source={dbt_project_path}",
-                "--dbt-adapter-version=3.4.5",
             ],
         )
 
         assert result.exit_code == 1, result.output
         assert "dbt_project.yml does not exist in provided path." in result.output
-        assert mock_connect.mocked_ctx.get_query() == ""
-
-    def test_raises_when_dbt_project_version_is_not_specified(
-        self, dbt_project_path, mock_connect, runner
-    ):
-        dbt_file = dbt_project_path / "dbt_project.yml"
-        with dbt_file.open(mode="w") as fd:
-            yaml.dump({}, fd)
-
-        result = runner.invoke(
-            [
-                "dbt",
-                "deploy",
-                "TEST_PIPELINE",
-                f"--source={dbt_project_path}",
-                "--dbt-adapter-version=3.4.5",
-            ]
-        )
-
-        assert result.exit_code == 1, result.output
-        assert (
-            "dbt-version was not provided and is not available in dbt_project.yml"
-            in result.output
-        )
         assert mock_connect.mocked_ctx.get_query() == ""
 
     def test_raises_when_dbt_project_exists_and_is_not_force(
