a3b4479c4f715cb471fd39a5a47fdce75bac94b1
Marcin Raba <marcin.raba@snowflake.com>
2025-03-26 13:48:10 +0100

    SNOW-1282073: repl poc - win32_dummy_output

---
+++ conftest.py
@@ -0,0 +1,14 @@
+import sys
+
+import pytest
+from prompt_toolkit.output import DummyOutput
+
+
+@pytest.fixture(name="win32_dummy_console", autouse=True)
+def make_win32_dummy_console():
+    if sys.platform == "win32":
+        to_patch = "prompt_toolkit.output.deautls.create_output"
+        with mock.patch(to_patch, return_value=DummyOutput()):
+            yield
+    else:
+        yield
