f4e2713cb525e1ab8bb4a8f35fe930192f52b068
Marcin Raba <marcin.raba@snowflake.com>
2025-03-26 14:12:35 +0100

    SNOW-1282073: repl poc - win32_dummy_console typo fix

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from prompt_toolkit.output import DummyOutput
 @pytest.fixture(name="win32_dummy_console", autouse=True)
 def make_win32_dummy_console():
     if sys.platform == "win32":
-        to_patch = "prompt_toolkit.output.deautls.create_output"
+        to_patch = "prompt_toolkit.output.defaults.create_output"
         with mock.patch(to_patch, return_value=DummyOutput()):
             yield
     else:
