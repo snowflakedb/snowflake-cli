b71d59dc00c23c8df6ed05df93ae428bf1ce42b2
Jan Sikorski <jan.sikorski@snowflake.com>
2025-04-10 15:27:43 +0200

    Tests debug

--- tests/test_snow_connector.py
+++ tests/test_snow_connector.py
@@ -17,7 +17,7 @@ from unittest import mock
 
 import pytest
 from snowflake.cli.api.secret import SecretType
-
+from pathlib import Path
 
 # Used as a solution to syrupy having some problems with comparing multilines string
 class CustomStr(str):
@@ -152,7 +152,8 @@ def test_private_key_loading_and_aliases(
 
 @mock.patch.dict(os.environ, {}, clear=True)
 def test_returns_nice_error_in_case_of_connectivity_error(runner):
-    result = runner.invoke(["sql", "-q", "select 1", "--config-file", "non-existent.toml"])
+    Path("non-existent.toml").write_text("")
+    result = runner.invoke(["--config-file", "non-existent.toml","sql", "-q", "select 1"])
 
     assert result.exit_code == 1, result.output
     assert "Invalid connection configuration" in result.output
