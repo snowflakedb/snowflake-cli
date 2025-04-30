6426760557b178e01b8c7f62e1754185b4b3d8ff
Ashwin Ravi <ashwin.ravi@snowflake.com>
2025-04-30 10:19:00 -0700

    Remove default value
    Co-authored-by: Patryk Czajka <patryk.czajka@snowflake.com>

--- src/snowflake/cli/_plugins/cortex/commands.py
+++ src/snowflake/cli/_plugins/cortex/commands.py
@@ -142,7 +142,7 @@ def complete(
     backend: Optional[Backend] = typer.Option(
         DEFAULT_BACKEND,
         "--backend",
-        help="String specifying whether to use sql or rest backend. Default is rest.",
+        help="String specifying whether to use sql or rest backend.",
     ),
     file: Optional[Path] = ExclusiveReadableFileOption(
         help="JSON file containing conversation history to be used to generate a completion. Cannot be combined with TEXT argument.",
