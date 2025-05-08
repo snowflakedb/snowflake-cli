43ab27869fc7ae323a7d55bd9fab621f892c27e0
Adam Stus <adam.stus@snowflake.com>
2025-05-08 10:19:33 +0200

    Increased performance test threshold

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -18,7 +18,7 @@ from timeit import default_timer as timer
 import pytest
 
 SAMPLE_AMOUNT = 20
-EXECUTION_TIME_THRESHOLD = 3.1  # seconds
+EXECUTION_TIME_THRESHOLD = 3.3  # seconds
 
 
 @pytest.mark.performance
