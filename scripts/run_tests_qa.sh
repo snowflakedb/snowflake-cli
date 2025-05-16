e33c78f25cc6cca32229fe386a105b0bb4fb2fb0
Patryk Czajka <patryk.czajka@snowflake.com>
2025-05-16 15:29:25 +0200

    try update certificates

--- scripts/run_tests_qa.sh
+++ scripts/run_tests_qa.sh
@@ -3,9 +3,10 @@ source venv/bin/activate
 python -m pip install --upgrade pip hatch
 
 #openssl s_client -connect snowcli-it.qa6.us-west-2.aws.snowflakecomputing.com:443 -showcerts
-ls /etc/ssl
-ls /etc/ssl/certs
-export REQUESTS_CA_BUNDLE='/etc/ssl/certs/ca-bundle.trust.crt'
+#ls /etc/ssl
+#ls /etc/ssl/certs
+#export REQUESTS_CA_BUNDLE='/etc/ssl/certs/ca-bundle.trust.crt'
+update-ca-certificates
 
 echo "Test cleanup"
 python -m hatch env create integration
