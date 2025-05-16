46be1c480b54c7bcd3c3f30bb7246e1b45932504
Patryk Czajka <patryk.czajka@snowflake.com>
2025-05-16 16:34:22 +0200

    try generating certificate

--- scripts/run_tests_qa.sh
+++ scripts/run_tests_qa.sh
@@ -2,11 +2,13 @@ python3.10 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip hatch
 
+openssl req -x509 -newkey rsa:4096 -sha256 -days 1 \
+  -nodes -keyout tmp_cert.key -out tmp_cert.crt -subj "/CN=snowcli-it.qa6.us-west-2.aws.snowflakecomputing.com"
 #openssl s_client -connect snowcli-it.qa6.us-west-2.aws.snowflakecomputing.com:443 -showcerts
 #ls /etc/ssl
 #ls /etc/ssl/certs
-#export REQUESTS_CA_BUNDLE='/etc/ssl/certs/ca-bundle.trust.crt'
-update-ca-certificates
+export REQUESTS_CA_BUNDLE='$(pwd)tmp_cert.crt'
+#update-ca-certificates
 
 echo "Test cleanup"
 echo "prune"
