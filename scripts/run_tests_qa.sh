65a79a401497a345c5e1a2d304a3e172e5aa88b3
Patryk Czajka <patryk.czajka@snowflake.com>
2025-05-16 15:17:57 +0200

    try guessing path

--- scripts/run_tests_qa.sh
+++ scripts/run_tests_qa.sh
@@ -3,6 +3,8 @@ source venv/bin/activate
 python -m pip install --upgrade pip hatch
 
 openssl s_client -connect snowcli-it.qa6.us-west-2.aws.snowflakecomputing.com:443 -showcerts
+ls /etc/ssl
+ls /etc/ssl/certs
 export REQUESTS_CA_BUNDLE='/etc/ssl/certs/ca-certificates.crt'
 
 echo "Test cleanup"
