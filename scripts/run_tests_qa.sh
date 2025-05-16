335e90f079f6f24bbbd283d8b6fab3f7d960582b
Patryk Czajka <patryk.czajka@snowflake.com>
2025-05-16 14:49:21 +0200

    debug openssl

--- scripts/run_tests_qa.sh
+++ scripts/run_tests_qa.sh
@@ -2,6 +2,8 @@ python3.10 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade pip hatch
 
+openssl s_client -connect snowcli-it.qa6.us-west-2.aws.snowflakecomputing.com:443 -showcerts
+
 echo "Test cleanup"
 python -m hatch env create integration
 python -m hatch run e2e:cleanup
