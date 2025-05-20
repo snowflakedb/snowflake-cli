43fa9a3b68dd9e00715a084ad19c3c697c6ec4e1
Patryk Czajka <patryk.czajka@snowflake.com>
2025-05-20 10:57:13 +0200

    debug

--- scripts/run_tests_qa.sh
+++ scripts/run_tests_qa.sh
@@ -3,8 +3,14 @@ source venv/bin/activate
 python -m pip install --upgrade pip hatch
 
 echo "Test cleanup"
+python -m hatch env prune
 python -m hatch env create integration
 python -m hatch run e2e:cleanup
+
+echo ">>> DEBUG <<<"
+export SNOWFLAKE_CONNECTIONS_INTEGRATION_ROLE=accountadmin
+python -m hatch run integration:debug
+export SNOWFLAKE_CONNECTIONS_INTEGRATION_ROLE=integration_tests
 
 echo "Run test"
 python -m hatch run integration:test_qa
