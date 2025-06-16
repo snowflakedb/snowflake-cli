2104d4901b26e12c98e320a260bdd8ac0ce7ea45
Patryk Czajka <patryk.czajka@snowflake.com>
2025-06-16 16:32:43 +0200

    rename test

--- tests_integration/test_debug.py
+++ tests_integration/test_debug.py
@@ -1,16 +1,8 @@
 import pytest
 
 
-# @pytest.mark.integration
-# @pytest.mark.parametrize("smurfs", list(range(20)))
-# def test_deliberately_flaky(smurfs):
-#     from time import time_ns
-#
-#     assert time_ns() % 2000 == 0, "This test is deliberately flaky to raise alert"
-
-
 @pytest.mark.integration
-def test_deliberately_flaky(runner):
+def test_deliberately_flaky2(runner):
     dbname = "snowflake_cli_deliberately_flaky_test_run"
 
     result = runner.invoke_with_connection(["object", "describe", "database", dbname])
