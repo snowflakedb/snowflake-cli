168fd1f65dee375870bc80435489a947046ab6ba
Patryk Pasterny <patryk.pasterny@snowflake.com>
2025-07-01 10:12:24 +0200

    SNOW-2118790: security fixes (#2473)

--- scripts/packaging/delete_mac_certs.sh
+++ scripts/packaging/delete_mac_certs.sh
@@ -4,10 +4,10 @@
 KEYCHAIN_PATH=$HOME/Library/Keychains/login.keychain-db
 
 # Delete the Developer ID Installer certificate from the keychain
-security delete-certificate -c "Developer ID Installer: Snowflake Computing INC. (W4NT6CRQ7U)" "$KEYCHAIN_PATH"
+security delete-identity -c "Developer ID Installer: Snowflake Computing INC. (W4NT6CRQ7U)" "$KEYCHAIN_PATH"
 
 # Delete the Developer ID Application certificate from the keychain
-security delete-certificate -c "Developer ID Application: Snowflake Computing INC. (W4NT6CRQ7U)" "$KEYCHAIN_PATH"
+security delete-identity -c "Developer ID Application: Snowflake Computing INC. (W4NT6CRQ7U)" "$KEYCHAIN_PATH"
 
 # Inform the user about the successful cleanup
 echo "Temporary files cleaned up successfully"
