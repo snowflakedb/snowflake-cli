58eeda7814d64baf93e14ac48d6024e72d0360cf
Jakub Wilkowski <jakub.wilkowski@snowflake.com>
2025-08-11 12:55:53 +0200

    fix: [SNOW-2256576] dbt deploy - handle quoted names gracefully (#2530)

--- src/snowflake/cli/_plugins/dbt/manager.py
+++ src/snowflake/cli/_plugins/dbt/manager.py
@@ -26,6 +26,7 @@ from snowflake.cli.api.console import cli_console
 from snowflake.cli.api.constants import DEFAULT_SIZE_LIMIT_MB, ObjectType
 from snowflake.cli.api.exceptions import CliError
 from snowflake.cli.api.identifiers import FQN
+from snowflake.cli.api.project.util import unquote_identifier
 from snowflake.cli.api.secure_path import SecurePath
 from snowflake.cli.api.sql_execution import SqlExecutionMixin
 from snowflake.connector.cursor import SnowflakeCursor
@@ -66,7 +67,8 @@ class DBTManager(SqlExecutionMixin):
 
         with cli_console.phase("Creating temporary stage"):
             stage_manager = StageManager()
-            stage_fqn = FQN.from_string(f"dbt_{fqn.name}_stage").using_context()
+            unquoted_name = unquote_identifier(fqn.name)
+            stage_fqn = FQN.from_string(f"DBT_{unquoted_name}_STAGE").using_context()
             stage_name = stage_manager.get_standard_stage_prefix(stage_fqn)
             stage_manager.create(stage_fqn, temporary=True)
 
